finos/legend-studio

commit 6592e02f8a8b00d5150aabf6160d98dd20b5a80d

Activate query builder (#176)
Fix a small bug with query builder config and activate this plugin in the webapp

packages/legend-studio/src/models/protocols/pure/v1/transformation/pureGraph/to/helpers/V1_RelationalConnectionBuilderHelper.ts
@@ -22,6 +22,7 @@ import {
   guaranteeNonEmptyString,
 } from '@finos/legend-studio-shared';
 import type { DatasourceSpecification } from '../../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/DatasourceSpecification';
+import { LocalH2DatasourceSpecification } from '../../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/DatasourceSpecification';
 import {
   StaticDatasourceSpecification,
   EmbeddedH2DatasourceSpecification,
@@ -36,6 +37,7 @@ import {
 } from '../../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/AuthenticationStrategy';
 import type { V1_GraphBuilderContext } from '../../../../transformation/pureGraph/to/V1_GraphBuilderContext';
 import type { V1_DatasourceSpecification } from '../../../../model/packageableElements/store/relational/connection/V1_DatasourceSpecification';
+import { V1_LocalH2DataSourceSpecification } from '../../../../model/packageableElements/store/relational/connection/V1_DatasourceSpecification';
 import {
   V1_StaticDatasourceSpecification,
   V1_EmbeddedH2DatasourceSpecification,
@@ -124,6 +126,10 @@ export const V1_processDatasourceSpecification = (
       datasourceSpecificationProtocol.databaseName,
     );
     return snowflakeSpec;
+  } else if (
+    datasourceSpecificationProtocol instanceof V1_LocalH2DataSourceSpecification
+  ) {
+    return new LocalH2DatasourceSpecification();
   }
   const extraConnectionDatasourceSpecificationBuilders = context.extensions.plugins.flatMap(
     (plugin) =>

packages/legend-studio-app/src/index.scss
@@ -16,3 +16,4 @@
 
 @import '@finos/legend-studio/lib/index.css';
 @import '@finos/legend-studio-preset-dsl-text/lib/index.css';
+@import '@finos/legend-studio-plugin-query-builder/lib/index.css';

packages/legend-studio-app/src/index.tsx
@@ -15,6 +15,7 @@
  */
 
 import { Studio } from '@finos/legend-studio';
+import { QueryBuilderPlugin } from '@finos/legend-studio-plugin-query-builder';
 import { DSLText_Preset } from '@finos/legend-studio-preset-dsl-text';
 import { EFJSONSchema_Preset } from '@finos/legend-studio-preset-external-format-json-schema';
 import studioConfig from '../studio.config';
@@ -23,6 +24,7 @@ import './index.scss';
 Studio.create()
   .setup({ baseUrl: studioConfig.baseUrl })
   .withPresets([new DSLText_Preset(), new EFJSONSchema_Preset()])
+  .withPlugins([new QueryBuilderPlugin()])
   .start()
   .catch((e) => {
     throw e;

packages/legend-studio-plugin-query-builder/src/stores/QueryBuilderState.ts
@@ -209,10 +209,16 @@ export class QueryBuilderState extends EditorExtensionState {
   }
 
   reset(): void {
+    const currentQueryBuilderState = this.editorStore.getEditorExtensionState(
+      QueryBuilderState,
+    );
     changeEntry(
       this.editorStore.editorExtensionStates,
       this.editorStore.getEditorExtensionState(QueryBuilderState),
-      new QueryBuilderState(this.editorStore),
+      new QueryBuilderState(this.editorStore, {
+        TEMPORARY__enableGraphFetch:
+          currentQueryBuilderState.TEMPORARY__enableGraphFetch,
+      }),
     );
   }
 

packages/legend-studio/src/models/metamodels/pure/model/packageableElements/store/relational/connection/DatasourceSpecification.ts
@@ -121,6 +121,21 @@ export class EmbeddedH2DatasourceSpecification
   }
 }
 
+export class LocalH2DatasourceSpecification
+  extends DatasourceSpecification
+  implements Hashable {
+  testDataSetupCsv?: string;
+  testDataSetupSqls: string[] = [];
+
+  get hashCode(): string {
+    return hashArray([
+      CORE_HASH_STRUCTURE.EMBEDDED_H2_DATASOURCE_SPECIFICATION,
+      this.testDataSetupCsv ?? '',
+      hashArray(this.testDataSetupSqls),
+    ]);
+  }
+}
+
 export class SnowflakeDatasourceSpecification
   extends DatasourceSpecification
   implements Hashable {

packages/legend-studio/src/models/protocols/pure/v1/model/packageableElements/store/relational/connection/V1_DatasourceSpecification.ts
@@ -22,6 +22,7 @@ export enum V1_DatasourceSpecificationType {
   STATIC = 'static',
   H2_EMBEDDED = 'h2Embedded',
   SNOWFLAKE = 'snowflake',
+  H2_LOCAL = 'h2Local',
 }
 
 export abstract class V1_DatasourceSpecification implements Hashable {
@@ -79,6 +80,21 @@ export class V1_SnowflakeDatasourceSpecification
       this.region,
       this.warehouseName,
       this.databaseName,
+    ]);
+  }
+}
+
+export class V1_LocalH2DataSourceSpecification
+  extends V1_DatasourceSpecification
+  implements Hashable {
+  testDataSetupCsv?: string;
+  testDataSetupSqls: string[] = [];
+
+  get hashCode(): string {
+    return hashArray([
+      CORE_HASH_STRUCTURE.EMBEDDED_H2_DATASOURCE_SPECIFICATION,
+      this.testDataSetupCsv ?? '',
+      hashArray(this.testDataSetupSqls),
     ]);
   }
 }

packages/legend-studio/src/models/protocols/pure/v1/transformation/pureGraph/from/V1_ConnectionTransformer.ts
@@ -38,6 +38,7 @@ import {
 } from '../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/AuthenticationStrategy';
 import type { DatasourceSpecification } from '../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/DatasourceSpecification';
 import {
+  LocalH2DatasourceSpecification,
   StaticDatasourceSpecification,
   EmbeddedH2DatasourceSpecification,
   SnowflakeDatasourceSpecification,
@@ -50,6 +51,7 @@ import {
 import { V1_PackageableConnection } from '../../../model/packageableElements/connection/V1_PackageableConnection';
 import type { V1_DatasourceSpecification } from '../../../model/packageableElements/store/relational/connection/V1_DatasourceSpecification';
 import {
+  V1_LocalH2DataSourceSpecification,
   V1_EmbeddedH2DatasourceSpecification,
   V1_SnowflakeDatasourceSpecification,
   V1_StaticDatasourceSpecification,
@@ -114,6 +116,8 @@ const transformDatasourceSpecification = (
     return transformEmbeddedH2DatasourceSpecification(metamodel);
   } else if (metamodel instanceof SnowflakeDatasourceSpecification) {
     return transformSnowflakeDatasourceSpecification(metamodel);
+  } else if (metamodel instanceof LocalH2DatasourceSpecification) {
+    return new V1_LocalH2DataSourceSpecification();
   }
   const extraConnectionDatasourceSpecificationTransformers = plugins.flatMap(
     (plugin) =>

packages/legend-studio/src/models/protocols/pure/v1/transformation/pureProtocol/serializationHelpers/V1_ConnectionSerializationHelper.ts
@@ -40,6 +40,7 @@ import { V1_FlatDataConnection } from '../../../model/packageableElements/store/
 import { V1_RelationalDatabaseConnection } from '../../../model/packageableElements/store/relational/connection/V1_RelationalDatabaseConnection';
 import type { V1_DatasourceSpecification } from '../../../model/packageableElements/store/relational/connection/V1_DatasourceSpecification';
 import {
+  V1_LocalH2DataSourceSpecification,
   V1_SnowflakeDatasourceSpecification,
   V1_StaticDatasourceSpecification,
   V1_EmbeddedH2DatasourceSpecification,
@@ -118,6 +119,7 @@ export const V1_flatDataConnectionModelSchema = createModelSchema(
 enum V1_DatasourceSpecificationType {
   STATIC = 'static',
   H2_EMBEDDED = 'h2Embedded',
+  H2_LOCAL = 'h2Local',
   SNOWFLAKE = 'snowflake',
 }
 
@@ -138,6 +140,15 @@ const embeddedH2DatasourceSpecificationModelSchema = createModelSchema(
     autoServerMode: primitive(),
     databaseName: primitive(),
     directory: primitive(),
+  },
+);
+
+const localH2DatasourceSpecificationModelSchema = createModelSchema(
+  V1_LocalH2DataSourceSpecification,
+  {
+    _type: usingConstantValueSchema(V1_DatasourceSpecificationType.H2_LOCAL),
+    testDataSetupCsv: primitive(),
+    testDataSetupSqls: list(primitive()),
   },
 );
 
@@ -162,6 +173,8 @@ export const V1_serializeDatasourceSpecification = (
     return serialize(embeddedH2DatasourceSpecificationModelSchema, protocol);
   } else if (protocol instanceof V1_SnowflakeDatasourceSpecification) {
     return serialize(snowflakeDatasourceSpecificationModelSchema, protocol);
+  } else if (protocol instanceof V1_LocalH2DataSourceSpecification) {
+    return serialize(localH2DatasourceSpecificationModelSchema, protocol);
   }
   const extraConnectionDatasourceSpecificationProtocolSerializers = plugins.flatMap(
     (plugin) =>
@@ -192,6 +205,8 @@ export const V1_deserializeDatasourceSpecification = (
       return deserialize(embeddedH2DatasourceSpecificationModelSchema, json);
     case V1_DatasourceSpecificationType.SNOWFLAKE:
       return deserialize(snowflakeDatasourceSpecificationModelSchema, json);
+    case V1_DatasourceSpecificationType.H2_LOCAL:
+      return deserialize(localH2DatasourceSpecificationModelSchema, json);
     default: {
       const extraConnectionDatasourceSpecificationProtocolDeserializers = plugins.flatMap(
         (plugin) =>

commit 3f70c1c0bb96c8b9c0dc91feb81f02f194493aaf

fix minor code issues introduced by #176

packages/legend-studio/src/models/protocols/pure/v1/transformation/pureGraph/to/helpers/V1_RelationalConnectionBuilderHelper.ts
@@ -22,8 +22,8 @@ import {
   guaranteeNonEmptyString,
 } from '@finos/legend-studio-shared';
 import type { DatasourceSpecification } from '../../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/DatasourceSpecification';
-import { LocalH2DatasourceSpecification } from '../../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/DatasourceSpecification';
 import {
+  LocalH2DatasourceSpecification,
   StaticDatasourceSpecification,
   EmbeddedH2DatasourceSpecification,
   SnowflakeDatasourceSpecification,
@@ -37,8 +37,8 @@ import {
 } from '../../../../../../../metamodels/pure/model/packageableElements/store/relational/connection/AuthenticationStrategy';
 import type { V1_GraphBuilderContext } from '../../../../transformation/pureGraph/to/V1_GraphBuilderContext';
 import type { V1_DatasourceSpecification } from '../../../../model/packageableElements/store/relational/connection/V1_DatasourceSpecification';
-import { V1_LocalH2DataSourceSpecification } from '../../../../model/packageableElements/store/relational/connection/V1_DatasourceSpecification';
 import {
+  V1_LocalH2DataSourceSpecification,
   V1_StaticDatasourceSpecification,
   V1_EmbeddedH2DatasourceSpecification,
   V1_SnowflakeDatasourceSpecification,