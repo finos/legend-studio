6e326cfaf801b6dafa2dbd8855cf08438cabaeb7
An Phi <aphi@skidmore.edu>
2021-06-23 14:35:15 -0400

    update ESLint config for no-extraneous-dependencies rule

--- packages/eslint-plugin/src/configs/computationally-expensive.js
+++ packages/eslint-plugin/src/configs/computationally-expensive.js
@@ -17,19 +17,11 @@
 const ERROR = 2;
 
 const IMPORT_RULES = {
-  // NOTE: this rule prevents using dependencies not listed in `package.json` but
-  // currently, it's not working well with monorepo
-  // See https://github.com/benmosher/eslint-plugin-import/pull/1696
-  // 'import/no-extraneous-dependencies': [
-  //   ERROR,
-  //   {
-  //     packageDir: [__dirname].concat(
-  //       packages.map((packageName) => // const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
-  //         path.resolve(__dirname, `packages/${packageName}/`),
-  //       ),
-  //     ),
-  //   },
-  // ],
+  // NOTE: this rule prevents using dependencies not listed in `package.json`
+  // We temporarily disable it due to the following issue
+  // See https://github.com/benmosher/eslint-plugin-import/issues/2120
+  // See https://github.com/benmosher/eslint-plugin-import/pull/2121
+  // 'import/no-extraneous-dependencies': ERROR,
   // See https://github.com/benmosher/eslint-plugin-import/blob/master/config/warnings.js
   'import/no-named-as-default': ERROR,
   'import/no-named-as-default-member': ERROR,
