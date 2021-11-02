2d855dc1e74f3d3cbfedbdf9750730ffe52d4cdd
mrudula-gs <89876341+mrudula-gs@users.noreply.github.com>
2021-11-02 10:50:52 -0500

    fix service test issue with (un)escaping assertion data (#629)

--- packages/legend-studio/src/stores/editor-state/element-editor-state/service/ServiceTestState.ts
+++ packages/legend-studio/src/stores/editor-state/element-editor-state/service/ServiceTestState.ts
@@ -29,8 +29,6 @@ import {
   tryToMinifyLosslessJSONString,
   tryToFormatLosslessJSONString,
   tryToFormatJSONString,
-  toGrammarString,
-  fromGrammarString,
   createUrlStringFromData,
 } from '@finos/legend-shared';
 import type { EditorStore } from '../../../EditorStore';
@@ -136,7 +134,11 @@ export class TestContainerState {
       this.testContainer.assert =
         this.editorStore.graphManagerState.graphManager.HACKY_createServiceTestAssertLambda(
           /* @MARKER: Workaround for https://github.com/finos/legend-studio/issues/68 */
-          toGrammarString(tryToMinifyLosslessJSONString(this.assertionData)),
+          // NOTE: due to discrepancies in the test runners for mapping and service, we have don't need
+          // to do any (un)escaping here like what we do for mapping test assertion data. For better context:
+          // See https://github.com/finos/legend-studio/issues/586
+          // See https://github.com/finos/legend-engine/issues/429
+          tryToMinifyLosslessJSONString(this.assertionData),
         );
     }
   }
@@ -147,10 +149,12 @@ export class TestContainerState {
         testContainter.assert,
       );
     this.assertionData = expectedResultAssertionString
-      ? fromGrammarString(
-          /* @MARKER: Workaround for https://github.com/finos/legend-studio/issues/68 */
-          tryToFormatLosslessJSONString(expectedResultAssertionString),
-        )
+      ? /* @MARKER: Workaround for https://github.com/finos/legend-studio/issues/68 */
+        // NOTE: due to discrepancies in the test runners for mapping and service, we have don't need
+        // to do any (un)escaping here like what we do for mapping test assertion data. For better context:
+        // See https://github.com/finos/legend-studio/issues/586
+        // See https://github.com/finos/legend-engine/issues/429
+        tryToFormatLosslessJSONString(expectedResultAssertionString)
       : undefined;
   }
 
