finos/legend-studio

commit 4623d5c1970823151de2f820d79662b560641c67

cleanup serialization logic (#1115)

packages/legend-shared/src/application/SerializationUtils.ts
@@ -16,7 +16,6 @@
 
 import { type PlainObject, pruneNullValues } from '../CommonUtils';
 import {
-  type ClazzOrModelSchema,
   type ModelSchema,
   type PropSchema,
   custom,
@@ -25,75 +24,46 @@ import {
   serialize,
 } from 'serializr';
 
-// NOTE: we need these methods because `map()` of `serializr` tries to smartly determines if it should produce object or ES6 Map
-// but we always want ES6 Map, so we would use this function
-export const deseralizeMap = <T>(
-  val: Record<string, T>,
-  schema?: ClazzOrModelSchema<T>,
-): Map<string, T> => {
-  const result = new Map<string, T>();
-  Object.keys(val).forEach((key: string) =>
-    result.set(
-      key,
-      schema ? deserialize(schema, val[key]) : (val[key] as NonNullable<T>),
-    ),
-  );
-  return result;
-};
-
-export const serializeMap = <T>(
-  val: Map<string, T> | undefined,
-  schema?: ClazzOrModelSchema<T>,
-): Record<PropertyKey, unknown> => {
-  const result: Record<PropertyKey, unknown> = {};
-  val?.forEach((v, key) => {
-    result[key] = schema ? serialize(schema, v) : v;
-  });
-  return result;
-};
-
-export class BasicSerializationFactory<T> {
+export class SerializationFactory<T> {
   readonly schema: ModelSchema<T>;
+  readonly deserializeNullAsUndefined: boolean;
 
-  constructor(schema: ModelSchema<T>) {
+  constructor(
+    schema: ModelSchema<T>,
+    options?: {
+      /**
+       * Sometimes, entities coming from server which returns values which have not been set to `null`
+       * instead of `undefined`. This will cause `serializr` to throw.
+       *
+       * e.g.
+       * // in Server (Java + Jackson):
+       * Person person; // this will be serialized to `null` by Jackson
+       * (depending on the setting of the server)
+       *
+       * // in our code (TS + serializr):
+       * person: optional(object(Person))
+       *
+       * --> error thrown
+       */
+      deserializeNullAsUndefined?: boolean | undefined;
+    },
+  ) {
     this.schema = schema;
-  }
-}
-
-// NOTE: due to contra-variance and variance, this class will not work for polymorphism
-// hence, use `BasicSerializationFactory`
-export class SerializationFactory<T> extends BasicSerializationFactory<T> {
-  toJson = (val: T): PlainObject<T> => serialize(this.schema, val);
-  fromJson = (val: PlainObject<T>): T => deserialize(this.schema, val);
-}
-
-/**
- * Similar to {@link SerializationFactory} but used for entities coming from server which returns
- * values which have not been set to `null` instead of `undefined`. This will cause `serializr` to
- * throw.
- *
- * e.g.
- * // in Server (Java + Jackson):
- * Person person; // this will be serialized to `null` by Jackson (depending on the setting of the server)
- *
- * // in our code (TS + serializr):
- * person: optional(object(Person))
- *
- * --> error thrown
- */
-export class NullphobicSerializationFactory<T> {
-  /**
-   * Since we customize the behavior of the deserializer, we must not expose the schema
-   */
-  private readonly schema: ModelSchema<T>;
-
-  constructor(schema: ModelSchema<T>) {
-    this.schema = schema;
+    this.deserializeNullAsUndefined = Boolean(
+      options?.deserializeNullAsUndefined,
+    );
   }
 
-  toJson = (val: T): PlainObject<T> => serialize(this.schema, val);
-  fromJson = (val: PlainObject<T>): T =>
-    deserialize(this.schema, pruneNullValues(val));
+  toJson(val: T): PlainObject<T> {
+    return serialize(this.schema, val);
+  }
+
+  fromJson(val: PlainObject<T>): T {
+    return deserialize(
+      this.schema,
+      this.deserializeNullAsUndefined ? pruneNullValues(val) : val,
+    );
+  }
 }
 
 export const usingModelSchema = <T>(schema: ModelSchema<T>): PropSchema =>
@@ -102,31 +72,81 @@ export const usingModelSchema = <T>(schema: ModelSchema<T>): PropSchema =>
     (value) => deserialize(schema, value),
   );
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const serializeArray = (
-  values: any,
-  itemSerializer: (val: any) => any,
-  skipIfEmpty: boolean,
-): any[] | typeof SKIP =>
-  Array.isArray(values)
-    ? values.length
-      ? values.map((value) => itemSerializer(value))
-      : skipIfEmpty
-      ? SKIP
-      : []
-    : SKIP;
-
-export const deserializeArray = (
-  values: any,
-  itemDeserializer: (val: any) => any,
-  skipIfEmpty: boolean,
-): any[] | typeof SKIP => {
+export const deserializeArray = <T>(
+  values: unknown,
+  itemDeserializer: (val: PlainObject<T>) => T,
+  options?: {
+    skipIfEmpty?: boolean;
+  },
+): T[] | typeof SKIP => {
   if (Array.isArray(values)) {
-    return values.map(itemDeserializer);
+    return (values as PlainObject<T>[]).map(itemDeserializer);
   }
-  return skipIfEmpty ? SKIP : [];
+  return options?.skipIfEmpty ? SKIP : [];
 };
-/* eslint-enable @typescript-eslint/no-explicit-any */
+
+export const serializeArray = <T>(
+  values: unknown,
+  itemSerializer: (val: T) => T extends object ? PlainObject<T> : T,
+  options?: {
+    /**
+     * If the array is empty, skip serializing it (the property will not
+     * appear in the JSON, this is different to setting it to `undefined`)
+     */
+    skipIfEmpty?: boolean;
+    /**
+     * In engine, the initialization handling of list-types attributes in protocol models, either done
+     * by the grammar parser or set natively in the model is fairly inconsistent. This produces protocol
+     * JSONs with the field sometimes being set as empty array, or sometimes being set as nullish (hence,
+     * omitted from the JSON).
+     *
+     * In Studio, we avoid serialize empty array altogether to lessen the size of the serialized graph to save bandwidth,
+     * this optimization will cause protocol roundtrip test to fail. As such, we add this flag to make sure
+     * roundtrip test can run fine. Setting this flag to `true` will override the effect of `skipIfEmpty=true`
+     */
+    INTERNAL__forceReturnEmptyInTest?: boolean;
+  },
+): (T extends object ? PlainObject<T> : T)[] | typeof SKIP => {
+  const forceReturnEmptyInTest =
+    options?.INTERNAL__forceReturnEmptyInTest &&
+    // eslint-disable-next-line no-process-env
+    process.env.TEST_MODE === 'grammar';
+  if (Array.isArray(values)) {
+    return values.length
+      ? values.map((value) => itemSerializer(value))
+      : forceReturnEmptyInTest
+      ? []
+      : options?.skipIfEmpty
+      ? SKIP
+      : [];
+  }
+  return forceReturnEmptyInTest ? [] : SKIP;
+};
+
+// NOTE: we need these methods because `map()` of `serializr` tries to smartly
+// determines if it should produce object or ES6 Map but we always want ES6 Map,
+// so we would use this function
+export const deserializeMap = <T>(
+  val: Record<string, PlainObject<T>>,
+  itemDeserializer: (val: PlainObject<T>) => T,
+): Map<string, T> => {
+  const result = new Map<string, T>();
+  Object.keys(val).forEach((key: string) =>
+    result.set(key, itemDeserializer(val[key] as PlainObject<T>)),
+  );
+  return result;
+};
+
+export const serializeMap = <T>(
+  val: Map<string, T>,
+  itemSerializer: (val: T) => T extends object ? PlainObject<T> : T,
+): Record<PropertyKey, unknown> => {
+  const result: Record<PropertyKey, unknown> = {};
+  val.forEach((v, key) => {
+    result[key] = itemSerializer(v);
+  });
+  return result;
+};
 
 export const usingConstantValueSchema = (
   value: unknown | typeof SKIP,
@@ -147,9 +167,11 @@ export const usingConstantValueSchema = (
  * are automatically skipped
  * See https://github.com/mobxjs/serializr/issues/73#issuecomment-535641545
  */
-export const optionalCustom = <T>(
-  serializer: (val: T) => PlainObject<T>,
-  deserializer: (val: PlainObject<T>) => T,
+export const optionalCustom = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  serializer: (val: any) => any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  deserializer: (val: any) => any,
 ): PropSchema =>
   custom(
     (val) => (val ? serializer(val) : SKIP),

packages/legend-extension-dsl-data-space/src/models/protocols/pure/v1/transformation/pureProtocol/V1_DSLDataSpace_ProtocolHelper.ts
@@ -26,7 +26,6 @@ import {
   SKIP,
 } from 'serializr';
 import {
-  type V1_StereotypePtr,
   V1_stereotypePtrSchema,
   V1_taggedValueSchema,
   V1_packageableElementPointerDeserializerSchema,
@@ -107,14 +106,19 @@ export const V1_dataSpaceModelSchema = createModelSchema(V1_DataSpace, {
         values,
         (value) =>
           serialize(V1_packageableElementPointerDeserializerSchema, value),
-        true,
+        {
+          skipIfEmpty: true,
+          INTERNAL__forceReturnEmptyInTest: true,
+        },
       ),
     (values) =>
       deserializeArray(
         values,
         (value) =>
           deserialize(V1_packageableElementPointerDeserializerSchema, value),
-        false,
+        {
+          skipIfEmpty: false,
+        },
       ),
   ),
   groupId: primitive(),
@@ -125,14 +129,15 @@ export const V1_dataSpaceModelSchema = createModelSchema(V1_DataSpace, {
       serializeArray(
         values,
         (value) => serialize(V1_stereotypePtrSchema, value),
-        true,
+        {
+          skipIfEmpty: true,
+          INTERNAL__forceReturnEmptyInTest: true,
+        },
       ),
     (values) =>
-      deserializeArray(
-        values,
-        (v: V1_StereotypePtr) => deserialize(V1_stereotypePtrSchema, v),
-        false,
-      ),
+      deserializeArray(values, (v) => deserialize(V1_stereotypePtrSchema, v), {
+        skipIfEmpty: false,
+      }),
   ),
   supportInfo: custom(
     (val) => V1_serializeSupportInfo(val),
@@ -143,14 +148,15 @@ export const V1_dataSpaceModelSchema = createModelSchema(V1_DataSpace, {
       serializeArray(
         values,
         (value) => serialize(V1_taggedValueSchema, value),
-        true,
+        {
+          skipIfEmpty: true,
+          INTERNAL__forceReturnEmptyInTest: true,
+        },
       ),
     (values) =>
-      deserializeArray(
-        values,
-        (v: V1_StereotypePtr) => deserialize(V1_taggedValueSchema, v),
-        false,
-      ),
+      deserializeArray(values, (v) => deserialize(V1_taggedValueSchema, v), {
+        skipIfEmpty: false,
+      }),
   ),
   versionId: primitive(),
 });

packages/legend-extension-dsl-persistence/src/models/protocols/pure/v1/transformation/pureProtocol/V1_DSLPersistence_ProtocolHelper.ts
@@ -154,8 +154,15 @@ const V1_deserializeNotifyee = (
 
 const V1_notifierModelSchema = createModelSchema(V1_Notifier, {
   notifyees: custom(
-    (val) => serializeArray(val, (v) => V1_serializeNotifyee(v), true),
-    (val) => deserializeArray(val, (v) => V1_deserializeNotifyee(v), false),
+    (val) =>
+      serializeArray(val, (v: V1_Notifyee) => V1_serializeNotifyee(v), {
+        skipIfEmpty: true,
+        INTERNAL__forceReturnEmptyInTest: true,
+      }),
+    (val) =>
+      deserializeArray(val, (v) => V1_deserializeNotifyee(v), {
+        skipIfEmpty: false,
+      }),
   ),
 });
 
@@ -789,16 +796,17 @@ const V1_multiFlatTargetModelSchema = createModelSchema(V1_MultiFlatTarget, {
   modelClass: primitive(),
   parts: custom(
     (val) =>
-      serializeArray(
-        val,
-        (v) => serialize(V1_multiFlatTargetPartSchema, v),
-        true,
-      ),
+      serializeArray(val, (v) => serialize(V1_multiFlatTargetPartSchema, v), {
+        skipIfEmpty: true,
+        INTERNAL__forceReturnEmptyInTest: true,
+      }),
     (val) =>
       deserializeArray(
         val,
         (v) => deserialize(V1_multiFlatTargetPartSchema, v),
-        false,
+        {
+          skipIfEmpty: false,
+        },
       ),
   ),
   transactionScope: primitive(),

packages/legend-extension-external-store-service/src/models/protocols/pure/v1/transformation/pureProtocol/V1_ESService_ProtocolHelper.ts
@@ -22,6 +22,8 @@ import {
   usingModelSchema,
   serializeArray,
   deserializeArray,
+  serializeMap,
+  deserializeMap,
 } from '@finos/legend-shared';
 import {
   type ModelSchema,
@@ -35,7 +37,6 @@ import {
   deserialize,
   object,
   raw,
-  SKIP,
 } from 'serializr';
 import { V1_ServiceStore } from '../../model/packageableElements/store/serviceStore/model/V1_ESService_ServiceStore';
 import { V1_ServiceStoreService } from '../../model/packageableElements/store/serviceStore/model/V1_ESService_ServiceStoreService';
@@ -569,69 +570,25 @@ export const V1_serviceRequestPatternModelSchema = createModelSchema(
       (values) =>
         serializeArray(
           values,
-          (value) => V1_serializeStringValuePattern(value),
-          true,
+          (value: V1_StringValuePattern) =>
+            V1_serializeStringValuePattern(value),
+          {
+            skipIfEmpty: true,
+          },
         ),
       (values) =>
-        deserializeArray(
-          values,
-          (v: PlainObject<V1_StringValuePattern>) =>
-            V1_deserializeStringValuePattern(v),
-          true,
-        ),
+        deserializeArray(values, (v) => V1_deserializeStringValuePattern(v), {
+          skipIfEmpty: false,
+        }),
     ),
-    headerParams: optional(
-      custom(
-        (val) => {
-          if (val) {
-            const result: Record<PropertyKey, unknown> = {};
-            val.forEach((v: V1_StringValuePattern, key: string | number) => {
-              result[key] = V1_serializeStringValuePattern(v);
-            });
-            return result;
-          } else {
-            return SKIP;
-          }
-        },
-        (val) => {
-          if (val) {
-            const result = new Map<string, V1_StringValuePattern>();
-            Object.keys(val).forEach((key: string) =>
-              result.set(key, V1_deserializeStringValuePattern(val[key])),
-            );
-            return result;
-          } else {
-            return SKIP;
-          }
-        },
-      ),
+    headerParams: optionalCustom(
+      (val) => serializeMap(val, V1_serializeStringValuePattern),
+      (val) => deserializeMap(val, V1_deserializeStringValuePattern),
     ),
     method: primitive(),
-    queryParams: optional(
-      custom(
-        (val) => {
-          if (val) {
-            const result: Record<PropertyKey, unknown> = {};
-            val.forEach((v: V1_StringValuePattern, key: string | number) => {
-              result[key] = V1_serializeStringValuePattern(v);
-            });
-            return result;
-          } else {
-            return SKIP;
-          }
-        },
-        (val) => {
-          if (val) {
-            const result = new Map<string, V1_StringValuePattern>();
-            Object.keys(val).forEach((key: string) =>
-              result.set(key, V1_deserializeStringValuePattern(val[key])),
-            );
-            return result;
-          } else {
-            return SKIP;
-          }
-        },
-      ),
+    queryParams: optionalCustom(
+      (val) => serializeMap(val, V1_serializeStringValuePattern),
+      (val) => deserializeMap(val, V1_deserializeStringValuePattern),
     ),
     url: optional(primitive()),
     urlPath: optional(primitive()),

packages/legend-graph/src/__tests__/ImportResolutionRoundtrip.test.ts
@@ -64,7 +64,7 @@ import {
 } from './roundtripTestData/TEST_DATA__MappingRoundtrip';
 import { TEST_DATA__RuntimeRoundtrip } from './roundtripTestData/TEST_DATA__RuntimeRoundtrip';
 import { TEST__checkBuildingElementsRoundtrip } from '../GraphManagerTestUtils';
-import { TEST_DATA__DataRoundtrip } from './roundtripTestData/TEST_DATA_DataRoundtrip';
+import { TEST_DATA__DataRoundtrip } from './roundtripTestData/TEST_DATA__DataRoundtrip';
 
 describe(unitTest('M2M graph roundtrip'), () => {
   test.each([
@@ -212,7 +212,7 @@ describe(unitTest('Service import resolution roundtrip'), () => {
   );
 });
 
-describe(unitTest('Data import resolution roundtrip'), () => {
+describe(unitTest('DSL Data import resolution roundtrip'), () => {
   test.each([['Simple data', TEST_DATA__DataRoundtrip]])(
     '%s',
     async (testName, entities) => {

packages/legend-graph/src/__tests__/Inference.test.ts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+/// <reference types="jest-extended" />
 import { unitTest } from '@finos/legend-shared';
 import {
   TEST_DATA__InferenceDefaultMappingElementID,

packages/legend-graph/src/__tests__/roundtripTestData/TEST_DATA__RelationalRoundtrip.ts
@@ -3816,6 +3816,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
           oauthKey: 'dummy',
           scopeName: 'UserPass',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'redshift',
           clusterID: 'cluster',
@@ -3844,6 +3845,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
           oauthKey: 'dummy',
           scopeName: 'UserPass',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'snowflake',
           accountName: 'dummy',
@@ -3870,6 +3872,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
           oauthKey: 'dummy',
           scopeName: 'UserPass',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'snowflake',
           accountName: 'dummy',
@@ -3897,6 +3900,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
           oauthKey: 'dummy',
           scopeName: 'UserPass',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'snowflake',
           accountName: 'dummy',
@@ -3924,6 +3928,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
           oauthKey: 'dummy',
           scopeName: 'UserPass',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'snowflake',
           accountName: 'dummy',
@@ -3949,6 +3954,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
         authenticationStrategy: {
           _type: 'h2Default',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'static',
           databaseName: 'myDb',
@@ -3972,6 +3978,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
         authenticationStrategy: {
           _type: 'h2Default',
         },
+        databaseType: 'Snowflake',
         datasourceSpecification: {
           _type: 'snowflake',
           accountName: 'acct1',
@@ -3996,6 +4003,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
         authenticationStrategy: {
           _type: 'h2Default',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'static',
           databaseName: 'myDb',
@@ -4021,6 +4029,7 @@ export const TEST_DATA__RelationalDatabaseConnectionRoundtrip = [
         authenticationStrategy: {
           _type: 'h2Default',
         },
+        databaseType: 'H2',
         datasourceSpecification: {
           _type: 'static',
           databaseName: 'myDb',

commit 8cf348dce6350d4133c943f5858a9b312227fdbb

fix bug introduced by #1115

packages/legend-shared/src/application/SerializationUtils.ts
@@ -107,10 +107,17 @@ export const serializeArray = <T>(
     INTERNAL__forceReturnEmptyInTest?: boolean;
   },
 ): (T extends object ? PlainObject<T> : T)[] | typeof SKIP => {
-  const forceReturnEmptyInTest =
-    options?.INTERNAL__forceReturnEmptyInTest &&
-    // eslint-disable-next-line no-process-env
-    process.env.TEST_MODE === 'grammar';
+  let forceReturnEmptyInTest = false;
+  // NOTE: this block is meant for test, `webpack` will tree-shake it
+  // so we never reach inside, else we would get error about `process is not defined` as we're
+  // in browser environment
+  // eslint-disable-next-line no-process-env
+  if (process.env.NODE_ENV === 'test') {
+    forceReturnEmptyInTest =
+      Boolean(options?.INTERNAL__forceReturnEmptyInTest) &&
+      // eslint-disable-next-line no-process-env
+      process.env.TEST_MODE === 'grammar';
+  }
   if (Array.isArray(values)) {
     return values.length
       ? values.map((value) => itemSerializer(value))