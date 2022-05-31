finos/legend-studio

commit 6ed0fcbcc0e7b77c84b8eb8442ef05fbdc7d8695

adopt new parser API endpoints (#960)

packages/legend-graph/src/models/protocols/pure/v1/engine/V1_EngineServerClient.ts
@@ -28,8 +28,6 @@ import type { V1_DEPRECATED__ServiceTestResult } from './service/V1_DEPRECATED__
 import type { V1_ServiceRegistrationResult } from './service/V1_ServiceRegistrationResult';
 import type { V1_ServiceConfigurationInfo } from './service/V1_ServiceConfiguration';
 import type { V1_CompileResult } from './compilation/V1_CompileResult';
-import type { V1_GrammarToJsonInput } from './grammar/V1_GrammarToJson';
-import type { V1_JsonToGrammarInput } from './grammar/V1_JsonToGrammarInput';
 import type { V1_RawLambda } from '../model/rawValueSpecification/V1_RawLambda';
 import type { V1_PureModelContextGenerationInput } from './import/V1_PureModelContextGenerationInput';
 import type { V1_GenerateFileInput } from './generation/V1_FileGenerationInput';
@@ -39,8 +37,6 @@ import type { V1_GenerationConfigurationDescription } from './generation/V1_Gene
 import type { V1_GenerationOutput } from './generation/V1_GenerationOutput';
 import type { V1_ExecuteInput } from './execution/V1_ExecuteInput';
 import type { V1_PureModelContext } from '../model/context/V1_PureModelContext';
-import type { V1_RelationalOperationElementGrammarToJsonInput } from './grammar/V1_RelationalOperationElementGrammarToJson';
-import type { V1_RelationalOperationElementJsonToGrammarInput } from './grammar/V1_RelationalOperationElementJsonToGrammarInput';
 import type { V1_ExecutionPlan } from '../model/executionPlan/V1_ExecutionPlan';
 import type { V1_LightQuery, V1_Query } from './query/V1_Query';
 import type { V1_ServiceStorage } from './service/V1_ServiceStorage';
@@ -51,6 +47,9 @@ import type { V1_ExternalFormatDescription } from './externalFormat/V1_ExternalF
 import type { V1_ExternalFormatModelGenerationInput } from './externalFormat/V1_ExternalFormatModelGeneration';
 import type { V1_RunTestsInput } from './test/V1_RunTestsInput';
 import type { V1_TestResult } from '../model/test/result/V1_TestResult';
+import type { V1_RawRelationalOperationElement } from '../model/packageableElements/store/relational/model/V1_RawRelationalOperationElement';
+import type { V1_RenderStyle } from './grammar/V1_RenderStyle';
+import type { V1_ParserError } from './grammar/V1_ParserError';
 
 enum CORE_ENGINE_TRACER_SPAN {
   GRAMMAR_TO_JSON = 'transform Pure code to protocol',
@@ -78,6 +77,18 @@ enum CORE_ENGINE_TRACER_SPAN {
   UPDATE_QUERY = 'update query',
   DELETE_QUERY = 'delete query',
 }
+
+type GrammarParserBatchInputEntry = {
+  value: string;
+  returnSourceInformation?: boolean | undefined;
+  sourceInformationOffset?:
+    | {
+        sourceId?: string | undefined;
+        lineOffset?: number | undefined;
+        columnOffset?: number | undefined;
+      }
+    | undefined;
+};
 
 export class V1_EngineServerClient extends AbstractServerClient {
   currentUserId?: string | undefined;
@@ -131,24 +142,59 @@ export class V1_EngineServerClient extends AbstractServerClient {
 
   // ------------------------------------------- Grammar -------------------------------------------
 
-  transformGrammarToJSON = (
-    input: PlainObject<V1_GrammarToJsonInput>,
-  ): Promise<PlainObject<V1_JsonToGrammarInput>> =>
+  _grammarToJSON = (): string => `${this._pure()}/grammar/grammarToJson`;
+
+  grammarToJSON_model = (
+    input: string,
+    sourceId?: string | undefined,
+    lineOffset?: number | undefined,
+    returnSourceInformation?: boolean | undefined,
+  ): Promise<PlainObject<V1_PureModelContextData>> =>
     this.postWithTracing(
       this.getTraceData(CORE_ENGINE_TRACER_SPAN.GRAMMAR_TO_JSON),
-      `${this._pure()}/grammar/transformGrammarToJson`,
+      `${this._grammarToJSON()}/model`,
       input,
       {},
       undefined,
-      undefined,
+      {
+        sourceId,
+        lineOffset,
+        returnSourceInformation,
+      },
       { enableCompression: true },
     );
-  transformJSONToGrammar = (
-    input: PlainObject<V1_JsonToGrammarInput>,
-  ): Promise<PlainObject<V1_JsonToGrammarInput>> =>
+
+  grammarToJSON_lambda = (
+    input: string,
+    sourceId?: string | undefined,
+    lineOffset?: number | undefined,
+    columnOffset?: number | undefined,
+    returnSourceInformation?: boolean | undefined,
+  ): Promise<PlainObject<V1_RawLambda>> =>
     this.postWithTracing(
-      this.getTraceData(CORE_ENGINE_TRACER_SPAN.JSON_TO_GRAMMAR),
-      `${this._pure()}/grammar/transformJsonToGrammar`,
+      this.getTraceData(CORE_ENGINE_TRACER_SPAN.GRAMMAR_TO_JSON),
+      `${this._grammarToJSON()}/lambda`,
+      input,
+      {},
+      undefined,
+      {
+        sourceId,
+        lineOffset,
+        columnOffset,
+        returnSourceInformation,
+      },
+      { enableCompression: true },
+    );
+
+  grammarToJSON_lambda_batch = (
+    input: Record<string, GrammarParserBatchInputEntry>,
+  ): Promise<{
+    errors?: Record<string, PlainObject<V1_ParserError>> | undefined;
+    result?: Record<string, PlainObject<V1_RawLambda>> | undefined;
+  }> =>
+    this.postWithTracing(
+      this.getTraceData(CORE_ENGINE_TRACER_SPAN.GRAMMAR_TO_JSON),
+      `${this._grammarToJSON()}/lambda/batch`,
       input,
       {},
       undefined,
@@ -156,12 +202,39 @@ export class V1_EngineServerClient extends AbstractServerClient {
       { enableCompression: true },
     );
 
-  transformRelationalOperationElementGrammarToJSON = (
-    input: PlainObject<V1_RelationalOperationElementGrammarToJsonInput>,
-  ): Promise<PlainObject<V1_RelationalOperationElementJsonToGrammarInput>> =>
+  grammarToJSON_relationalOperationElement = (
+    input: string,
+    sourceId?: string | undefined,
+    lineOffset?: number | undefined,
+    columnOffset?: number | undefined,
+    returnSourceInformation?: boolean | undefined,
+  ): Promise<PlainObject<V1_RawRelationalOperationElement>> =>
     this.postWithTracing(
       this.getTraceData(CORE_ENGINE_TRACER_SPAN.GRAMMAR_TO_JSON),
-      `${this._pure()}/grammar/transformRelationalOperationElementGrammarToJson`,
+      `${this._grammarToJSON()}/relationalOperationElement`,
+      input,
+      {},
+      undefined,
+      {
+        sourceId,
+        lineOffset,
+        columnOffset,
+        returnSourceInformation,
+      },
+      { enableCompression: true },
+    );
+
+  grammarToJSON_relationalOperationElement_batch = (
+    input: Record<string, GrammarParserBatchInputEntry>,
+  ): Promise<{
+    errors?: Record<string, PlainObject<V1_ParserError>> | undefined;
+    result?:
+      | Record<string, PlainObject<V1_RawRelationalOperationElement>>
+      | undefined;
+  }> =>
+    this.postWithTracing(
+      this.getTraceData(CORE_ENGINE_TRACER_SPAN.GRAMMAR_TO_JSON),
+      `${this._grammarToJSON()}/relationalOperationElement/batch`,
       input,
       {},
       undefined,
@@ -169,16 +242,75 @@ export class V1_EngineServerClient extends AbstractServerClient {
       { enableCompression: true },
     );
 
-  transformRelationalOperationElementJSONToGrammar = (
-    input: PlainObject<V1_RelationalOperationElementJsonToGrammarInput>,
-  ): Promise<PlainObject<V1_RelationalOperationElementGrammarToJsonInput>> =>
+  _JSONToGrammar = (): string => `${this._pure()}/grammar/jsonToGrammar`;
+
+  JSONToGrammar_model = (
+    input: PlainObject<V1_PureModelContextData>,
+    renderStyle?: V1_RenderStyle | undefined,
+  ): Promise<string> =>
     this.postWithTracing(
       this.getTraceData(CORE_ENGINE_TRACER_SPAN.JSON_TO_GRAMMAR),
-      `${this._pure()}/grammar/transformRelationalOperationElementJsonToGrammar`,
+      `${this._JSONToGrammar()}/model`,
+      input,
+      {},
+      { Accept: ContentType.TEXT_PLAIN },
+      { renderStyle },
+      { enableCompression: true },
+    );
+
+  JSONToGrammar_lambda = (
+    input: PlainObject<V1_RawLambda>,
+    renderStyle?: V1_RenderStyle | undefined,
+  ): Promise<string> =>
+    this.postWithTracing(
+      this.getTraceData(CORE_ENGINE_TRACER_SPAN.JSON_TO_GRAMMAR),
+      `${this._JSONToGrammar()}/lambda`,
       input,
       {},
       undefined,
+      { renderStyle },
+      { enableCompression: true },
+    );
+
+  JSONToGrammar_lambda_batch = (
+    input: Record<string, PlainObject<V1_RawLambda>>,
+    renderStyle?: V1_RenderStyle | undefined,
+  ): Promise<Record<string, string>> =>
+    this.postWithTracing(
+      this.getTraceData(CORE_ENGINE_TRACER_SPAN.JSON_TO_GRAMMAR),
+      `${this._JSONToGrammar()}/lambda/batch`,
+      input,
+      {},
       undefined,
+      { renderStyle },
+      { enableCompression: true },
+    );
+
+  JSONToGrammar_relationalOperationElement = (
+    input: PlainObject<V1_RawRelationalOperationElement>,
+    renderStyle?: V1_RenderStyle | undefined,
+  ): Promise<string> =>
+    this.postWithTracing(
+      this.getTraceData(CORE_ENGINE_TRACER_SPAN.JSON_TO_GRAMMAR),
+      `${this._JSONToGrammar()}/relationalOperationElement`,
+      input,
+      {},
+      undefined,
+      { renderStyle },
+      { enableCompression: true },
+    );
+
+  JSONToGrammar_relationalOperationElement_batch = (
+    input: Record<string, PlainObject<V1_RawRelationalOperationElement>>,
+    renderStyle?: V1_RenderStyle | undefined,
+  ): Promise<Record<string, string>> =>
+    this.postWithTracing(
+      this.getTraceData(CORE_ENGINE_TRACER_SPAN.JSON_TO_GRAMMAR),
+      `${this._JSONToGrammar()}/relationalOperationElement/batch`,
+      input,
+      {},
+      undefined,
+      { renderStyle },
       { enableCompression: true },
     );
 

packages/legend-manual-tests/src/__tests__/roundtrip-grammar-mismatch/RoundtripGrammarMismatch.test.ts
@@ -22,8 +22,10 @@ import {
   TEST__buildGraphWithEntities,
   TEST__getTestGraphManagerState,
   TEST__GraphPluginManager,
+  type V1_PureModelContextData,
 } from '@finos/legend-graph';
 import { getLegendGraphExtensionCollection } from '@finos/legend-graph-extension-collection';
+import { ContentType, type PlainObject } from '@finos/legend-shared';
 
 const engineConfig = JSON.parse(
   fs.readFileSync(resolve(__dirname, '../../../engine-config.json'), {
@@ -121,16 +123,19 @@ const checkGrammarRoundtripMismatch = async (
 
   const transformGrammarToJsonResult = await axios.post<
     unknown,
-    AxiosResponse<{ modelDataContext: unknown }>
-  >(
-    `${ENGINE_SERVER_URL}/pure/v1/grammar/transformGrammarToJson`,
-    {
-      code: grammarBefore,
+    AxiosResponse<PlainObject<V1_PureModelContextData>>
+  >(`${ENGINE_SERVER_URL}/pure/v1/grammar/grammarToJson/model`, grammarBefore, {
+    headers: {
+      'Content-Type': ContentType.TEXT_PLAIN,
     },
-    {},
-  );
+    // TODO: we should enable this, but we need to make sure engine works first
+    // See https://github.com/finos/legend-engine/pull/692
+    // params: {
+    //   returnSourceInformation: false,
+    // },
+  });
   const entities = graphManagerState.graphManager.pureProtocolTextToEntities(
-    JSON.stringify(transformGrammarToJsonResult.data.modelDataContext),
+    JSON.stringify(transformGrammarToJsonResult.data),
   );
   await TEST__buildGraphWithEntities(graphManagerState, entities, {
     TEMPORARY__preserveSectionIndex: false,
@@ -155,17 +160,21 @@ const checkGrammarRoundtripMismatch = async (
   };
   const transformJsonToGrammarResult = await axios.post<
     unknown,
-    AxiosResponse<{ code: string }>
+    AxiosResponse<string>
   >(
-    `${ENGINE_SERVER_URL}/pure/v1/grammar/transformJsonToGrammar`,
+    `${ENGINE_SERVER_URL}/pure/v1/grammar/jsonToGrammar/model`,
+    modelDataContext,
     {
-      modelDataContext,
-      renderStyle: 'STANDARD',
+      headers: {
+        Accept: ContentType.TEXT_PLAIN,
+      },
+      params: {
+        renderStyle: 'STANDARD',
+      },
     },
-    {},
   );
   if (!excludes.includes(phase)) {
-    expect(transformJsonToGrammarResult.data.code).toEqual(grammarAfter);
+    expect(transformJsonToGrammarResult.data).toEqual(grammarAfter);
     logSuccess(phase, options?.debug);
   }
 

packages/legend-manual-tests/src/__tests__/roundtrip-grammar/ProfilingGraphBuilding.test.ts
@@ -19,13 +19,20 @@
 import { resolve } from 'path';
 import fs from 'fs';
 import axios, { type AxiosResponse } from 'axios';
-import { WebConsole, Log, LogEvent } from '@finos/legend-shared';
+import {
+  WebConsole,
+  Log,
+  LogEvent,
+  ContentType,
+  type PlainObject,
+} from '@finos/legend-shared';
 import {
   TEST__GraphPluginManager,
   TEST__buildGraphWithEntities,
   TEST__getTestGraphManagerState,
   GRAPH_MANAGER_EVENT,
   V1_ENGINE_EVENT,
+  type V1_PureModelContextData,
 } from '@finos/legend-graph';
 import { getLegendGraphExtensionCollection } from '@finos/legend-graph-extension-collection';
 
@@ -167,14 +174,17 @@ const profileRoundtrip = async (
   let startTime = Date.now();
   const transformGrammarToJsonResult = await axios.post<
     unknown,
-    AxiosResponse<{ modelDataContext: { elements: object[] } }>
-  >(
-    `${ENGINE_SERVER_URL}/pure/v1/grammar/transformGrammarToJson`,
-    {
-      code: grammarText,
+    AxiosResponse<PlainObject<V1_PureModelContextData>>
+  >(`${ENGINE_SERVER_URL}/pure/v1/grammar/grammarToJson/model`, grammarText, {
+    headers: {
+      'Content-Type': ContentType.TEXT_PLAIN,
     },
-    {},
-  );
+    // TODO: we should enable this, but we need to make sure engine works first
+    // See https://github.com/finos/legend-engine/pull/692
+    // params: {
+    //   returnSourceInformation: false,
+    // },
+  });
   if (options.debug) {
     log.info(
       LogEvent.create(V1_ENGINE_EVENT.GRAMMAR_TO_JSON),
@@ -183,7 +193,7 @@ const profileRoundtrip = async (
     );
   }
   const entities = graphManagerState.graphManager.pureProtocolTextToEntities(
-    JSON.stringify(transformGrammarToJsonResult.data.modelDataContext),
+    JSON.stringify(transformGrammarToJsonResult.data),
   );
   if (options.debug) {
     log.info(
@@ -239,13 +249,17 @@ const profileRoundtrip = async (
       .map((entity) => entity.content),
   };
   startTime = Date.now();
-  await axios.post<unknown, AxiosResponse<{ code: string }>>(
-    `${ENGINE_SERVER_URL}/pure/v1/grammar/transformJsonToGrammar`,
+  await axios.post<unknown, AxiosResponse<string>>(
+    `${ENGINE_SERVER_URL}/pure/v1/grammar/jsonToGrammar/model`,
+    modelDataContext,
     {
-      modelDataContext,
-      renderStyle: 'STANDARD',
+      headers: {
+        Accept: ContentType.TEXT_PLAIN,
+      },
+      params: {
+        renderStyle: 'STANDARD',
+      },
     },
-    {},
   );
   if (options.debug) {
     log.info(

packages/legend-manual-tests/src/__tests__/roundtrip-grammar/RoundtripGrammar.test.ts
@@ -23,6 +23,7 @@ import {
   WebConsole,
   Log,
   LogEvent,
+  ContentType,
 } from '@finos/legend-shared';
 import {
   type V1_PackageableElement,
@@ -157,14 +158,17 @@ const checkGrammarRoundtrip = async (
   let startTime = Date.now();
   const transformGrammarToJsonResult = await axios.post<
     unknown,
-    AxiosResponse<{ modelDataContext: { elements: object[] } }>
-  >(
-    `${ENGINE_SERVER_URL}/pure/v1/grammar/transformGrammarToJson`,
-    {
-      code: grammarText,
+    AxiosResponse<{ elements: object[] }>
+  >(`${ENGINE_SERVER_URL}/pure/v1/grammar/grammarToJson/model`, grammarText, {
+    headers: {
+      'Content-Type': ContentType.TEXT_PLAIN,
     },
-    {},
-  );
+    // TODO: we should enable this, but we need to make sure engine works first
+    // See https://github.com/finos/legend-engine/pull/692
+    // params: {
+    //   returnSourceInformation: false,
+    // },
+  });
   if (options?.debug) {
     log.info(
       LogEvent.create(V1_ENGINE_EVENT.GRAMMAR_TO_JSON),
@@ -173,7 +177,7 @@ const checkGrammarRoundtrip = async (
     );
   }
   const entities = graphManagerState.graphManager.pureProtocolTextToEntities(
-    JSON.stringify(transformGrammarToJsonResult.data.modelDataContext),
+    JSON.stringify(transformGrammarToJsonResult.data),
   );
   if (options?.debug) {
     log.info(
@@ -216,7 +220,10 @@ const checkGrammarRoundtrip = async (
         .map(graphManagerState.graphManager.pruneSourceInformation),
     ).toIncludeSameMembers(
       // expected: protocol JSON parsed from grammar text
-      transformGrammarToJsonResult.data.modelDataContext.elements
+      (
+        (transformGrammarToJsonResult.data as { elements: object[] })
+          .elements as PlainObject<V1_PackageableElement>[]
+      )
         .map(graphManagerState.graphManager.pruneSourceInformation)
         .filter(
           (elementProtocol: PlainObject<V1_PackageableElement>) =>
@@ -256,14 +263,18 @@ const checkGrammarRoundtrip = async (
   startTime = Date.now();
   const transformJsonToGrammarResult = await axios.post<
     unknown,
-    AxiosResponse<{ code: string }>
+    AxiosResponse<string>
   >(
-    `${ENGINE_SERVER_URL}/pure/v1/grammar/transformJsonToGrammar`,
+    `${ENGINE_SERVER_URL}/pure/v1/grammar/jsonToGrammar/model`,
+    modelDataContext,
     {
-      modelDataContext,
-      renderStyle: 'STANDARD',
+      headers: {
+        Accept: ContentType.TEXT_PLAIN,
+      },
+      params: {
+        renderStyle: 'STANDARD',
+      },
     },
-    {},
   );
   if (options?.debug) {
     log.info(
@@ -273,7 +284,7 @@ const checkGrammarRoundtrip = async (
     );
   }
   if (!excludes.includes(phase)) {
-    expect(transformJsonToGrammarResult.data.code).toEqual(grammarText);
+    expect(transformJsonToGrammarResult.data).toEqual(grammarText);
     logSuccess(phase, log, options?.debug);
   }
 

packages/legend-graph/src/MetaModelUtils.ts
@@ -34,6 +34,7 @@ import {
   hashArray,
   hashObject,
   hashString,
+  recursiveOmit,
 } from '@finos/legend-shared';
 
 export const extractElementNameFromPath = (fullPath: string): string =>
@@ -85,6 +86,20 @@ export const isValidPath = (path: string): boolean =>
 export const fromElementPathToMappingElementId = (className: string): string =>
   className.split(ELEMENT_PATH_DELIMITER).join('_');
 
+/**
+ * Prune source information from object such as raw lambda, raw value specification, etc.
+ *
+ * NOTE: currently, there is no exhaustive way to do this. Majority of the cases, the source information field
+ * is named `sourceInformation`, however, we have sometimes deviated from this pattern in our protocol model,
+ * so we have fields like `classSourceInformation`, etc. So this is really an optimistic, non-exhaustive prune.
+ * To do this exhaustively, we might need to make use of engine grammar API endpoints or do a full roundtrip
+ * raw object-protocol transformation to prune unrecognized fields, which include source information fields.
+ */
+export const pruneSourceInformation = (
+  object: Record<PropertyKey, unknown>,
+): Record<PropertyKey, unknown> =>
+  recursiveOmit(object, [SOURCE_INFORMATION_KEY]);
+
 // -------------------------------- HASHING -------------------------------------
 // TODO: this should be moved after we refactor `hashing` out of metamodels
 
@@ -93,7 +108,8 @@ export const hashObjectWithoutSourceInformation = (val: object): string =>
   hashObject(val, {
     excludeKeys: (key: string) => key === SOURCE_INFORMATION_KEY,
   });
-export const hashLambda = (
+
+export const hashRawLambda = (
   parameters: object | undefined,
   body: object | undefined,
 ): string =>

packages/legend-graph/src/__tests__/MetaModelUtils.test.ts
@@ -18,7 +18,7 @@ import {
   extractElementNameFromPath,
   fromElementPathToMappingElementId,
   matchFunctionName,
-  hashLambda,
+  hashRawLambda,
   isValidFullPath,
   isValidPath,
   isValidPathIdentifier,
@@ -66,11 +66,11 @@ test(
       parameters: [{ b: 2 }, { a: 1 }],
       body: { a: 3 },
     };
-    expect(hashLambda(lambda1.parameters, lambda1.body)).toEqual(
-      hashLambda(lambda2.parameters, lambda2.body),
+    expect(hashRawLambda(lambda1.parameters, lambda1.body)).toEqual(
+      hashRawLambda(lambda2.parameters, lambda2.body),
     );
-    expect(hashLambda(lambda1.parameters, lambda1.body)).not.toEqual(
-      hashLambda(lambda3.parameters, lambda3.body),
+    expect(hashRawLambda(lambda1.parameters, lambda1.body)).not.toEqual(
+      hashRawLambda(lambda3.parameters, lambda3.body),
     );
   },
 );

commit f6620539c0be0061ea4fa2ff7480b6fe6c1fa42e

fix a regression introduced by #960 where empty grammar causes error in text-mode

packages/legend-graph/src/models/protocols/pure/v1/engine/V1_EngineServerClient.ts
@@ -20,6 +20,7 @@ import {
   type PlainObject,
   type ServerClientConfig,
   type TraceData,
+  HttpHeader,
 } from '@finos/legend-shared';
 import type { ImportMode } from '../../../../../graphManager/action/generation/ImportConfigurationDescription';
 import type { V1_PureModelContextData } from '../model/context/V1_PureModelContextData';
@@ -155,7 +156,9 @@ export class V1_EngineServerClient extends AbstractServerClient {
       `${this._grammarToJSON()}/model`,
       input,
       {},
-      undefined,
+      {
+        [HttpHeader.CONTENT_TYPE]: ContentType.TEXT_PLAIN,
+      },
       {
         sourceId,
         lineOffset,
@@ -176,7 +179,9 @@ export class V1_EngineServerClient extends AbstractServerClient {
       `${this._grammarToJSON()}/lambda`,
       input,
       {},
-      undefined,
+      {
+        [HttpHeader.CONTENT_TYPE]: ContentType.TEXT_PLAIN,
+      },
       {
         sourceId,
         lineOffset,
@@ -214,7 +219,9 @@ export class V1_EngineServerClient extends AbstractServerClient {
       `${this._grammarToJSON()}/relationalOperationElement`,
       input,
       {},
-      undefined,
+      {
+        [HttpHeader.CONTENT_TYPE]: ContentType.TEXT_PLAIN,
+      },
       {
         sourceId,
         lineOffset,
@@ -253,7 +260,7 @@ export class V1_EngineServerClient extends AbstractServerClient {
       `${this._JSONToGrammar()}/model`,
       input,
       {},
-      { Accept: ContentType.TEXT_PLAIN },
+      { [HttpHeader.ACCPEPT]: ContentType.TEXT_PLAIN },
       { renderStyle },
       { enableCompression: true },
     );
@@ -489,7 +496,7 @@ export class V1_EngineServerClient extends AbstractServerClient {
       `${this._execution()}/testDataGeneration/generateTestData_WithDefaultSeed`,
       input,
       {},
-      { Accept: ContentType.TEXT_PLAIN },
+      { [HttpHeader.ACCPEPT]: ContentType.TEXT_PLAIN },
       undefined,
       { enableCompression: true },
     );

packages/legend-server-sdlc/src/SDLCServerClient.ts
@@ -33,6 +33,7 @@ import {
   AbstractServerClient,
   ContentType,
   guaranteeNonNullable,
+  HttpHeader,
 } from '@finos/legend-shared';
 import type { Entity } from '@finos/legend-model-storage';
 import type {
@@ -477,7 +478,7 @@ export class SDLCServerClient extends AbstractServerClient {
         workflowJob.id,
       )}/logs`,
       {},
-      { Accept: ContentType.TEXT_PLAIN },
+      { [HttpHeader.ACCPEPT]: ContentType.TEXT_PLAIN },
     );
   cancelWorkflowJob = (
     projectId: string,
@@ -612,7 +613,7 @@ export class SDLCServerClient extends AbstractServerClient {
         workflowJob.id,
       )}/logs`,
       {},
-      { Accept: ContentType.TEXT_PLAIN },
+      { [HttpHeader.ACCPEPT]: ContentType.TEXT_PLAIN },
     );
   cancelWorkflowJobByVersion = (
     projectId: string,

packages/legend-shared/src/network/NetworkUtils.ts
@@ -42,6 +42,11 @@ const compressData = (data: Record<PropertyKey, unknown> | string): Blob =>
 export const HttpStatus = StatusCodes;
 export const CHARSET = 'charset=utf-8';
 
+export enum HttpHeader {
+  CONTENT_TYPE = 'Content-Type',
+  ACCPEPT = 'Accept',
+}
+
 export enum ContentType {
   APPLICATION_JSON = 'application/json',
   APPLICATION_XML = 'application/xml',
@@ -290,8 +295,8 @@ export const createRequestHeaders = (
   baseRequestHeaders.Accept = ContentType.APPLICATION_JSON;
   if (method !== HttpMethod.GET) {
     baseRequestHeaders[
-      'Content-Type'
-    ] = `${ContentType.APPLICATION_JSON}; ${CHARSET}`;
+      HttpHeader.CONTENT_TYPE
+    ] = `${ContentType.APPLICATION_JSON};${CHARSET}`;
   }
   return mergeRequestHeaders(baseRequestHeaders, headers);
 };
@@ -412,17 +417,17 @@ export class NetworkClient {
     responseProcessConfig?: ResponseProcessConfig | undefined,
   ): Promise<T> {
     const requestUrl = makeUrl(this.baseUrl, url, parameters ?? {});
-    if (data && requestProcessConfig?.enableCompression) {
+    if (data !== undefined && requestProcessConfig?.enableCompression) {
       assertTrue(
         method !== HttpMethod.GET,
         ' GET request should not have any request payload',
       );
       data = compressData(data as Record<PropertyKey, unknown> | string);
       // NOTE: do not use Content-Type for GET to avoid unnecessary pre-flight when cross-origin
-      headers = mergeRequestHeaders(
-        { 'Content-Type': `${ContentType.APPLICATION_ZLIB};${CHARSET}` },
-        headers,
-      );
+      headers = mergeRequestHeaders(headers, {
+        // Override Content-Type header when compression is enabled
+        [HttpHeader.CONTENT_TYPE]: `${ContentType.APPLICATION_ZLIB};${CHARSET}`,
+      });
     }
     let body: Blob | string | undefined;
     if (data !== undefined) {

packages/legend-tracer-extension-zipkin/src/ZipkinTracerPlugin.ts
@@ -26,6 +26,9 @@ import {
   guaranteeNonNullable,
   isNonNullable,
   TracerServicePlugin,
+  HttpHeader,
+  ContentType,
+  CHARSET,
 } from '@finos/legend-shared';
 
 interface ZipkinTracerPluginConfigData {
@@ -68,8 +71,8 @@ export class ZipkinTracerPlugin extends TracerServicePlugin<ZipkinSpan> {
               credentials: 'include', // allow sending credentials to other domain
               redirect: 'manual', // avoid following authentication redirects
               headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-                Accept: 'application/json',
+                [HttpHeader.CONTENT_TYPE]: `${ContentType.APPLICATION_JSON};${CHARSET}`,
+                [HttpHeader.ACCPEPT]: ContentType.APPLICATION_JSON,
               },
             }),
           // eslint-disable-next-line @typescript-eslint/no-explicit-any

packages/legend-manual-tests/src/__tests__/roundtrip-grammar-mismatch/RoundtripGrammarMismatch.test.ts
@@ -25,7 +25,11 @@ import {
   type V1_PureModelContextData,
 } from '@finos/legend-graph';
 import { getLegendGraphExtensionCollection } from '@finos/legend-graph-extension-collection';
-import { ContentType, type PlainObject } from '@finos/legend-shared';
+import {
+  ContentType,
+  HttpHeader,
+  type PlainObject,
+} from '@finos/legend-shared';
 
 const engineConfig = JSON.parse(
   fs.readFileSync(resolve(__dirname, '../../../engine-config.json'), {
@@ -126,7 +130,7 @@ const checkGrammarRoundtripMismatch = async (
     AxiosResponse<PlainObject<V1_PureModelContextData>>
   >(`${ENGINE_SERVER_URL}/pure/v1/grammar/grammarToJson/model`, grammarBefore, {
     headers: {
-      'Content-Type': ContentType.TEXT_PLAIN,
+      [HttpHeader.CONTENT_TYPE]: ContentType.TEXT_PLAIN,
     },
     // TODO: we should enable this, but we need to make sure engine works first
     // See https://github.com/finos/legend-engine/pull/692
@@ -166,7 +170,7 @@ const checkGrammarRoundtripMismatch = async (
     modelDataContext,
     {
       headers: {
-        Accept: ContentType.TEXT_PLAIN,
+        [HttpHeader.ACCPEPT]: ContentType.TEXT_PLAIN,
       },
       params: {
         renderStyle: 'STANDARD',

packages/legend-manual-tests/src/__tests__/roundtrip-grammar/ProfilingGraphBuilding.test.ts
@@ -25,6 +25,7 @@ import {
   LogEvent,
   ContentType,
   type PlainObject,
+  HttpHeader,
 } from '@finos/legend-shared';
 import {
   TEST__GraphPluginManager,
@@ -177,7 +178,7 @@ const profileRoundtrip = async (
     AxiosResponse<PlainObject<V1_PureModelContextData>>
   >(`${ENGINE_SERVER_URL}/pure/v1/grammar/grammarToJson/model`, grammarText, {
     headers: {
-      'Content-Type': ContentType.TEXT_PLAIN,
+      [HttpHeader.CONTENT_TYPE]: ContentType.TEXT_PLAIN,
     },
     // TODO: we should enable this, but we need to make sure engine works first
     // See https://github.com/finos/legend-engine/pull/692
@@ -254,7 +255,7 @@ const profileRoundtrip = async (
     modelDataContext,
     {
       headers: {
-        Accept: ContentType.TEXT_PLAIN,
+        [HttpHeader.ACCPEPT]: ContentType.TEXT_PLAIN,
       },
       params: {
         renderStyle: 'STANDARD',

packages/legend-manual-tests/src/__tests__/roundtrip-grammar/RoundtripGrammar.test.ts
@@ -24,6 +24,7 @@ import {
   Log,
   LogEvent,
   ContentType,
+  HttpHeader,
 } from '@finos/legend-shared';
 import {
   type V1_PackageableElement,
@@ -160,7 +161,7 @@ const checkGrammarRoundtrip = async (
     AxiosResponse<{ elements: object[] }>
   >(`${ENGINE_SERVER_URL}/pure/v1/grammar/grammarToJson/model`, grammarText, {
     headers: {
-      'Content-Type': ContentType.TEXT_PLAIN,
+      [HttpHeader.CONTENT_TYPE]: ContentType.TEXT_PLAIN,
     },
     // TODO: we should enable this, but we need to make sure engine works first
     // See https://github.com/finos/legend-engine/pull/692
@@ -268,7 +269,7 @@ const checkGrammarRoundtrip = async (
     modelDataContext,
     {
       headers: {
-        Accept: ContentType.TEXT_PLAIN,
+        [HttpHeader.ACCPEPT]: ContentType.TEXT_PLAIN,
       },
       params: {
         renderStyle: 'STANDARD',