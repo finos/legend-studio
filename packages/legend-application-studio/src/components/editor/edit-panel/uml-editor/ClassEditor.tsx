1bea00391c4009ca01a2a12342ea64ddde7816ce
xannem <41248956+xannem@users.noreply.github.com>
2022-07-26 23:55:01 -0400

    test dnd class property

--- packages/legend-application-studio/src/components/editor/edit-panel/uml-editor/ClassEditor.tsx
+++ packages/legend-application-studio/src/components/editor/edit-panel/uml-editor/ClassEditor.tsx
@@ -153,6 +153,7 @@ const PropertyBasicEditor = observer(
       _class.properties.filter((p) => p.name === val.name).length >= 2;
     const selectProperty = (): void =>
       editorState.setSelectedProperty(property);
+    // const dragProperty = (): void => editorState.setSelectedProperty(property);
     // Name
     const changeValue: React.ChangeEventHandler<HTMLInputElement> = (event) => {
       property_setName(property, event.target.value);
@@ -303,7 +304,7 @@ const PropertyBasicEditor = observer(
               value={property.name}
               spellCheck={false}
               onChange={changeValue}
-              placeholder={`Property name`}
+              placeholder={`Property namesvp`}
               validationErrorMessage={
                 isPropertyDuplicated(property)
                   ? 'Duplicated property'
