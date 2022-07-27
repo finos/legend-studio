90c3cb2b498c617f02317421b1dc662d10893042
An Phi <aphi@skidmore.edu>
2022-07-26 21:41:28 -0400

    refactor: update plugin and preset names

--- packages/legend-application-studio-bootstrap/scripts/setup.js
+++ packages/legend-application-studio-bootstrap/scripts/setup.js
@@ -74,7 +74,7 @@ export const setup = (outputDir) => {
           ],
         },
         extensions: {
-          '@finos/legend-plugin-external-language-morphir-application-studio': {
+          '@finos/legend-application-studio-plugin-external-language-morphir': {
             visualizer: { url: 'http://localhost:8092' },
             linterServer: { url: 'http://localhost:8091' },
             linterApp: { url: 'http://localhost:8090' },
