b8d6757267acaf57e8cd48d83cea92b64afa51c2
xannem <41248956+xannem@users.noreply.github.com>
2022-07-29 11:14:04 -0400

    feat: allow DnD to rearrange properties

--- packages/legend-application-studio/src/components/editor/edit-panel/uml-editor/ClassEditor.tsx
+++ packages/legend-application-studio/src/components/editor/edit-panel/uml-editor/ClassEditor.tsx
@@ -190,7 +190,6 @@ const PropertyBasicEditor = observer(
       _class.properties.filter((p) => p.name === val.name).length >= 2;
     const selectProperty = (): void =>
       editorState.setSelectedProperty(property);
-    // const dragProperty = (): void => editorState.setSelectedProperty(property);
     // Name
     const changeValue: React.ChangeEventHandler<HTMLInputElement> = (event) => {
       property_setName(property, event.target.value);
