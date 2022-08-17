finos/legend-studio

commit f30a591e75687a52e93faa577731c2f7f372f8bf

query-builder: add support for light theme (#1388)
* component minor cleanups

* minor cleanup post #1383

* temp

* minor cleanups

* bump dependencies

* renamed hashString() to hashValue()

* update CHANGELOG

* update changesets check

* minor fixes

* changesets: do not account for private packages without a version

* minor fixes

* legend-query: add support for light theme

* bump dependencies

* query builder: default to graph fetch mode

* query builder: fix a small problem with switching between fetch modes

* query builder: make the projection panel error more subtle

* query builder: minor fix for dataspace query editor

* cleanup post #1370

* enhance project command search

* cleanup and fix problem with DnD overlay

* cleanup BlankPanelPlaceholder props

* minor cleanup post #1395 (part 1)

* query editor: better handle the case when a query is created without a class

* query-builder: minor styling fixes

* clean up HACKY__ methods in AbstractPureGraphManager

* query: add a config to enable/disable beta theme switcher

* simplify naming of DND_TYPE

* minor fixes

* cleanup post #1395 (part 2)

* create an use a new component for DnD dropzone placeholder

* more cleanups

* standardize usage of DnD overlay

* improve TS usage for react-dnd

* bump dependencies

packages/legend-extension-application-studio-query-builder/src/components/QueryBuilder_LegendStudioApplicationPlugin.tsx
@@ -221,6 +221,11 @@ export class QueryBuilder_LegendStudioApplicationPlugin
                 );
                 if (queryBuilderExtension.mode) {
                   queryBuilderExtension.queryBuilderState.changeClass(element);
+                  queryBuilderExtension.queryBuilderState.initialize(
+                    editorStore.graphManagerState.graphManager.createGetAllRawLambda(
+                      element,
+                    ),
+                  );
                 }
               },
             );
@@ -326,6 +331,11 @@ export class QueryBuilder_LegendStudioApplicationPlugin
                   if (queryBuilderExtension.mode) {
                     queryBuilderExtension.queryBuilderState.changeClass(
                       classView.class.value,
+                    );
+                    queryBuilderExtension.queryBuilderState.initialize(
+                      diagramEditorState.editorStore.graphManagerState.graphManager.createGetAllRawLambda(
+                        classView.class.value,
+                      ),
                     );
                   }
                 },

commit 5bbe03948951d1dee21c8ad3e0a28c0cc751f880

Fix a regression introduced for milestoning in #1388

packages/legend-extension-application-studio-query-builder/src/components/QueryBuilder_LegendStudioApplicationPlugin.tsx
@@ -220,12 +220,12 @@ export class QueryBuilder_LegendStudioApplicationPlugin
                   }),
                 );
                 if (queryBuilderExtension.mode) {
-                  queryBuilderExtension.queryBuilderState.changeClass(element);
                   queryBuilderExtension.queryBuilderState.initialize(
                     editorStore.graphManagerState.graphManager.createGetAllRawLambda(
                       element,
                     ),
                   );
+                  queryBuilderExtension.queryBuilderState.changeClass(element);
                 }
               },
             );
@@ -329,13 +329,13 @@ export class QueryBuilder_LegendStudioApplicationPlugin
                     }),
                   );
                   if (queryBuilderExtension.mode) {
-                    queryBuilderExtension.queryBuilderState.changeClass(
-                      classView.class.value,
-                    );
                     queryBuilderExtension.queryBuilderState.initialize(
                       diagramEditorState.editorStore.graphManagerState.graphManager.createGetAllRawLambda(
                         classView.class.value,
                       ),
+                    );
+                    queryBuilderExtension.queryBuilderState.changeClass(
+                      classView.class.value,
                     );
                   }
                 },