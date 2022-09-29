finos/legend-studio

commit f30a591e75687a52e93faa577731c2f7f372f8bf

query-builder: add support for light theme (#1388)
* component minor cleanups

* minor cleanup post #1383

* temp

* minor cleanups

* bump dependencies

* renamed hashString() to hashValue()

* update CHANGELOG

* update changesets check

* minor fixes

* changesets: do not account for private packages without a version

* minor fixes

* legend-query: add support for light theme

* bump dependencies

* query builder: default to graph fetch mode

* query builder: fix a small problem with switching between fetch modes

* query builder: make the projection panel error more subtle

* query builder: minor fix for dataspace query editor

* cleanup post #1370

* enhance project command search

* cleanup and fix problem with DnD overlay

* cleanup BlankPanelPlaceholder props

* minor cleanup post #1395 (part 1)

* query editor: better handle the case when a query is created without a class

* query-builder: minor styling fixes

* clean up HACKY__ methods in AbstractPureGraphManager

* query: add a config to enable/disable beta theme switcher

* simplify naming of DND_TYPE

* minor fixes

* cleanup post #1395 (part 2)

* create an use a new component for DnD dropzone placeholder

* more cleanups

* standardize usage of DnD overlay

* improve TS usage for react-dnd

* bump dependencies

packages/legend-graph/src/graphManager/protocol/pure/v1/V1_PureGraphManager.ts
@@ -30,7 +30,6 @@ import {
   getClass,
   guaranteeNonNullable,
   UnsupportedOperationError,
-  assertTrue,
   assertErrorThrown,
   promisify,
   StopWatch,
@@ -1721,7 +1720,7 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
     }
   }
 
-  // ------------------------------------------- ValueSpecification -------------------------------------------
+  // ------------------------------------------- Value Specification -------------------------------------------
 
   buildValueSpecification(
     json: Record<PropertyKey, unknown>,
@@ -1780,7 +1779,41 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
     );
   }
 
+  createGetAllRawLambda(_class: Class): RawLambda {
+    return new RawLambda(
+      [],
+      [
+        {
+          _type: 'func',
+          function: 'getAll',
+          parameters: [
+            {
+              _type: 'packageableElementPtr',
+              fullPath: _class.path,
+            },
+          ],
+        },
+      ],
+    );
+  }
+
+  createDefaultBasicRawLambda(options?: {
+    addDummyParameter?: boolean;
+  }): RawLambda {
+    return new RawLambda(
+      options?.addDummyParameter ? [{ _type: 'var', name: 'x' }] : undefined,
+      [
+        {
+          _type: 'string',
+          multiplicity: { lowerBound: 1, upperBound: 1 },
+          values: [''],
+        },
+      ],
+    );
+  }
+
   // ------------------------------------------- External Format --------------------------------
+
   getAvailableExternalFormatsDescriptions(): Promise<
     ExternalFormatDescription[]
   > {
@@ -1987,7 +2020,7 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
       testDataGenerationExecuteInput,
     );
     testDataGenerationExecuteInput.parameters = parameters;
-    testDataGenerationExecuteInput.hashStrings = Boolean(
+    testDataGenerationExecuteInput.hashValues = Boolean(
       options?.anonymizeGeneratedData,
     );
     return this.engine.generateExecuteTestData(testDataGenerationExecuteInput);
@@ -2343,116 +2376,6 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
     dbBuilderInput.config = config;
     return this.pureModelContextDataToEntities(
       await this.engine.buildDatabase(dbBuilderInput),
-    );
-  }
-
-  // --------------------------------------------- HACKY ---------------------------------------------
-
-  HACKY__createGetAllLambda(_class: Class): RawLambda {
-    return new RawLambda(
-      [],
-      [
-        {
-          _type: 'func',
-          function: 'getAll',
-          parameters: [
-            {
-              _type: 'packageableElementPtr',
-              fullPath: _class.path,
-            },
-          ],
-        },
-      ],
-    );
-  }
-
-  HACKY__createServiceTestAssertLambda(assertData: string): RawLambda {
-    return new RawLambda(
-      [
-        {
-          _type: 'var',
-          class: 'meta::pure::mapping::Result',
-          multiplicity: { lowerBound: 1, upperBound: 1 },
-          name: 'res',
-        },
-      ],
-      [
-        {
-          _type: 'func',
-          function: 'equalJsonStrings',
-          parameters: [
-            {
-              _type: 'func',
-              function: 'toString',
-              parameters: [
-                {
-                  _type: 'func',
-                  function: 'toOne',
-                  parameters: [
-                    {
-                      _type: 'property',
-                      parameters: [
-                        {
-                          _type: 'var',
-                          name: 'res',
-                        },
-                      ],
-                      property: 'values',
-                    },
-                  ],
-                },
-              ],
-            },
-            {
-              _type: 'string',
-              multiplicity: {
-                lowerBound: 1,
-                upperBound: 1,
-              },
-              values: [assertData],
-            },
-          ],
-        },
-      ],
-    );
-  }
-
-  HACKY__extractServiceTestAssertionData(query: RawLambda): string | undefined {
-    let json: string | undefined;
-    try {
-      json = (
-        (
-          ((query.body as unknown[])[0] as Record<PropertyKey, unknown>) // FunctionValue
-            .parameters as unknown[]
-        )[1] as {
-          values: (string | undefined)[];
-        }
-      ).values[0];
-      assertTrue(typeof json === 'string', `Expected value of type 'string'`);
-    } catch (error) {
-      assertErrorThrown(error);
-      this.log.warn(
-        LogEvent.create(GRAPH_MANAGER_EVENT.GRAPH_MANAGER_FAILURE),
-        `Can't extract assertion result`,
-      );
-      json = undefined;
-    }
-    if (!json) {
-      /* Add other assertion cases if we read others */
-    }
-    return json;
-  }
-
-  HACKY__createDefaultBlankLambda(): RawLambda {
-    return new RawLambda(
-      [{ _type: 'var', name: 'x' }],
-      [
-        {
-          _type: 'string',
-          multiplicity: { lowerBound: 1, upperBound: 1 },
-          values: [''],
-        },
-      ],
     );
   }
 

packages/legend-graph/src/graphManager/protocol/pure/v1/engine/execution/V1_ExecuteInput.ts
@@ -64,7 +64,7 @@ export class V1_ExecuteInput {
 
 export class V1_TestDataGenerationExecutionInput extends V1_ExecuteInput {
   parameters: (string | number | boolean)[] = [];
-  hashStrings = false;
+  hashValues = false;
 
   static override readonly serialization = new SerializationFactory(
     createModelSchema(V1_TestDataGenerationExecutionInput, {
@@ -77,7 +77,7 @@ export class V1_TestDataGenerationExecutionInput extends V1_ExecuteInput {
         () => SKIP,
       ),
       context: usingModelSchema(V1_rawBaseExecutionContextModelSchema),
-      hashStrings: primitive(),
+      hashValues: primitive(),
       parameters: list(primitive()),
     }),
   );

commit 16c16ee4ec04cfd71e1838c08e92eb0d63cee578

fix hashStrings bug introduced by #1388

packages/legend-graph/src/graphManager/protocol/pure/v1/V1_PureGraphManager.ts
@@ -2011,7 +2011,7 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
       testDataGenerationExecuteInput,
     );
     testDataGenerationExecuteInput.parameters = parameters;
-    testDataGenerationExecuteInput.hashValues = Boolean(
+    testDataGenerationExecuteInput.hashStrings = Boolean(
       options?.anonymizeGeneratedData,
     );
     return this.engine.generateExecuteTestData(testDataGenerationExecuteInput);

packages/legend-graph/src/graphManager/protocol/pure/v1/engine/execution/V1_ExecuteInput.ts
@@ -64,7 +64,7 @@ export class V1_ExecuteInput {
 
 export class V1_TestDataGenerationExecutionInput extends V1_ExecuteInput {
   parameters: (string | number | boolean)[] = [];
-  hashValues = false;
+  hashStrings = false;
 
   static override readonly serialization = new SerializationFactory(
     createModelSchema(V1_TestDataGenerationExecutionInput, {
@@ -77,7 +77,7 @@ export class V1_TestDataGenerationExecutionInput extends V1_ExecuteInput {
         () => SKIP,
       ),
       context: usingModelSchema(V1_rawBaseExecutionContextModelSchema),
-      hashValues: primitive(),
+      hashStrings: primitive(),
       parameters: list(primitive()),
     }),
   );