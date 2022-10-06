1c81183b31ba56d0a8bac677c085653fd9f9f029
xannem <41248956+xannem@users.noreply.github.com>
2022-10-06 00:50:07 -0400

    platform config header fix

--- packages/legend-server-sdlc/src/models/configuration/Platform.ts
+++ packages/legend-server-sdlc/src/models/configuration/Platform.ts
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2020-present, Goldman Sachs
  *
- * Licensed under the Apache License, platformVersion 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
