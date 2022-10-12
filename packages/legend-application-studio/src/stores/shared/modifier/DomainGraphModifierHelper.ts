finos/legend-studio

commit a6830b4a8677668ab9f23d190ee8dc6316189542

minor improvements (#1543)
* minor improvements

* fix build

* clean changeset

packages/legend-query-builder/src/components/QueryBuilderParametersPanel.tsx
@@ -39,7 +39,6 @@ import {
   MULTIPLICITY_INFINITE,
   PRIMITIVE_TYPE,
   VariableExpression,
-  Multiplicity,
   GenericTypeExplicitReference,
   GenericType,
 } from '@finos/legend-graph';
@@ -347,7 +346,7 @@ export const QueryBuilderParametersPanel = observer(
         const parmaterState = new LambdaParameterState(
           new VariableExpression(
             generateEnumerableNameFromToken(varNames, DEFAULT_VARIABLE_NAME),
-            new Multiplicity(1, 1),
+            queryBuilderState.graphManagerState.graph.getMultiplicity(1, 1),
             GenericTypeExplicitReference.create(
               new GenericType(
                 queryParameterState.queryBuilderState.graphManagerState.graph.getPrimitiveType(

packages/legend-application-studio/src/components/editor/edit-panel/FunctionEditor.tsx
@@ -66,7 +66,6 @@ import {
   MULTIPLICITY_INFINITE,
   Unit,
   Type,
-  Multiplicity,
   Enumeration,
   Class,
   PrimitiveType,
@@ -191,7 +190,7 @@ const ParameterBasicEditor = observer(
       if (!isNaN(lBound) && (uBound === undefined || !isNaN(uBound))) {
         rawVariableExpression_setMultiplicity(
           parameter,
-          new Multiplicity(lBound, uBound),
+          editorStore.graphManagerState.graph.getMultiplicity(lBound, uBound),
         );
       }
     };
@@ -445,7 +444,7 @@ const ReturnTypeEditor = observer(
       if (!isNaN(lBound) && (uBound === undefined || !isNaN(uBound))) {
         function_setReturnMultiplicity(
           functionElement,
-          new Multiplicity(lBound, uBound),
+          editorStore.graphManagerState.graph.getMultiplicity(lBound, uBound),
         );
       }
     };

packages/legend-application-studio/src/components/editor/edit-panel/uml-editor/AssociationEditor.tsx
@@ -64,7 +64,6 @@ import {
   type TaggedValue,
   MULTIPLICITY_INFINITE,
   Profile,
-  Multiplicity,
   Class,
   PrimitiveType,
   Unit,
@@ -166,7 +165,10 @@ const AssociationPropertyBasicEditor = observer(
           ? upper
           : parseInt(upper, 10);
       if (!isNaN(lBound) && (uBound === undefined || !isNaN(uBound))) {
-        property_setMultiplicity(property, new Multiplicity(lBound, uBound));
+        property_setMultiplicity(
+          property,
+          editorStore.graphManagerState.graph.getMultiplicity(lBound, uBound),
+        );
       }
     };
     const changeLowerBound: React.ChangeEventHandler<HTMLInputElement> = (

packages/legend-application-studio/src/components/editor/edit-panel/uml-editor/ClassEditor.tsx
@@ -74,7 +74,6 @@ import {
   Class,
   GenericType,
   Profile,
-  Multiplicity,
   Type,
   PrimitiveType,
   Unit,
@@ -208,7 +207,10 @@ const PropertyBasicEditor = observer(
           ? upper
           : parseInt(upper, 10);
       if (!isNaN(lBound) && (uBound === undefined || !isNaN(uBound))) {
-        property_setMultiplicity(property, new Multiplicity(lBound, uBound));
+        property_setMultiplicity(
+          property,
+          editorStore.graphManagerState.graph.getMultiplicity(lBound, uBound),
+        );
       }
     };
     const changeLowerBound: React.ChangeEventHandler<HTMLInputElement> = (
@@ -538,7 +540,7 @@ const DerivedPropertyBasicEditor = observer(
       if (!isNaN(lBound) && (uBound === undefined || !isNaN(uBound))) {
         property_setMultiplicity(
           derivedProperty,
-          new Multiplicity(lBound, uBound),
+          editorStore.graphManagerState.graph.getMultiplicity(lBound, uBound),
         );
       }
     };

packages/legend-extension-dsl-diagram/src/components/studio/DSL_Diagram_DiagramEditor.tsx
@@ -75,10 +75,10 @@ import {
 } from '@finos/legend-art';
 import {
   type Type,
+  type Multiplicity,
   Class,
   DerivedProperty,
   Property,
-  Multiplicity,
   ELEMENT_PATH_DELIMITER,
   MULTIPLICITY_INFINITE,
   GenericType,
@@ -960,6 +960,7 @@ const DiagramEditorInlinePropertyMultiplicityEditor = observer(
     const [upperBound, setUpperBound] = useState<string | number>(
       value.upperBound ?? MULTIPLICITY_INFINITE,
     );
+    const editorStore = useEditorStore();
     const updateMultiplicity = (
       lower: number | string,
       upper: number | string,
@@ -972,7 +973,9 @@ const DiagramEditorInlinePropertyMultiplicityEditor = observer(
           ? upper
           : parseInt(upper, 10);
       if (!isNaN(lBound) && (uBound === undefined || !isNaN(uBound))) {
-        updateValue(new Multiplicity(lBound, uBound));
+        updateValue(
+          editorStore.graphManagerState.graph.getMultiplicity(lBound, uBound),
+        );
       }
     };
     const changeLowerBound: React.ChangeEventHandler<HTMLInputElement> = (

packages/legend-extension-store-service-store/src/graphManager/protocol/pure/STO_ServiceStore_PureProtocolProcessorPlugin.ts
@@ -72,7 +72,6 @@ import {
   fromElementPathToMappingElementId,
   InferableMappingElementIdImplicitValue,
   InferableMappingElementRootExplicitValue,
-  Multiplicity,
   V1_ElementBuilder,
   V1_initPackageableElement,
   V1_buildFullPath,
@@ -258,7 +257,7 @@ export class STO_ServiceStore_PureProtocolProcessorPlugin
               const mappingProperty = new LocalMappingProperty();
               mappingProperty.type = localMappingProperty.type;
               mappingProperty.name = localMappingProperty.name;
-              const multiplicity = new Multiplicity(
+              const multiplicity = context.graph.getMultiplicity(
                 localMappingProperty.multiplicity.lowerBound,
                 localMappingProperty.multiplicity.upperBound,
               );

packages/legend-graph/src/graphManager/protocol/pure/v1/transformation/pureGraph/to/helpers/V1_MilestoneBuilderHelper.ts
@@ -31,7 +31,6 @@ import { Class } from '../../../../../../../../graph/metamodel/pure/packageableE
 import { DerivedProperty } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/DerivedProperty.js';
 import { GenericType } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/GenericType.js';
 import { GenericTypeExplicitReference } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/GenericTypeReference.js';
-import { Multiplicity } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/Multiplicity.js';
 import { Property } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/Property.js';
 import { V1_Multiplicity } from '../../../../model/packageableElements/domain/V1_Multiplicity.js';
 import type { V1_ValueSpecification } from '../../../../model/valueSpecification/V1_ValueSpecification.js';
@@ -83,7 +82,7 @@ export const V1_buildMilestoningProperties = (
           ];
           const milestonedAllVersions = new Property(
             `${property.name}${MILESTONING_VERSION_PROPERTY_SUFFIX.ALL_VERSIONS}`,
-            new Multiplicity(property.multiplicity.lowerBound, undefined),
+            graph.getMultiplicity(property.multiplicity.lowerBound, undefined),
             GenericTypeExplicitReference.create(
               new GenericType(property.genericType.value.rawType),
             ),
@@ -126,7 +125,7 @@ export const V1_buildMilestoningProperties = (
           ];
           const milestonedAllVersions = new Property(
             `${property.name}${MILESTONING_VERSION_PROPERTY_SUFFIX.ALL_VERSIONS}`,
-            new Multiplicity(property.multiplicity.lowerBound, undefined),
+            graph.getMultiplicity(property.multiplicity.lowerBound, undefined),
             GenericTypeExplicitReference.create(
               new GenericType(property.genericType.value.rawType),
             ),
@@ -172,7 +171,7 @@ export const V1_buildMilestoningProperties = (
           ];
           const milestonedAllVersions = new Property(
             `${property.name}${MILESTONING_VERSION_PROPERTY_SUFFIX.ALL_VERSIONS}`,
-            new Multiplicity(property.multiplicity.lowerBound, undefined),
+            graph.getMultiplicity(property.multiplicity.lowerBound, undefined),
             GenericTypeExplicitReference.create(
               new GenericType(property.genericType.value.rawType),
             ),

packages/legend-graph/src/graphManager/protocol/pure/v1/transformation/pureGraph/to/helpers/V1_ValueSpecificationBuilderHelper.ts
@@ -63,7 +63,7 @@ import {
   PureListInstanceValue,
   CollectionInstanceValue,
 } from '../../../../../../../../graph/metamodel/pure/valueSpecification/InstanceValue.js';
-import { Multiplicity } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/Multiplicity.js';
+import type { Multiplicity } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/Multiplicity.js';
 import type { Type } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/Type.js';
 import { PropertyExplicitReference } from '../../../../../../../../graph/metamodel/pure/packageableElements/domain/PropertyReference.js';
 import { PackageableElementExplicitReference } from '../../../../../../../../graph/metamodel/pure/packageableElements/PackageableElementReference.js';
@@ -168,7 +168,7 @@ export class V1_ValueSpecificationBuilder
   visit_Variable(variable: V1_Variable): ValueSpecification {
     this.openVariables.push(variable.name);
     if (variable.class && variable.multiplicity) {
-      const multiplicity = new Multiplicity(
+      const multiplicity = this.context.graph.getMultiplicity(
         variable.multiplicity.lowerBound,
         variable.multiplicity.upperBound,
       );
@@ -264,7 +264,7 @@ export class V1_ValueSpecificationBuilder
       ),
     );
     const instance = new CollectionInstanceValue(
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -380,7 +380,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.INTEGER,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -392,7 +392,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.DECIMAL,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -404,7 +404,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.STRING,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -416,7 +416,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.BOOLEAN,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -428,7 +428,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.FLOAT,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -440,7 +440,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.DATETIME,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -452,7 +452,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.STRICTDATE,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -464,7 +464,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.STRICTTIME,
       valueSpecification.values,
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),
@@ -476,7 +476,7 @@ export class V1_ValueSpecificationBuilder
       PRIMITIVE_TYPE.LATESTDATE,
       [],
       this.context,
-      new Multiplicity(
+      this.context.graph.getMultiplicity(
         valueSpecification.multiplicity.lowerBound,
         valueSpecification.multiplicity.upperBound,
       ),

commit 1d0b8200f34044f11b993326715697b383b394c4

fix a regression introduced in #1543

packages/legend-application-studio/src/stores/shared/modifier/DomainGraphModifierHelper.ts
@@ -59,7 +59,6 @@ import {
   observe_TaggedValue,
   observe_Constraint,
   observe_GenericType,
-  observe_Multiplicity,
   observe_Type,
   observe_Unit,
   observe_RawLambda,
@@ -194,7 +193,7 @@ export const property_setGenericType = action(
 );
 export const property_setMultiplicity = action(
   (_property: Property | DerivedProperty, value: Multiplicity): void => {
-    _property.multiplicity = observe_Multiplicity(value);
+    _property.multiplicity = value;
   },
 );
 export const stereotypeReference_setValue = action(
@@ -356,7 +355,7 @@ export const function_setReturnType = action(
 );
 export const function_setReturnMultiplicity = action(
   (_func: ConcreteFunctionDefinition, val: Multiplicity): void => {
-    _func.returnMultiplicity = observe_Multiplicity(val);
+    _func.returnMultiplicity = val;
   },
 );
 

packages/legend-application-studio/src/stores/shared/modifier/RawValueSpecificationGraphModifierHelper.ts
@@ -17,7 +17,6 @@ import {
   type Multiplicity,
   type RawVariableExpression,
   type Type,
-  observe_Multiplicity,
   observe_Type,
 } from '@finos/legend-graph';
 import { action } from 'mobx';
@@ -34,6 +33,6 @@ export const rawVariableExpression_setType = action(
 );
 export const rawVariableExpression_setMultiplicity = action(
   (target: RawVariableExpression, value: Multiplicity): void => {
-    target.multiplicity = observe_Multiplicity(value);
+    target.multiplicity = value;
   },
 );

packages/legend-extension-store-service-store/src/graphManager/action/changeDetection/STO_ServiceStore_ObserverHelper.ts
@@ -21,7 +21,6 @@ import {
   skipObservedWithContext,
   observe_PackageableElementReference,
   observe_Abstract_Connection,
-  observe_Multiplicity,
   observe_Abstract_InstanceSetImplementation,
   observe_RawLambda,
   observe_ExternalFormatData,
@@ -359,9 +358,6 @@ export const observe_LocalMappingProperty = skipObserved(
       multiplicity: observable,
       hashCode: computed,
     });
-
-    observe_Multiplicity(metamodel.multiplicity);
-
     return metamodel;
   },
 );

packages/legend-graph/src/graph/metamodel/pure/packageableElements/domain/Multiplicity.ts
@@ -18,8 +18,8 @@ import { CORE_HASH_STRUCTURE } from '../../../../../graph/Core_HashUtils.js';
 import { hashArray, type Hashable } from '@finos/legend-shared';
 
 export class Multiplicity implements Hashable {
-  lowerBound: number;
-  upperBound?: number | undefined;
+  readonly lowerBound: number;
+  readonly upperBound?: number | undefined;
 
   constructor(lowerBound: number, upperBound: number | undefined) {
     this.lowerBound = lowerBound;

packages/legend-graph/src/graphManager/action/changeDetection/CoreObserverHelper.ts
@@ -23,7 +23,6 @@ import {
 } from 'mobx';
 import type { PackageableElement } from '../../../graph/metamodel/pure/packageableElements/PackageableElement.js';
 import type { PackageableElementReference } from '../../../graph/metamodel/pure/packageableElements/PackageableElementReference.js';
-import type { Multiplicity } from '../../../graph/metamodel/pure/packageableElements/domain/Multiplicity.js';
 import type { InferableValue } from '../../../graph/metamodel/pure/InferableValue.js';
 import type { PureGraphManagerPlugin } from '../../PureGraphManagerPlugin.js';
 
@@ -74,15 +73,6 @@ export const observe_PackageableElementReference = skipObserved(
     makeObservable(metamodel, {
       value: observable,
       valueForSerialization: computed,
-    }),
-);
-
-export const observe_Multiplicity = skipObserved(
-  (metamodel: Multiplicity): Multiplicity =>
-    makeObservable(metamodel, {
-      lowerBound: observable,
-      upperBound: observable,
-      hashCode: computed,
     }),
 );
 

packages/legend-graph/src/graphManager/action/changeDetection/DSL_Mapping_ObserverHelper.ts
@@ -91,7 +91,6 @@ import {
   observe_PackageableElementReference,
   skipObserved,
   skipObservedWithContext,
-  observe_Multiplicity,
 } from './CoreObserverHelper.js';
 import {
   observe_EnumValueReference,
@@ -167,7 +166,6 @@ export const observe_LocalMappingPropertyInfo = skipObserved(
     });
 
     observe_PackageableElementReference(metamodel.localMappingPropertyType);
-    observe_Multiplicity(metamodel.localMappingPropertyMultiplicity);
 
     return metamodel;
   },

packages/legend-graph/src/graphManager/action/changeDetection/DomainObserverHelper.ts
@@ -54,7 +54,6 @@ import {
 } from '../../../graph/metamodel/pure/packageableElements/section/Section.js';
 import type { SectionIndex } from '../../../graph/metamodel/pure/packageableElements/section/SectionIndex.js';
 import {
-  observe_Multiplicity,
   observe_Abstract_PackageableElement,
   observe_PackageableElementReference,
   skipObserved,
@@ -336,7 +335,6 @@ export const observe_Property = skipObserved(
     });
 
     observe_GenericTypeReference(metamodel.genericType);
-    observe_Multiplicity(metamodel.multiplicity);
     metamodel.stereotypes.forEach(observe_StereotypeReference);
     metamodel.taggedValues.forEach(observe_TaggedValue);
 
@@ -357,7 +355,6 @@ export const observe_DerivedProperty = skipObserved(
     });
 
     observe_GenericTypeReference(metamodel.genericType);
-    observe_Multiplicity(metamodel.multiplicity);
     metamodel.stereotypes.forEach(observe_StereotypeReference);
     metamodel.taggedValues.forEach(observe_TaggedValue);
 
@@ -466,7 +463,6 @@ export const observe_ConcreteFunctionDefinition = skipObserved(
 
     metamodel.parameters.forEach(observe_RawVariableExpression);
     observe_PackageableElementReference(metamodel.returnType);
-    observe_Multiplicity(metamodel.returnMultiplicity);
     metamodel.stereotypes.forEach(observe_StereotypeReference);
     metamodel.taggedValues.forEach(observe_TaggedValue);
 

packages/legend-graph/src/graphManager/action/changeDetection/RawValueSpecificationObserver.ts
@@ -24,7 +24,6 @@ import type {
 import type { RawVariableExpression } from '../../../graph/metamodel/pure/rawValueSpecification/RawVariableExpression.js';
 import {
   observe_PackageableElementReference,
-  observe_Multiplicity,
   skipObserved,
   skipObservedWithContext,
   type ObserverContext,
@@ -41,7 +40,6 @@ export const observe_RawPrimitiveInstanceValue = skipObserved(
     });
 
     observe_PackageableElementReference(metamodel.type);
-    observe_Multiplicity(metamodel.multiplicity);
 
     return metamodel;
   },
@@ -65,7 +63,6 @@ export const observe_RawVariableExpression = skipObserved(
     });
 
     observe_PackageableElementReference(metamodel.type);
-    observe_Multiplicity(metamodel.multiplicity);
 
     return metamodel;
   },

packages/legend-graph/src/graphManager/action/changeDetection/ValueSpecificationObserver.ts
@@ -60,7 +60,6 @@ import {
   type ObserverContext,
   skipObservedWithContext,
   skipObserved,
-  observe_Multiplicity,
   observe_PackageableElementReference,
 } from './CoreObserverHelper.js';
 import {
@@ -81,7 +80,6 @@ const observe_Abstract_ValueSpecification = (
   if (metamodel.genericType) {
     observe_GenericTypeReference(metamodel.genericType);
   }
-  observe_Multiplicity(metamodel.multiplicity);
 };
 
 export const observe_VariableExpression = skipObserved(
@@ -266,7 +264,6 @@ const observe_FunctionType = skipObserved(
     metamodel.parameters.forEach((parameter) =>
       observe_VariableExpression(parameter),
     );
-    observe_Multiplicity(metamodel.returnMultiplicity);
 
     return metamodel;
   },

packages/legend-query-builder/src/components/QueryBuilderParametersPanel.tsx
@@ -114,6 +114,7 @@ const VariableExpressionEditor = observer(
       event,
     ) => {
       lambdaParameterState.changeMultiplicity(
+        varState,
         parseInt(event.target.value),
         multiplity.upperBound,
       );
@@ -133,6 +134,7 @@ const VariableExpressionEditor = observer(
         !isNaN(parseInt(value, 10))
       ) {
         lambdaParameterState.changeMultiplicity(
+          varState,
           multiplity.lowerBound,
           value === MULTIPLICITY_INFINITE || value === ''
             ? undefined

packages/legend-query-builder/src/stores/shared/LambdaParameterState.ts
@@ -47,9 +47,8 @@ import {
 import { makeObservable, observable, action, computed } from 'mobx';
 import { generateVariableExpressionMockValue } from './ValueSpecificationEditorHelper.js';
 import {
-  multiplicity_setLowerBound,
-  multiplicity_setUpperBound,
   valueSpecification_setGenericType,
+  valueSpecification_setMultiplicity,
 } from './ValueSpecificationModifierHelper.js';
 
 export enum PARAMETER_SUBMIT_ACTION {
@@ -158,6 +157,7 @@ export class LambdaParameterState implements Hashable {
   }
 
   changeMultiplicity(
+    variableExpression: VariableExpression,
     lowerBound: number,
     uppderBound: number | undefined,
   ): void {
@@ -166,8 +166,10 @@ export class LambdaParameterState implements Hashable {
       current.lowerBound !== lowerBound ||
       current.upperBound !== uppderBound
     ) {
-      multiplicity_setLowerBound(current, lowerBound);
-      multiplicity_setUpperBound(current, uppderBound);
+      valueSpecification_setMultiplicity(
+        variableExpression,
+        this.graph.getMultiplicity(lowerBound, uppderBound),
+      );
       this.mockParameterValue();
     }
   }

packages/legend-query-builder/src/stores/shared/ValueSpecificationModifierHelper.ts
@@ -45,15 +45,9 @@ export const valueSpecification_setGenericType = action(
   },
 );
 
-export const multiplicity_setLowerBound = action(
-  (target: Multiplicity, val: number): void => {
-    target.lowerBound = val;
-  },
-);
-
-export const multiplicity_setUpperBound = action(
-  (target: Multiplicity, val: number | undefined): void => {
-    target.upperBound = val;
+export const valueSpecification_setMultiplicity = action(
+  (target: ValueSpecification, val: Multiplicity): void => {
+    target.multiplicity = val;
   },
 );
 