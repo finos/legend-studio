finos/legend-studio

commit 683800ab3ca1752c4382f22bcf8dede42518449d

more application framework improvements (#1552)

packages/legend-application-studio/src/components/editor/edit-panel/data-editor/RelationalCSVDataEditor.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { EDITOR_LANGUAGE } from '@finos/legend-application';
+import { EDITOR_LANGUAGE, TextInputEditor } from '@finos/legend-application';
 import {
   BlankPanelPlaceholder,
   clsx,
@@ -32,7 +32,6 @@ import type { RelationalCSVDataTable } from '@finos/legend-graph';
 import { observer } from 'mobx-react-lite';
 import { forwardRef, useState } from 'react';
 import type { RelationalCSVDataState } from '../../../../stores/editor-state/element-editor-state/data/EmbeddedDataState.js';
-import { StudioTextInputEditor } from '../../../shared/StudioTextInputEditor.js';
 
 const RelationalTableIdentifierModal = observer(
   (props: { dataState: RelationalCSVDataState; isReadOnly: boolean }) => {
@@ -226,7 +225,7 @@ export const RelationalCSVDataEditor = observer(
             <div className="relational-data-editor__content">
               {currentTableState && (
                 <div className="relational-data-editor__values">
-                  <StudioTextInputEditor
+                  <TextInputEditor
                     inputValue={currentTableState.table.values}
                     updateInput={updateTableValues}
                     isReadOnly={isReadOnly}

packages/legend-application-studio/src/components/editor/side-bar/WorkspaceReview.tsx
@@ -30,18 +30,14 @@ import {
   PanelContent,
 } from '@finos/legend-art';
 import { ACTIVITY_MODE } from '../../../stores/EditorConfig.js';
-import { formatDistanceToNow } from 'date-fns';
 import { generateReviewRoute } from '../../../stores/LegendStudioRouter.js';
 import { LEGEND_STUDIO_TEST_ID } from '../../LegendStudioTestID.js';
 import { flowResult } from 'mobx';
 import type { EntityDiff } from '@finos/legend-server-sdlc';
 import { entityDiffSorter } from '../../../stores/EditorSDLCState.js';
 import { useEditorStore } from '../EditorStoreProvider.js';
-import {
-  ActionAlertType,
-  ActionAlertActionType,
-} from '@finos/legend-application';
 import { useLegendStudioApplicationStore } from '../../LegendStudioBaseStoreProvider.js';
+import { formatDistanceToNow } from '@finos/legend-shared';
 
 export const WorkspaceReviewDiffs = observer(() => {
   const editorStore = useEditorStore();
@@ -134,38 +130,12 @@ export const WorkspaceReview = observer(() => {
   };
   const commitReview = (): void => {
     if (workspaceReview && !isDispatchingAction) {
-      const commit = (): void => {
+      editorStore.localChangesState.alertUnsavedChanges((): void => {
         workspaceReviewState.setReviewTitle('');
         flowResult(
           workspaceReviewState.commitWorkspaceReview(workspaceReview),
         ).catch(applicationStore.alertUnhandledError);
-      };
-      if (editorStore.localChangesState.hasUnpushedChanges) {
-        editorStore.setActionAlertInfo({
-          message: 'You have unpushed changes',
-          prompt:
-            'This action will discard these changes and refresh the application',
-          type: ActionAlertType.CAUTION,
-          onEnter: (): void => editorStore.setBlockGlobalHotkeys(true),
-          onClose: (): void => editorStore.setBlockGlobalHotkeys(false),
-          actions: [
-            {
-              label: 'Proceed to commit review',
-              type: ActionAlertActionType.PROCEED_WITH_CAUTION,
-              handler: (): void => {
-                commit();
-              },
-            },
-            {
-              label: 'Abort',
-              type: ActionAlertActionType.PROCEED,
-              default: true,
-            },
-          ],
-        });
-      } else {
-        commit();
-      }
+      });
     }
   };
   const createReview = (): void => {

packages/legend-extension-dsl-service/src/components/studio/ServiceQueryEditor.tsx
@@ -29,7 +29,6 @@ import {
   BlankPanelContent,
   CustomSelectorInput,
   Dialog,
-  ExternalLinkSquareIcon,
   Panel,
   PanelLoadingIndicator,
   PlusIcon,
@@ -39,6 +38,7 @@ import {
   clsx,
   CheckSquareIcon,
   SquareIcon,
+  GitBranchIcon,
 } from '@finos/legend-art';
 import {
   type ProjectServiceQueryUpdaterPathParams,
@@ -52,7 +52,10 @@ import {
   ServiceQueryUpdaterStoreProvider,
   useServiceQueryEditorStore,
 } from './ServiceQueryEditorStoreProvider.js';
-import { QueryBuilder } from '@finos/legend-query-builder';
+import {
+  QueryBuilder,
+  QueryBuilderNavigationBlocker,
+} from '@finos/legend-query-builder';
 import {
   ELEMENT_PATH_DELIMITER,
   extractElementNameFromPath,
@@ -122,7 +125,7 @@ const NewServiceModal = observer(() => {
                 label: 'Open Service',
                 type: ActionAlertActionType.PROCEED,
                 handler: (): void => {
-                  applicationStore.navigator.reloadToLocation(
+                  applicationStore.navigator.goToLocation(
                     generateProjectServiceQueryUpdaterRoute(
                       editorStore.sdlcState.activeProject.projectId,
                       editorStore.sdlcState.activeWorkspace.workspaceId,
@@ -394,7 +397,7 @@ const RegisterServiceModal = observer(() => {
 const ServiceQueryEditorHeaderContent = observer(() => {
   const editorStore = useServiceQueryEditorStore();
   const applicationStore = useLegendStudioApplicationStore();
-  const viewProject = (): void =>
+  const openWorkspace = (): void =>
     applicationStore.navigator.visitAddress(
       applicationStore.navigator.generateAddress(
         generateEditorRoute(
@@ -417,7 +420,7 @@ const ServiceQueryEditorHeaderContent = observer(() => {
       );
     flowResult(
       editorStore.saveWorkspace(serviceEntity, false, (): void => {
-        applicationStore.navigator.reloadToLocation(
+        applicationStore.navigator.goToLocation(
           generateServiceQueryUpdaterRoute(
             editorStore.projectConfigurationEditorState
               .currentProjectConfiguration.groupId,
@@ -466,10 +469,10 @@ const ServiceQueryEditorHeaderContent = observer(() => {
         <button
           className="service-query-editor__header__action service-query-editor__header__action--simple btn--dark"
           tabIndex={-1}
-          title="View project"
-          onClick={viewProject}
+          title="Open workspace"
+          onClick={openWorkspace}
         >
-          <ExternalLinkSquareIcon />
+          <GitBranchIcon className="service-query-editor__header__action__icon--branch" />
         </button>
         <div className="service-query-editor__header__actions__divider" />
         <ServiceQueryEditorReviewAction />
@@ -517,7 +520,12 @@ export const ServiceQueryEditor = observer(() => {
       <div className="service-query-editor__content">
         <PanelLoadingIndicator isLoading={editorStore.initState.isInProgress} />
         {editorStore.queryBuilderState && (
-          <QueryBuilder queryBuilderState={editorStore.queryBuilderState} />
+          <>
+            <QueryBuilderNavigationBlocker
+              queryBuilderState={editorStore.queryBuilderState}
+            />
+            <QueryBuilder queryBuilderState={editorStore.queryBuilderState} />
+          </>
         )}
         {!editorStore.queryBuilderState && (
           <BlankPanelContent>

commit f3fcb9730d00d179dbd6fff78b858022b39df23c

fix a regression introduced by #1552

packages/legend-application-studio/src/components/editor/edit-panel/data-editor/RelationalCSVDataEditor.tsx
@@ -39,10 +39,6 @@ const RelationalTableIdentifierModal = observer(
     const tableIdentifierState = dataState.tableIdentifierState;
     const editableTable = tableIdentifierState.table;
     const closeModal = (): void => dataState.closeModal();
-    const handleSubmit = (): void => {
-      tableIdentifierState.handleSubmit();
-      closeModal();
-    };
     const changeTableValue: React.ChangeEventHandler<HTMLInputElement> = (
       event,
     ) => {
@@ -61,7 +57,11 @@ const RelationalTableIdentifierModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            tableIdentifierState.handleSubmit();
+            closeModal();
+          }}
           className="modal modal--dark search-modal"
         >
           <div className="modal__title">

packages/legend-application-studio/src/components/editor/edit-panel/element-generation-editor/ElementGenerationEditor.tsx
@@ -78,10 +78,6 @@ const NewFileGenerationModal = observer(
         close();
       }
     };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-      event.preventDefault();
-      promoteToFileGeneration().catch(applicationStore.alertUnhandledError);
-    };
     const changeValue: React.ChangeEventHandler<HTMLInputElement> = (event) =>
       setServicePath(event.target.value);
 
@@ -98,7 +94,15 @@ const NewFileGenerationModal = observer(
           },
         }}
       >
-        <form onSubmit={onSubmit} className="modal search-modal modal--dark">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            promoteToFileGeneration().catch(
+              applicationStore.alertUnhandledError,
+            );
+          }}
+          className="modal search-modal modal--dark"
+        >
           <div className="modal__title">
             Promote file generation specification
           </div>

packages/legend-application-studio/src/components/editor/edit-panel/mapping-editor/NewMappingElementModal.tsx
@@ -189,10 +189,6 @@ export const NewMappingElementModal = observer(() => {
       handleClose();
     }
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    handleSubmit();
-  };
   // Title
   const titleText = spec
     ? spec.showTarget
@@ -224,7 +220,10 @@ export const NewMappingElementModal = observer(() => {
     >
       {spec && (
         <form
-          onSubmit={onSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleSubmit();
+          }}
           className="modal search-modal new-mapping-element-modal"
         >
           {titleText && <div className="modal__title">{titleText}</div>}

packages/legend-application-studio/src/components/editor/edit-panel/service-editor/NewServiceModal.tsx
@@ -61,10 +61,6 @@ export const NewServiceModal = observer(
           .catch(applicationStore.alertUnhandledError);
       }
     };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-      event.preventDefault();
-      create();
-    };
     const changeValue: React.ChangeEventHandler<HTMLInputElement> = (event) =>
       setServicePath(event.target.value);
     return (
@@ -80,7 +76,13 @@ export const NewServiceModal = observer(
           },
         }}
       >
-        <form onSubmit={onSubmit} className="modal search-modal modal--dark">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            create();
+          }}
+          className="modal search-modal modal--dark"
+        >
           <div className="modal__title">Promote to Service</div>
           <div className="input-group">
             <input

packages/legend-application-studio/src/components/editor/edit-panel/service-editor/ServiceExecutionEditor.tsx
@@ -334,10 +334,6 @@ export const ChangeExecutionModal = observer(
     const closeModal = (): void => executionState.setShowChangeExecModal(false);
     const isChangingToMulti =
       executionState instanceof SingleServicePureExecutionState;
-    const handleSubmit = (): void => {
-      executionState.changeExecution();
-      closeModal();
-    };
     const renderChangeExecution = (): React.ReactNode => {
       if (executionState instanceof SingleServicePureExecutionState) {
         const keyValue = executionState.multiExecutionKey;
@@ -404,7 +400,11 @@ export const ChangeExecutionModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            executionState.changeExecution();
+            closeModal();
+          }}
           className="modal modal--dark search-modal"
         >
           <div className="modal__title">
@@ -553,11 +553,6 @@ export const NewExecutionParameterModal = observer(
 
     const closeModal = (): void =>
       executionState.setNewKeyParameterModal(false);
-    const handleSubmit = (): void => {
-      executionState.addExecutionParameter(keyValue);
-      setKeyValue('');
-      closeModal();
-    };
     const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
       setKeyValue(event.target.value);
     };
@@ -569,7 +564,12 @@ export const NewExecutionParameterModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            executionState.addExecutionParameter(keyValue);
+            setKeyValue('');
+            closeModal();
+          }}
           className="modal modal--dark search-modal"
         >
           <div className="modal__title">New Execution Context</div>
@@ -607,10 +607,6 @@ const RenameModal = observer(
   }) => {
     const { val, isReadOnly, showModal, closeModal, setValue } = props;
     const [inputValue, setInputValue] = useState(val);
-    const handleSubmit = (): void => {
-      setValue(inputValue);
-      closeModal();
-    };
     const changeValue: React.ChangeEventHandler<HTMLInputElement> = (event) => {
       setInputValue(event.target.value);
     };
@@ -622,7 +618,11 @@ const RenameModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            setValue(inputValue);
+            closeModal();
+          }}
           className="modal modal--dark search-modal"
         >
           <div className="modal__title">Rename</div>

packages/legend-application-studio/src/components/editor/edit-panel/service-editor/testable/ServiceTestDataEditor.tsx
@@ -253,14 +253,6 @@ export const NewConnectionDataModal = observer(
           (c) => c.connectionId === selectedConnection?.value.id,
         ),
       );
-    const handleSubmit = (): void => {
-      const connection = newConnectionState.connection;
-      const data = newConnectionState.embeddedDataType;
-      if (connection && data) {
-        testDataState.createConnectionTestData();
-        closeModal();
-      }
-    };
     const onConnectionSelectionChange = (val: {
       label: string;
       value?: IdentifiedConnection;
@@ -309,7 +301,15 @@ export const NewConnectionDataModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            const connection = newConnectionState.connection;
+            const data = newConnectionState.embeddedDataType;
+            if (connection && data) {
+              testDataState.createConnectionTestData();
+              closeModal();
+            }
+          }}
           className="modal service-test-data-modal modal--dark"
         >
           <div className="modal__header">

packages/legend-application-studio/src/components/editor/edit-panel/service-editor/testable/ServiceTestableEditor.tsx
@@ -101,10 +101,6 @@ export const RenameModal = observer(
   }) => {
     const { val, isReadOnly, showModal, closeModal, setValue } = props;
     const [inputValue, setInputValue] = useState(val);
-    const handleSubmit = (): void => {
-      setValue(inputValue);
-      closeModal();
-    };
     const changeValue: React.ChangeEventHandler<HTMLInputElement> = (event) => {
       setInputValue(event.target.value);
     };
@@ -116,7 +112,11 @@ export const RenameModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            setValue(inputValue);
+            closeModal();
+          }}
           className="modal modal--dark search-modal"
         >
           <div className="modal__title">Rename</div>

packages/legend-application-studio/src/components/editor/edit-panel/service-editor/testable/ServiceTestsEditor.tsx
@@ -159,7 +159,6 @@ export const NewParameterModal = observer(
     };
     const options = setupState.newParamOptions;
     const closeModal = (): void => setupState.setShowNewParameterModal(false);
-    const handleSubmit = (): void => setupState.addParameterValue();
     const onChange = (val: { label: string; value: string } | null): void => {
       if (val === null) {
         setupState.setNewParameterValueName('');
@@ -175,7 +174,10 @@ export const NewParameterModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            setupState.addParameterValue();
+          }}
           className="modal modal--dark search-modal"
         >
           <div className="modal__title">New Test Parameter Value </div>

packages/legend-application-studio/src/components/editor/side-bar/CreateNewElementModal.tsx
@@ -464,10 +464,6 @@ export const CreateNewElementModal = observer(() => {
     newElementState.setName('');
     elementNameInputRef.current?.focus();
   };
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    save();
-  };
 
   if (!newElementState.showModal) {
     return null;
@@ -484,7 +480,10 @@ export const CreateNewElementModal = observer(() => {
     >
       <form
         data-testid={LEGEND_STUDIO_TEST_ID.NEW_ELEMENT_MODAL}
-        onSubmit={handleSubmit}
+        onSubmit={(event) => {
+          event.preventDefault();
+          save();
+        }}
         className="modal modal--dark search-modal"
       >
         <div className="modal__title">

packages/legend-application-studio/src/components/editor/side-bar/WorkspaceReview.tsx
@@ -209,8 +209,8 @@ export const WorkspaceReview = observer(() => {
             <>
               <form
                 className="workspace-review__title"
-                onSubmit={(e): void => {
-                  e.preventDefault();
+                onSubmit={(event) => {
+                  event.preventDefault();
                 }}
               >
                 <div className="workspace-review__title__content">

packages/legend-application-studio/src/components/workspace-setup/CreateProjectModal.tsx
@@ -124,10 +124,7 @@ const CreateNewProjectTab = observer(() => {
   const disableSubmit =
     dispatchingActions || !projectName || !artifactId || !groupId;
 
-  const handleSubmit = (
-    event: React.FormEvent<HTMLFormElement | HTMLButtonElement>,
-  ): void => {
-    event.preventDefault();
+  const handleSubmit = (): void => {
     if (projectName && groupId && artifactId) {
       if (allowCreatingNewProject) {
         flowResult(
@@ -158,7 +155,12 @@ const CreateNewProjectTab = observer(() => {
     );
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        handleSubmit();
+      }}
+    >
       <PanelLoadingIndicator
         isLoading={setupStore.createOrImportProjectState.isInProgress}
       />
@@ -437,10 +439,7 @@ const ImportProjectTab = observer(() => {
   const disableSubmit =
     dispatchingActions || !projectIdentifier || !artifactId || !groupId;
 
-  const handleSubmit = (
-    event: React.FormEvent<HTMLFormElement | HTMLButtonElement>,
-  ): void => {
-    event.preventDefault();
+  const handleSubmit = (): void => {
     if (importProjectSuccessReport) {
       applicationStore.navigator.visitAddress(
         importProjectSuccessReport.reviewUrl,
@@ -455,7 +454,12 @@ const ImportProjectTab = observer(() => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        handleSubmit();
+      }}
+    >
       <PanelLoadingIndicator
         isLoading={setupStore.createOrImportProjectState.isInProgress}
       />

packages/legend-application-studio/src/components/workspace-setup/CreateWorkspaceModal.tsx
@@ -78,10 +78,6 @@ export const CreateWorkspaceModal = observer(
     const onClose = (): void => {
       setupStore.setShowCreateWorkspaceModal(false);
     };
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-      event.preventDefault();
-      createWorkspace();
-    };
 
     return (
       <Dialog
@@ -103,7 +99,12 @@ export const CreateWorkspaceModal = observer(
               }
             />
           </div>
-          <form onSubmit={handleSubmit}>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              createWorkspace();
+            }}
+          >
             <PanelLoadingIndicator
               isLoading={setupStore.createWorkspaceState.isInProgress}
             />

packages/legend-application-studio/src/components/workspace-setup/WorkspaceSetup.tsx
@@ -227,7 +227,12 @@ export const WorkspaceSetup = withWorkspaceSetupStore(
                     </div>
                   )}
                 </div>
-                <form onSubmit={handleProceed}>
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    handleProceed();
+                  }}
+                >
                   <div className="workspace-setup__selector">
                     <div
                       className="workspace-setup__selector__icon"

packages/legend-application/src/components/ActionAlert.tsx
@@ -48,10 +48,6 @@ const ActionAlertContent = observer((props: { info: ActionAlertInfo }) => {
     actions.find((action) => action.default)?.handler?.();
     handleClose();
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    handleSubmit();
-  };
 
   return (
     <Dialog
@@ -62,7 +58,10 @@ const ActionAlertContent = observer((props: { info: ActionAlertInfo }) => {
       }}
     >
       <form
-        onSubmit={onSubmit}
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmit();
+        }}
         className={`modal search-modal modal--dark blocking-alert blocking-alert--${(
           type ?? ActionAlertType.STANDARD
         ).toLowerCase()}`}

packages/legend-extension-dsl-service/src/components/studio/ServiceQueryEditor.tsx
@@ -141,10 +141,6 @@ const NewServiceModal = observer(() => {
       ).catch(applicationStore.alertUnhandledError);
     }
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    create();
-  };
   const onClose = (): void => editorStore.setShowNewServiceModal(false);
 
   return (
@@ -160,7 +156,13 @@ const NewServiceModal = observer(() => {
         },
       }}
     >
-      <form onSubmit={onSubmit} className="modal search-modal modal--dark">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          create();
+        }}
+        className="modal search-modal modal--dark"
+      >
         <div className="modal__title">Create New Service</div>
         <Panel>
           <div className="panel__content__form">
@@ -292,10 +294,6 @@ const RegisterServiceModal = observer(() => {
       ).catch(applicationStore.alertUnhandledError);
     }
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    registerService();
-  };
   const onClose = (): void =>
     editorStore.setShowServiceRegistrationModal(false);
 
@@ -312,7 +310,13 @@ const RegisterServiceModal = observer(() => {
         },
       }}
     >
-      <form onSubmit={onSubmit} className="modal search-modal modal--dark">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          registerService();
+        }}
+        className="modal search-modal modal--dark"
+      >
         <div className="modal__title">Register Service</div>
         <Panel>
           <div className="panel__content__form">

packages/legend-extension-dsl-service/src/components/studio/ServiceQueryEditorReviewAction.tsx
@@ -50,10 +50,6 @@ const NewReviewModal = observer(() => {
         .catch(applicationStore.alertUnhandledError);
     }
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    submitReview();
-  };
   const onClose = (): void => editorStore.setShowSubmitReviewModal(false);
 
   return (
@@ -69,7 +65,13 @@ const NewReviewModal = observer(() => {
         },
       }}
     >
-      <form onSubmit={onSubmit} className="modal search-modal modal--dark">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          submitReview();
+        }}
+        className="modal search-modal modal--dark"
+      >
         <div className="modal__title">Submit for Review</div>
         <Panel>
           <PanelLoadingIndicator

packages/legend-extension-dsl-service/src/components/studio/UpdateProjectServiceQuerySetup.tsx
@@ -128,10 +128,6 @@ const CreateWorkspaceModal = observer((props: { selectedProject: Project }) => {
   const onClose = (): void => {
     setupStore.setShowCreateWorkspaceModal(false);
   };
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    createWorkspace();
-  };
 
   return (
     <Dialog
@@ -143,7 +139,13 @@ const CreateWorkspaceModal = observer((props: { selectedProject: Project }) => {
       classes={{ container: 'search-modal__container' }}
       PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
     >
-      <form onSubmit={handleSubmit} className="modal modal--dark search-modal">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          createWorkspace();
+        }}
+        className="modal modal--dark search-modal"
+      >
         <div className="modal__title">Create New Workspace</div>
         <Panel>
           <PanelLoadingIndicator

packages/legend-extension-dsl-service/src/components/studio/UpdateServiceQuerySetup.tsx
@@ -145,10 +145,6 @@ const CreateWorkspaceModal = observer(
     const onClose = (): void => {
       setupStore.setShowCreateWorkspaceModal(false);
     };
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-      event.preventDefault();
-      createWorkspace();
-    };
 
     return (
       <Dialog
@@ -161,7 +157,10 @@ const CreateWorkspaceModal = observer(
         PaperProps={{ classes: { root: 'search-modal__inner-container' } }}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            event.preventDefault();
+            createWorkspace();
+          }}
           className="modal modal--dark search-modal"
         >
           <div className="modal__title">Create New Workspace</div>