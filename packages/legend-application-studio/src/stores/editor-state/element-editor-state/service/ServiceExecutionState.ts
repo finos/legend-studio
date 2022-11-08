finos/legend-studio

commit cb6451c33e0e747ced31b631c6f5e3ba0ac6c53a

leverage engine to execute queries with parameters (#1572)
* Leverage engine to execute queries with parameters

fix builds

clean code

add test

* fix builds

* fix tests

* fix build

* clean code

packages/legend-application-studio/src/stores/editor-state/element-editor-state/service/ServiceExecutionState.ts
@@ -23,6 +23,7 @@ import {
   stringifyLosslessJSON,
   UnsupportedOperationError,
   filterByType,
+  guaranteeNonNullable,
 } from '@finos/legend-shared';
 import type { ServiceEditorState } from './ServiceEditorState.js';
 import {
@@ -60,9 +61,9 @@ import {
   buildLambdaVariableExpressions,
   observe_ValueSpecification,
   VariableExpression,
-  buildRawLambdaFromLambdaFunction,
   stub_PackageableRuntime,
   stub_Mapping,
+  ParameterValue,
 } from '@finos/legend-graph';
 import { type Entity, parseGACoordinates } from '@finos/legend-storage';
 import { runtime_addMapping } from '../../../shared/modifier/DSL_Mapping_GraphModifierHelper.js';
@@ -78,7 +79,6 @@ import {
   service_setExecution,
 } from '../../../shared/modifier/DSL_Service_GraphModifierHelper.js';
 import {
-  buildParametersLetLambdaFunc,
   LambdaEditorState,
   LambdaParametersState,
   LambdaParameterState,
@@ -578,15 +578,15 @@ export abstract class ServicePureExecutionState extends ServiceExecutionState {
     }
     try {
       this.isRunningQuery = true;
-      const query = this.getExecutionQuery();
       const promise =
         this.editorStore.graphManagerState.graphManager.executeMapping(
-          query,
+          this.queryState.query,
           this.selectedExecutionContextState.executionContext.mapping.value,
           this.selectedExecutionContextState.executionContext.runtime,
           this.editorStore.graphManagerState.graph,
           {
             useLosslessParse: true,
+            parameterValues: this.buildExecutionParameterValues(),
           },
         );
       this.setQueryRunPromise(promise);
@@ -609,29 +609,16 @@ export abstract class ServicePureExecutionState extends ServiceExecutionState {
     }
   }
 
-  getExecutionQuery(): RawLambda {
-    if (this.parameterState.parameterStates.length) {
-      const letlambdaFunction = buildParametersLetLambdaFunc(
-        this.editorStore.graphManagerState.graph,
-        this.parameterState.parameterStates,
-      );
-      const letRawLambda = buildRawLambdaFromLambdaFunction(
-        letlambdaFunction,
-        this.editorStore.graphManagerState,
-      );
-      // reset parameters
-      if (
-        Array.isArray(this.queryState.query.body) &&
-        Array.isArray(letRawLambda.body)
-      ) {
-        letRawLambda.body = [
-          ...(letRawLambda.body as object[]),
-          ...(this.queryState.query.body as object[]),
-        ];
-        return letRawLambda;
-      }
-    }
-    return this.queryState.query;
+  buildExecutionParameterValues(): ParameterValue[] {
+    return this.parameterState.parameterStates.map((queryParamState) => {
+      const paramValue = new ParameterValue();
+      paramValue.name = queryParamState.parameter.name;
+      paramValue.value =
+        this.editorStore.graphManagerState.graphManager.serializeValueSpecification(
+          guaranteeNonNullable(queryParamState.value),
+        );
+      return paramValue;
+    });
   }
 
   get serviceExecutionParameters():

packages/legend-query-builder/src/stores/QueryBuilderResultState.ts
@@ -35,10 +35,10 @@ import {
   EXECUTION_SERIALIZATION_FORMAT,
   RawExecutionResult,
   buildRawLambdaFromLambdaFunction,
+  ParameterValue,
 } from '@finos/legend-graph';
 import { buildLambdaFunction } from './QueryBuilderValueSpecificationBuilder.js';
 import { ExecutionPlanState } from '@finos/legend-application';
-import { buildParametersLetLambdaFunc } from './shared/LambdaParameterState.js';
 
 const DEFAULT_LIMIT = 1000;
 
@@ -125,29 +125,22 @@ export class QueryBuilderResultState {
         this.queryBuilderState.unsupportedQueryState.rawLambda,
         'Lambda is required to execute query',
       );
-      if (
-        !this.queryBuilderState.isParameterSupportDisabled &&
-        this.queryBuilderState.parametersState.parameterStates.length
-      ) {
-        const letlambdaFunction = buildParametersLetLambdaFunc(
-          this.queryBuilderState.graphManagerState.graph,
-          this.queryBuilderState.parametersState.parameterStates,
-        );
-        const letRawLambda = buildRawLambdaFromLambdaFunction(
-          letlambdaFunction,
-          this.queryBuilderState.graphManagerState,
-        );
-        // reset paramaters
-        if (Array.isArray(query.body) && Array.isArray(letRawLambda.body)) {
-          letRawLambda.body = [
-            ...(letRawLambda.body as object[]),
-            ...(query.body as object[]),
-          ];
-          query = letRawLambda;
-        }
-      }
     }
     return query;
+  }
+
+  buildExecutionParameterValues(): ParameterValue[] {
+    return this.queryBuilderState.parametersState.parameterStates.map(
+      (queryParamState) => {
+        const paramValue = new ParameterValue();
+        paramValue.name = queryParamState.parameter.name;
+        paramValue.value =
+          this.queryBuilderState.graphManagerState.graphManager.serializeValueSpecification(
+            guaranteeNonNullable(queryParamState.value),
+          );
+        return paramValue;
+      },
+    );
   }
 
   *exportData(
@@ -217,6 +210,7 @@ export class QueryBuilderResultState {
         `Runtime is required to execute query`,
       );
       const query = this.buildExecutionRawLambda();
+      const parameterValues = this.buildExecutionParameterValues();
       const startTime = Date.now();
       const promise =
         this.queryBuilderState.graphManagerState.graphManager.executeMapping(
@@ -224,6 +218,9 @@ export class QueryBuilderResultState {
           mapping,
           runtime,
           this.queryBuilderState.graphManagerState.graph,
+          {
+            parameterValues: parameterValues,
+          },
         );
       this.setQueryRunPromise(promise);
       const result = (yield promise) as ExecutionResult;

packages/legend-query-builder/src/stores/QueryBuilderValueSpecificationBuilder.ts
@@ -35,7 +35,6 @@ import { buildFilterExpression } from './filter/QueryBuilderFilterValueSpecifica
 import type { LambdaFunctionBuilderOption } from './QueryBuilderValueSpecificationBuilderHelper.js';
 import type { QueryBuilderFetchStructureState } from './fetch-structure/QueryBuilderFetchStructureState.js';
 import { QUERY_BUILDER_SUPPORTED_FUNCTIONS } from '../graphManager/QueryBuilderSupportedFunctions.js';
-import { buildParametersLetLambdaFunc } from './shared/LambdaParameterState.js';
 import { buildWatermarkExpression } from './watermark/QueryBuilderWatermarkValueSpecificationBuilder.js';
 
 const buildGetAllFunction = (
@@ -160,25 +159,8 @@ export const buildLambdaFunction = (
     !queryBuilderState.isParameterSupportDisabled &&
     queryBuilderState.parametersState.parameterStates.length
   ) {
-    // NOTE: if we are executing:
-    // 1. set the parameters to empty
-    // 2. add let statements for each parameter
-    if (options?.isBuildingExecutionQuery) {
-      lambdaFunction.functionType.parameters = [];
-      const letsFuncs = buildParametersLetLambdaFunc(
-        queryBuilderState.graphManagerState.graph,
-        queryBuilderState.parametersState.parameterStates,
-      );
-      lambdaFunction.expressionSequence = [
-        ...letsFuncs.expressionSequence,
-        ...lambdaFunction.expressionSequence,
-      ];
-    } else {
-      lambdaFunction.functionType.parameters =
-        queryBuilderState.parametersState.parameterStates.map(
-          (e) => e.parameter,
-        );
-    }
+    lambdaFunction.functionType.parameters =
+      queryBuilderState.parametersState.parameterStates.map((e) => e.parameter);
   }
 
   return lambdaFunction;

packages/legend-query-builder/src/stores/shared/LambdaParameterState.ts
@@ -23,17 +23,7 @@ import {
   GenericTypeExplicitReference,
   observe_ValueSpecification,
   observe_VariableExpression,
-  PrimitiveInstanceValue,
   VariableExpression,
-  LambdaFunction,
-  CORE_PURE_PATH,
-  FunctionType,
-  SimpleFunctionExpression,
-  SUPPORTED_FUNCTIONS,
-  extractElementNameFromPath,
-  PackageableElementExplicitReference,
-  Multiplicity,
-  PrimitiveType,
 } from '@finos/legend-graph';
 import {
   addUniqueEntry,
@@ -41,7 +31,6 @@ import {
   type Hashable,
   hashArray,
   IllegalStateError,
-  isNonNullable,
   uuid,
 } from '@finos/legend-shared';
 import { makeObservable, observable, action, computed } from 'mobx';
@@ -60,41 +49,6 @@ enum LAMABA_PARAMETER_HASH_STRUCTURE {
   LAMBDA_PARAMETER_STATE = 'LAMBDA_PARAMETER_STATE',
   LAMBDA_PARAMETERS_STATE = 'LAMBDA_PARAMETERS_STATE',
 }
-
-export const buildParametersLetLambdaFunc = (
-  graph: PureModel,
-  lambdaParametersStates: LambdaParameterState[],
-): LambdaFunction => {
-  const letlambdaFunction = new LambdaFunction(
-    new FunctionType(
-      PackageableElementExplicitReference.create(
-        graph.getType(CORE_PURE_PATH.ANY),
-      ),
-      Multiplicity.ONE,
-    ),
-  );
-  letlambdaFunction.expressionSequence = lambdaParametersStates
-    .map((queryParamState) => {
-      if (queryParamState.value) {
-        const letFunc = new SimpleFunctionExpression(
-          extractElementNameFromPath(SUPPORTED_FUNCTIONS.LET),
-        );
-        const letVar = new PrimitiveInstanceValue(
-          GenericTypeExplicitReference.create(
-            new GenericType(PrimitiveType.STRING),
-          ),
-        );
-        letVar.values = [queryParamState.variableName];
-        letFunc.parametersValues.push(letVar);
-        letFunc.parametersValues.push(queryParamState.value);
-        return letFunc;
-      }
-      return undefined;
-    })
-    .filter(isNonNullable);
-  return letlambdaFunction;
-};
-
 export class LambdaParameterState implements Hashable {
   readonly uuid = uuid();
   readonly parameter: VariableExpression;

packages/legend-graph/src/graphManager/AbstractPureGraphManager.ts
@@ -73,6 +73,7 @@ import type {
   CompilationResult,
   TextCompilationResult,
 } from './action/compilation/CompilationResult.js';
+import type { ParameterValue } from '../DSL_Service_Exports.js';
 
 export interface TEMPORARY__EngineSetupConfig {
   env: string;
@@ -105,6 +106,7 @@ export interface ExecutionOptions {
    */
   useLosslessParse?: boolean | undefined;
   serializationFormat?: EXECUTION_SERIALIZATION_FORMAT | undefined;
+  parameterValues?: ParameterValue[];
 }
 
 export interface ServiceRegistrationOptions {

packages/legend-graph/src/graphManager/protocol/pure/v1/V1_PureGraphManager.ts
@@ -227,6 +227,7 @@ import {
 } from '../../../../graph/metamodel/pure/test/result/TestResult.js';
 import {
   type Service,
+  type ParameterValue,
   MultiExecutionServiceTestResult,
 } from '../../../../DSL_Service_Exports.js';
 import type { Testable } from '../../../../graph/metamodel/pure/test/Testable.js';
@@ -257,6 +258,7 @@ import type {
   TextCompilationResult,
 } from '../../../action/compilation/CompilationResult.js';
 import { CompilationWarning } from '../../../action/compilation/CompilationWarning.js';
+import { V1_transformParameterValue } from './transformation/pureGraph/from/V1_ServiceTransformer.js';
 
 class V1_PureModelContextDataIndex {
   elements: V1_PackageableElement[] = [];
@@ -1920,6 +1922,7 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
     lambda: RawLambda,
     runtime: Runtime,
     clientVersion: string,
+    parameterValues?: ParameterValue[],
   ): V1_ExecuteInput =>
     this.buildExecutionInput(
       graph,
@@ -1928,6 +1931,7 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
       runtime,
       clientVersion,
       new V1_ExecuteInput(),
+      parameterValues,
     );
 
   private buildExecutionInput = (
@@ -1937,6 +1941,7 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
     runtime: Runtime,
     clientVersion: string,
     executeInput: V1_ExecuteInput,
+    parameterValues?: ParameterValue[],
   ): V1_ExecuteInput => {
     /**
      * NOTE: to lessen network load, we might need to think of a way to only include relevant part of the pure model context data here
@@ -1994,6 +1999,11 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
     );
     executeInput.model = prunedGraphData;
     executeInput.context = new V1_RawBaseExecutionContext(); // TODO: potentially need to support more types
+    if (parameterValues) {
+      executeInput.parameterValues = parameterValues.map((p) =>
+        V1_transformParameterValue(p),
+      );
+    }
     return executeInput;
   };
 
@@ -2012,6 +2022,7 @@ export class V1_PureGraphManager extends AbstractPureGraphManager {
           lambda,
           runtime,
           V1_PureGraphManager.TARGET_PROTOCOL_VERSION,
+          options?.parameterValues,
         ),
         options,
       ),

packages/legend-graph/src/graphManager/protocol/pure/v1/engine/execution/V1_ExecuteInput.ts
@@ -17,13 +17,20 @@
 import {
   createModelSchema,
   custom,
+  deserialize,
   list,
   object,
   optional,
   primitive,
+  serialize,
   SKIP,
 } from 'serializr';
-import { SerializationFactory, usingModelSchema } from '@finos/legend-shared';
+import {
+  deserializeArray,
+  SerializationFactory,
+  serializeArray,
+  usingModelSchema,
+} from '@finos/legend-shared';
 import { V1_PureModelContextData } from '../../model/context/V1_PureModelContextData.js';
 import type { V1_Runtime } from '../../model/packageableElements/runtime/V1_Runtime.js';
 import type { V1_RawExecutionContext } from '../../model/rawValueSpecification/V1_RawExecutionContext.js';
@@ -33,6 +40,8 @@ import {
   V1_rawBaseExecutionContextModelSchema,
   V1_rawLambdaModelSchema,
 } from '../../transformation/pureProtocol/serializationHelpers/V1_RawValueSpecificationSerializationHelper.js';
+import type { V1_ParameterValue } from '../../model/packageableElements/service/V1_ParameterValue.js';
+import { V1_parameterValueModelSchema } from '../../transformation/pureProtocol/serializationHelpers/V1_ServiceSerializationHelper.js';
 
 export class V1_ExecuteInput {
   clientVersion!: string;
@@ -46,6 +55,7 @@ export class V1_ExecuteInput {
   model!: V1_PureModelContextData;
   runtime!: V1_Runtime;
   context!: V1_RawExecutionContext;
+  parameterValues: V1_ParameterValue[] = [];
 
   static readonly serialization = new SerializationFactory(
     createModelSchema(V1_ExecuteInput, {
@@ -58,6 +68,24 @@ export class V1_ExecuteInput {
         () => SKIP,
       ),
       context: usingModelSchema(V1_rawBaseExecutionContextModelSchema),
+      parameterValues: custom(
+        (values) =>
+          serializeArray(
+            values,
+            (value) => serialize(V1_parameterValueModelSchema, value),
+            {
+              skipIfEmpty: true,
+            },
+          ),
+        (values) =>
+          deserializeArray(
+            values,
+            (v) => deserialize(V1_parameterValueModelSchema, v),
+            {
+              skipIfEmpty: false,
+            },
+          ),
+      ),
     }),
   );
 }

packages/legend-graph/src/graphManager/protocol/pure/v1/transformation/pureGraph/from/V1_ServiceTransformer.ts
@@ -79,7 +79,7 @@ const transformConnectionTestData = (
   return connectionTestData;
 };
 
-const transformParameterValue = (
+export const V1_transformParameterValue = (
   element: ParameterValue,
 ): V1_ParameterValue => {
   const parameterValue = new V1_ParameterValue();
@@ -107,7 +107,7 @@ export const V1_transformServiceTest = (
   serviceTest.id = element.id;
   serviceTest.serializationFormat = element.serializationFormat;
   serviceTest.parameters = element.parameters.map((parameter) =>
-    transformParameterValue(parameter),
+    V1_transformParameterValue(parameter),
   );
   serviceTest.assertions = element.assertions.map((assertion) =>
     V1_transformTestAssertion(assertion),

commit b6083c3c137d76187095bed574c9448dfab81ea4

fix two regressions introduced by #1572 and #1628

packages/legend-application-studio/src/stores/editor-state/element-editor-state/service/ServiceExecutionState.ts
@@ -64,6 +64,8 @@ import {
   stub_PackageableRuntime,
   stub_Mapping,
   ParameterValue,
+  buildRawLambdaFromLambdaFunction,
+  SimpleFunctionExpression,
 } from '@finos/legend-graph';
 import { type Entity, parseGACoordinates } from '@finos/legend-storage';
 import { runtime_addMapping } from '../../../shared/modifier/DSL_Mapping_GraphModifierHelper.js';
@@ -79,6 +81,7 @@ import {
   service_setExecution,
 } from '../../../shared/modifier/DSL_Service_GraphModifierHelper.js';
 import {
+  buildParametersLetLambdaFunc,
   LambdaEditorState,
   LambdaParametersState,
   LambdaParameterState,
@@ -580,7 +583,7 @@ export abstract class ServicePureExecutionState extends ServiceExecutionState {
       this.isRunningQuery = true;
       const promise =
         this.editorStore.graphManagerState.graphManager.executeMapping(
-          this.queryState.query,
+          this.getExecutionQuery(),
           this.selectedExecutionContextState.executionContext.mapping.value,
           this.selectedExecutionContextState.executionContext.runtime,
           this.editorStore.graphManagerState.graph,
@@ -609,16 +612,55 @@ export abstract class ServicePureExecutionState extends ServiceExecutionState {
     }
   }
 
+  getExecutionQuery(): RawLambda {
+    const funcParameterStates = this.parameterState.parameterStates.filter(
+      (ps) =>
+        ps.value instanceof SimpleFunctionExpression &&
+        ps.parameter.multiplicity.upperBound,
+    );
+    // Hack query execution with parameters are of type SimpleFunctionExpression by using let statements
+    if (funcParameterStates.length > 0) {
+      const letlambdaFunction = buildParametersLetLambdaFunc(
+        this.editorStore.graphManagerState.graph,
+        funcParameterStates,
+      );
+      const letRawLambda = buildRawLambdaFromLambdaFunction(
+        letlambdaFunction,
+        this.editorStore.graphManagerState,
+      );
+      // reset parameters
+      if (
+        Array.isArray(this.queryState.query.body) &&
+        Array.isArray(letRawLambda.body)
+      ) {
+        letRawLambda.body = [
+          ...(letRawLambda.body as object[]),
+          ...(this.queryState.query.body as object[]),
+        ];
+        return letRawLambda;
+      }
+    }
+    return this.queryState.query;
+  }
+
   buildExecutionParameterValues(): ParameterValue[] {
-    return this.parameterState.parameterStates.map((queryParamState) => {
-      const paramValue = new ParameterValue();
-      paramValue.name = queryParamState.parameter.name;
-      paramValue.value =
-        this.editorStore.graphManagerState.graphManager.serializeValueSpecification(
-          guaranteeNonNullable(queryParamState.value),
-        );
-      return paramValue;
-    });
+    return this.parameterState.parameterStates
+      .filter(
+        (ps) =>
+          !(
+            ps.value instanceof SimpleFunctionExpression &&
+            ps.parameter.multiplicity.upperBound
+          ),
+      )
+      .map((queryParamState) => {
+        const paramValue = new ParameterValue();
+        paramValue.name = queryParamState.parameter.name;
+        paramValue.value =
+          this.editorStore.graphManagerState.graphManager.serializeValueSpecification(
+            guaranteeNonNullable(queryParamState.value),
+          );
+        return paramValue;
+      });
   }
 
   get serviceExecutionParameters():
@@ -821,6 +863,7 @@ export class MultiServicePureExecutionState extends ServicePureExecutionState {
       setIsRunningQuery: action,
       changeExecution: action,
       generatePlan: flow,
+      handleExecute: flow,
       runQuery: flow,
     });
 

packages/legend-query-builder/src/components/QueryBuilderParametersPanel.tsx
@@ -55,10 +55,7 @@ import {
 import { useDrag } from 'react-dnd';
 import { generateEnumerableNameFromToken } from '@finos/legend-shared';
 import { DEFAULT_VARIABLE_NAME } from '../stores/QueryBuilderConfig.js';
-import {
-  valueSpecification_setMultiplicity,
-  variableExpression_setName,
-} from '../stores/shared/ValueSpecificationModifierHelper.js';
+import { variableExpression_setName } from '../stores/shared/ValueSpecificationModifierHelper.js';
 import { LambdaParameterState } from '../stores/shared/LambdaParameterState.js';
 import { LambdaParameterValuesEditor } from './shared/LambdaParameterValuesEditor.js';
 
@@ -125,9 +122,7 @@ const VariableExpressionEditor = observer(
     const multilicityOptions: MultiplicityOption[] =
       validParamMultiplicityList.map(buildMultiplicityOption);
     const changeMultiplicity = (val: MultiplicityOption): void => {
-      if (multiplity !== val.value) {
-        valueSpecification_setMultiplicity(varState, val.value);
-      }
+      lambdaParameterState.changeMultiplicity(varState, val.value);
     };
 
     const close = (): void => {

packages/legend-query-builder/src/stores/QueryBuilderResultState.ts
@@ -36,9 +36,11 @@ import {
   RawExecutionResult,
   buildRawLambdaFromLambdaFunction,
   ParameterValue,
+  SimpleFunctionExpression,
 } from '@finos/legend-graph';
 import { buildLambdaFunction } from './QueryBuilderValueSpecificationBuilder.js';
 import { ExecutionPlanState } from '@finos/legend-application';
+import { buildParametersLetLambdaFunc } from './shared/LambdaParameterState.js';
 
 const DEFAULT_LIMIT = 1000;
 
@@ -125,13 +127,53 @@ export class QueryBuilderResultState {
         this.queryBuilderState.unsupportedQueryState.rawLambda,
         'Lambda is required to execute query',
       );
+      // Hack query execution with parameters are of type SimpleFunctionExpression by using let statements
+      const funcParameterStates =
+        this.queryBuilderState.parametersState.parameterStates.filter(
+          (ps) =>
+            ps.value instanceof SimpleFunctionExpression &&
+            ps.parameter.multiplicity.upperBound,
+        );
+      if (
+        !this.queryBuilderState.isParameterSupportDisabled &&
+        funcParameterStates.length > 0
+      ) {
+        const letlambdaFunction = buildParametersLetLambdaFunc(
+          this.queryBuilderState.graphManagerState.graph,
+          funcParameterStates,
+        );
+        letlambdaFunction.functionType.parameters =
+          this.queryBuilderState.parametersState.parameterStates
+            .filter((ps) => !funcParameterStates.includes(ps))
+            .map((e) => e.parameter);
+
+        const letRawLambda = buildRawLambdaFromLambdaFunction(
+          letlambdaFunction,
+          this.queryBuilderState.graphManagerState,
+        );
+        // reset paramaters
+        if (Array.isArray(query.body) && Array.isArray(letRawLambda.body)) {
+          letRawLambda.body = [
+            ...(letRawLambda.body as object[]),
+            ...(query.body as object[]),
+          ];
+          query = letRawLambda;
+        }
+      }
     }
     return query;
   }
 
   buildExecutionParameterValues(): ParameterValue[] {
-    return this.queryBuilderState.parametersState.parameterStates.map(
-      (queryParamState) => {
+    return this.queryBuilderState.parametersState.parameterStates
+      .filter(
+        (ps) =>
+          !(
+            ps.value instanceof SimpleFunctionExpression &&
+            ps.parameter.multiplicity.upperBound
+          ),
+      )
+      .map((queryParamState) => {
         const paramValue = new ParameterValue();
         paramValue.name = queryParamState.parameter.name;
         paramValue.value =
@@ -139,8 +181,7 @@ export class QueryBuilderResultState {
             guaranteeNonNullable(queryParamState.value),
           );
         return paramValue;
-      },
-    );
+      });
   }
 
   *exportData(

packages/legend-query-builder/src/stores/QueryBuilderValueSpecificationBuilder.ts
@@ -36,6 +36,7 @@ import type { LambdaFunctionBuilderOption } from './QueryBuilderValueSpecificati
 import type { QueryBuilderFetchStructureState } from './fetch-structure/QueryBuilderFetchStructureState.js';
 import { QUERY_BUILDER_SUPPORTED_FUNCTIONS } from '../graphManager/QueryBuilderSupportedFunctions.js';
 import { buildWatermarkExpression } from './watermark/QueryBuilderWatermarkValueSpecificationBuilder.js';
+import { buildParametersLetLambdaFunc } from './shared/LambdaParameterState.js';
 
 const buildGetAllFunction = (
   _class: Class,
@@ -159,9 +160,38 @@ export const buildLambdaFunction = (
     !queryBuilderState.isParameterSupportDisabled &&
     queryBuilderState.parametersState.parameterStates.length
   ) {
-    lambdaFunction.functionType.parameters =
-      queryBuilderState.parametersState.parameterStates.map((e) => e.parameter);
+    // Hack query execution with parameters are of type SimpleFunctionExpression by using let statements
+    const funcParameterStates =
+      queryBuilderState.parametersState.parameterStates.filter(
+        (ps) =>
+          ps.value instanceof SimpleFunctionExpression &&
+          ps.parameter.multiplicity.upperBound,
+      );
+    if (
+      funcParameterStates.length === 0 ||
+      !options?.isBuildingExecutionQuery
+    ) {
+      lambdaFunction.functionType.parameters =
+        queryBuilderState.parametersState.parameterStates.map(
+          (e) => e.parameter,
+        );
+    } else {
+      // NOTE: if we are executing:
+      // 1. set the parameters to empty
+      // 2. add let statements for each parameter
+      lambdaFunction.functionType.parameters =
+        queryBuilderState.parametersState.parameterStates
+          .filter((ps) => !funcParameterStates.includes(ps))
+          .map((e) => e.parameter);
+      const letsFuncs = buildParametersLetLambdaFunc(
+        queryBuilderState.graphManagerState.graph,
+        funcParameterStates,
+      );
+      lambdaFunction.expressionSequence = [
+        ...letsFuncs.expressionSequence,
+        ...lambdaFunction.expressionSequence,
+      ];
+    }
   }
-
   return lambdaFunction;
 };

packages/legend-query-builder/src/stores/shared/LambdaParameterState.ts
@@ -24,6 +24,17 @@ import {
   observe_ValueSpecification,
   observe_VariableExpression,
   VariableExpression,
+  SimpleFunctionExpression,
+  PackageableElementExplicitReference,
+  LambdaFunction,
+  FunctionType,
+  CORE_PURE_PATH,
+  extractElementNameFromPath,
+  Multiplicity,
+  PrimitiveInstanceValue,
+  PrimitiveType,
+  SUPPORTED_FUNCTIONS,
+  areMultiplicitiesEqual,
 } from '@finos/legend-graph';
 import {
   addUniqueEntry,
@@ -32,6 +43,7 @@ import {
   hashArray,
   IllegalStateError,
   uuid,
+  isNonNullable,
 } from '@finos/legend-shared';
 import { makeObservable, observable, action, computed } from 'mobx';
 import { generateVariableExpressionMockValue } from './ValueSpecificationEditorHelper.js';
@@ -49,6 +61,41 @@ enum LAMABA_PARAMETER_HASH_STRUCTURE {
   LAMBDA_PARAMETER_STATE = 'LAMBDA_PARAMETER_STATE',
   LAMBDA_PARAMETERS_STATE = 'LAMBDA_PARAMETERS_STATE',
 }
+
+export const buildParametersLetLambdaFunc = (
+  graph: PureModel,
+  lambdaParametersStates: LambdaParameterState[],
+): LambdaFunction => {
+  const letlambdaFunction = new LambdaFunction(
+    new FunctionType(
+      PackageableElementExplicitReference.create(
+        graph.getType(CORE_PURE_PATH.ANY),
+      ),
+      Multiplicity.ONE,
+    ),
+  );
+  letlambdaFunction.expressionSequence = lambdaParametersStates
+    .map((queryParamState) => {
+      if (queryParamState.value) {
+        const letFunc = new SimpleFunctionExpression(
+          extractElementNameFromPath(SUPPORTED_FUNCTIONS.LET),
+        );
+        const letVar = new PrimitiveInstanceValue(
+          GenericTypeExplicitReference.create(
+            new GenericType(PrimitiveType.STRING),
+          ),
+        );
+        letVar.values = [queryParamState.variableName];
+        letFunc.parametersValues.push(letVar);
+        letFunc.parametersValues.push(queryParamState.value);
+        return letFunc;
+      }
+      return undefined;
+    })
+    .filter(isNonNullable);
+  return letlambdaFunction;
+};
+
 export class LambdaParameterState implements Hashable {
   readonly uuid = uuid();
   readonly parameter: VariableExpression;
@@ -109,18 +156,11 @@ export class LambdaParameterState implements Hashable {
 
   changeMultiplicity(
     variableExpression: VariableExpression,
-    lowerBound: number,
-    uppderBound: number | undefined,
+    mul: Multiplicity,
   ): void {
     const current = this.parameter.multiplicity;
-    if (
-      current.lowerBound !== lowerBound ||
-      current.upperBound !== uppderBound
-    ) {
-      valueSpecification_setMultiplicity(
-        variableExpression,
-        this.graph.getMultiplicity(lowerBound, uppderBound),
-      );
+    if (!areMultiplicitiesEqual(current, mul)) {
+      valueSpecification_setMultiplicity(variableExpression, mul);
       this.mockParameterValue();
     }
   }