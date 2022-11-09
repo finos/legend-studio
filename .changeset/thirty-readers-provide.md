e50a97aea214c4e5153c157c6e6269eec45286f3
Yannan <73408381+YannanGao-gs@users.noreply.github.com>
2022-11-09 12:41:53 -0500

    add an icon to open service test doc (#1663)

---
+++ .changeset/thirty-readers-provide.md
@@ -0,0 +1,5 @@
+---
+'@finos/legend-application-studio': patch
+---
+
+Add an icon to open service test doc ([#1621](https://github.com/finos/legend-studio/issues/1621)).
--- packages/legend-application/src/stores/LegendApplicationDocumentation.ts
+++ packages/legend-application/src/stores/LegendApplicationDocumentation.ts
@@ -17,4 +17,5 @@
 export enum LEGEND_APPLICATION_DOCUMENTATION_KEY {
   QUESTION_HOW_TO_USE_ADVANCED_SEARCH_SYNTAX = 'question.how-to-use-advanced-search-syntax',
   QUESTION_WHEN_TO_CONFIGURE_PLATFORM_VERSIONS = 'question.when-to-configure-project-platform-dependencies-versions',
+  QUESTION_HOW_TO_WRITE_A_SERVICE_TEST = 'question.how-to-write-a-service-test',
 }
