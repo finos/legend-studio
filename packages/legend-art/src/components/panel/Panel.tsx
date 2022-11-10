a146b94f31d990c4f7b4dcfc4367c71c1f46b7cc
xannem <41248956+xannem@users.noreply.github.com>
2022-11-10 15:23:27 -0500

    fix: create workspace group toogle (#1678)

--- packages/legend-art/src/components/panel/Panel.tsx
+++ packages/legend-art/src/components/panel/Panel.tsx
@@ -246,6 +246,7 @@ export const PanelFormBooleanField = observer(
           onClick={toggle}
         >
           <button
+            type="button"
             className={clsx('panel__content__form__section__toggler__btn', {
               'panel__content__form__section__toggler__btn--toggled': value,
             })}
