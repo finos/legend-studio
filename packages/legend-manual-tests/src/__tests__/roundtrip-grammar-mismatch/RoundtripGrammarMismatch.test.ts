a86a3d0a80461440def44d13d795d74f5de2519c
gs-gunjan <72594207+gs-gunjan@users.noreply.github.com>
2022-11-15 13:20:13 +0530

    add import for the preset used

--- packages/legend-manual-tests/src/__tests__/roundtrip-grammar-mismatch/RoundtripGrammarMismatch.test.ts
+++ packages/legend-manual-tests/src/__tests__/roundtrip-grammar-mismatch/RoundtripGrammarMismatch.test.ts
@@ -41,6 +41,7 @@ import {
  * See https://github.com/axios/axios/pull/5104
  */
 import { default as axios, type AxiosResponse } from 'axios';
+import {DSL_ExternalFormat_GraphPreset} from "../../../../legend-graph/src/DSL_ExternalFormat_Extension.js";
 
 const engineConfig = JSON.parse(
   fs.readFileSync(resolve(__dirname, '../../../engine-config.json'), {
