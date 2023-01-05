b7c15ebf47c7bba54ba8e1f0cccedeb12ce80d59
Yannan <Yannan.Gao@ny.email.gs.com>
2023-01-05 15:46:42 -0500

    clean code

--- packages/legend-server-sdlc/src/SDLCServerClient.ts
+++ packages/legend-server-sdlc/src/SDLCServerClient.ts
@@ -169,7 +169,7 @@ export class SDLCServerClient extends AbstractServerClient {
   getCurrentUser = (): Promise<PlainObject<User>> =>
     this.get(this.currentUserUrl);
   getUsers = (name: string): Promise<PlainObject<User>[]> =>
-    this.get(`${this.usersUrl}/?search=${encodeURIComponent(name)}`);
+    this.get(this.usersUrl, undefined, undefined, { search: name });
 
   // ------------------------------------------- Project -------------------------------------------
 
