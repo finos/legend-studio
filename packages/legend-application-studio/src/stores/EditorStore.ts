finos/legend-studio

commit bf2487fbb590bc2c025f746d2393785762b71434

remove graph building form text mode (#1693)

packages/legend-application-studio/src/stores/EditorStore.ts
@@ -42,7 +42,11 @@ import { NewElementState } from './editor/NewElementState.js';
 import { WorkspaceUpdaterState } from './sidebar-state/WorkspaceUpdaterState.js';
 import { ProjectOverviewState } from './sidebar-state/ProjectOverviewState.js';
 import { WorkspaceReviewState } from './sidebar-state/WorkspaceReviewState.js';
-import { LocalChangesState } from './sidebar-state/LocalChangesState.js';
+import {
+  FormLocalChangesState,
+  type LocalChangesState,
+  TextLocalChangesState,
+} from './sidebar-state/LocalChangesState.js';
 import { WorkspaceWorkflowManagerState } from './sidebar-state/WorkflowManagerState.js';
 import { GrammarTextEditorState } from './editor-state/GrammarTextEditorState.js';
 import {
@@ -270,7 +274,7 @@ export class EditorStore implements CommandRegistrar {
       this.sdlcState,
     );
     this.workspaceReviewState = new WorkspaceReviewState(this, this.sdlcState);
-    this.localChangesState = new LocalChangesState(this, this.sdlcState);
+    this.localChangesState = new FormLocalChangesState(this, this.sdlcState);
     this.conflictResolutionState = new WorkspaceUpdateConflictResolutionState(
       this,
       this.sdlcState,
@@ -338,6 +342,7 @@ export class EditorStore implements CommandRegistrar {
 
   setGraphEditMode(graphEditor: GRAPH_EDITOR_MODE): void {
     this.graphEditMode = graphEditor;
+    this.changeLocalChangesState();
     this.graphState.clearProblems();
   }
 
@@ -1115,6 +1120,13 @@ export class EditorStore implements CommandRegistrar {
           this.tabManagerState.currentTab.element,
         );
       }
+      // Stop change detection as we don't need the actual change detection in text mode
+      this.changeDetectionState.stop();
+      this.changeDetectionState.computeLocalChangesInTextMode(
+        (yield this.graphManagerState.graphManager.pureCodeToEntities(
+          this.grammarTextEditorState.graphGrammarText,
+        )) as Entity[],
+      );
     } else if (this.isInGrammarTextMode) {
       yield flowResult(this.graphState.leaveTextMode());
     } else {
@@ -1153,5 +1165,13 @@ export class EditorStore implements CommandRegistrar {
             ).getExtraSupportedElementTypes?.() ?? [],
         ),
     );
+  }
+
+  changeLocalChangesState(): void {
+    if (this.isInFormMode) {
+      this.localChangesState = new FormLocalChangesState(this, this.sdlcState);
+    } else {
+      this.localChangesState = new TextLocalChangesState(this, this.sdlcState);
+    }
   }
 }

packages/legend-application-studio/src/components/editor/side-bar/LocalChanges.tsx
@@ -51,6 +51,7 @@ import { useApplicationStore } from '@finos/legend-application';
 import { useEffect } from 'react';
 import { EntityChangeConflictEditorState } from '../../../stores/editor-state/entity-diff-editor-state/EntityChangeConflictEditorState.js';
 import { EntityChangeConflictSideBarItem } from '../edit-panel/diff-editor/EntityChangeConflictEditor.js';
+import { FormLocalChangesState } from '../../../stores/sidebar-state/LocalChangesState.js';
 
 const PatchLoader = observer(() => {
   const editorStore = useEditorStore();
@@ -172,8 +173,11 @@ export const LocalChanges = observer(() => {
     editorStore.changeDetectionState.workspaceLocalLatestRevisionState.changes;
   const openChange =
     (diff: EntityDiff): (() => void) =>
-    (): void =>
-      localChangesState.openLocalChange(diff);
+    (): void => {
+      if (localChangesState instanceof FormLocalChangesState) {
+        localChangesState.openLocalChange(diff);
+      }
+    };
   // Local/Remote Workspace Conflicts
   const conflicts =
     editorStore.changeDetectionState.potentialWorkspacePullConflicts;
@@ -182,8 +186,11 @@ export const LocalChanges = observer(() => {
     conflict.entityPath === currentTabState.entityPath;
   const openPotentialConflict =
     (conflict: EntityChangeConflict): (() => void) =>
-    (): void =>
-      localChangesState.openPotentialWorkspacePullConflict(conflict);
+    (): void => {
+      if (localChangesState instanceof FormLocalChangesState) {
+        localChangesState.openPotentialWorkspacePullConflict(conflict);
+      }
+    };
   // Local/Remote Workspace Changes
   const workspacePullChanges =
     editorStore.changeDetectionState.aggregatedWorkspaceRemoteChanges;
@@ -195,8 +202,11 @@ export const LocalChanges = observer(() => {
   );
   const openWorkspacePullChange =
     (diff: EntityDiff): (() => void) =>
-    (): void =>
-      localChangesState.openWorkspacePullChange(diff);
+    (): void => {
+      if (localChangesState instanceof FormLocalChangesState) {
+        localChangesState.openWorkspacePullChange(diff);
+      }
+    };
 
   // check if workspace is still in-sync
   useEffect(() => {

packages/legend-application-studio/src/stores/ChangeDetectionState.ts
@@ -35,6 +35,8 @@ import {
   hashObject,
   promisify,
   ActionState,
+  guaranteeNonNullable,
+  guaranteeType,
 } from '@finos/legend-shared';
 import type { EditorStore } from './EditorStore.js';
 import type { EditorGraphState } from './EditorGraphState.js';
@@ -44,6 +46,7 @@ import {
   EntityChangeConflict,
   EntityChangeType,
   EntityDiff,
+  type EntityChange,
 } from '@finos/legend-server-sdlc';
 import {
   ObserverContext,
@@ -51,6 +54,7 @@ import {
   observe_GraphElements,
 } from '@finos/legend-graph';
 import { type IDisposer, keepAlive } from 'mobx-utils';
+import { TextLocalChangesState } from './sidebar-state/LocalChangesState.js';
 
 class RevisionChangeDetectionState {
   editorStore: EditorStore;
@@ -59,6 +63,7 @@ class RevisionChangeDetectionState {
   entityHashesIndex = new Map<string, string>();
   isBuildingEntityHashesIndex = false;
   entities: Entity[] = [];
+  currentEntityHashesIndex = new Map<string, string>();
 
   setEntityHashesIndex(hashesIndex: Map<string, string>): void {
     this.entityHashesIndex = hashesIndex;
@@ -80,6 +85,7 @@ class RevisionChangeDetectionState {
       setIsBuildingEntityHashesIndex: action,
       setEntities: action,
       computeChanges: flow,
+      computeChangesInTextMode: flow,
       buildEntityHashesIndex: flow,
     });
 
@@ -129,6 +135,95 @@ class RevisionChangeDetectionState {
       );
     }
     this.changes = changes;
+    if (!quiet) {
+      this.editorStore.applicationStore.log.info(
+        LogEvent.create(
+          CHANGE_DETECTION_EVENT.CHANGE_DETECTION_CHANGES_COMPUTED,
+        ),
+        Date.now() - startTime,
+        'ms',
+      );
+    }
+  }
+
+  *computeChangesInTextMode(
+    currentEntities: Entity[],
+    quiet?: boolean,
+  ): GeneratorFn<void> {
+    const startTime = Date.now();
+    const changes: EntityDiff[] = [];
+    const entityChanges: EntityChange[] = [];
+    if (!this.isBuildingEntityHashesIndex) {
+      let currentHashesIndex;
+      if (currentEntities.length) {
+        currentHashesIndex =
+          (yield this.editorStore.graphManagerState.graphManager.buildHashesIndex(
+            currentEntities,
+          )) as Map<string, string>;
+        this.currentEntityHashesIndex = currentHashesIndex;
+      }
+      const originalPaths = new Set(Array.from(this.entityHashesIndex.keys()));
+      if (currentHashesIndex) {
+        yield Promise.all<void>(
+          Array.from(currentHashesIndex.entries()).map(
+            ([elementPath, elementHash]) =>
+              promisify(() => {
+                const entity = currentEntities.find(
+                  (e) => e.path === elementPath,
+                );
+                const originalElementHash =
+                  this.entityHashesIndex.get(elementPath);
+                if (!originalElementHash) {
+                  changes.push(
+                    new EntityDiff(
+                      undefined,
+                      elementPath,
+                      EntityChangeType.CREATE,
+                    ),
+                  );
+                  entityChanges.push({
+                    classifierPath: guaranteeNonNullable(entity).classifierPath,
+                    entityPath: guaranteeNonNullable(entity).path,
+                    content: guaranteeNonNullable(entity).content,
+                    type: EntityChangeType.CREATE,
+                  });
+                } else if (originalElementHash !== elementHash) {
+                  changes.push(
+                    new EntityDiff(
+                      elementPath,
+                      elementPath,
+                      EntityChangeType.MODIFY,
+                    ),
+                  );
+                  entityChanges.push({
+                    classifierPath: guaranteeNonNullable(entity).classifierPath,
+                    entityPath: guaranteeNonNullable(entity).path,
+                    content: guaranteeNonNullable(entity).content,
+                    type: EntityChangeType.MODIFY,
+                  });
+                }
+                originalPaths.delete(elementPath);
+              }),
+          ),
+        );
+      }
+      yield promisify(() => {
+        Array.from(originalPaths).forEach((path) => {
+          changes.push(
+            new EntityDiff(path, undefined, EntityChangeType.DELETE),
+          );
+          entityChanges.push({
+            type: EntityChangeType.DELETE,
+            entityPath: path,
+          });
+        });
+      });
+    }
+    this.changes = changes;
+    guaranteeType(
+      this.editorStore.localChangesState,
+      TextLocalChangesState,
+    ).localChanges = entityChanges;
     if (!quiet) {
       this.editorStore.applicationStore.log.info(
         LogEvent.create(
@@ -303,6 +398,7 @@ export class ChangeDetectionState {
       computeConflictResolutionConflicts: flow,
       computeEntityChangeConflicts: flow,
       computeLocalChanges: flow,
+      computeLocalChangesInTextMode: flow,
       computeAggregatedWorkspaceRemoteChanges: flow,
       observeGraph: flow,
     });
@@ -757,6 +853,29 @@ export class ChangeDetectionState {
         'ms',
       );
     }
+  }
+
+  *computeLocalChangesInTextMode(
+    currentEntities: Entity[],
+    quiet?: boolean,
+  ): GeneratorFn<void> {
+    const startTime = Date.now();
+    yield Promise.all([
+      this.workspaceLocalLatestRevisionState.computeChangesInTextMode(
+        currentEntities,
+        quiet,
+      ),
+    ]);
+    if (!quiet) {
+      this.editorStore.applicationStore.log.info(
+        LogEvent.create(
+          CHANGE_DETECTION_EVENT.CHANGE_DETECTION_CHANGES_COMPUTED,
+        ),
+        Date.now() - startTime,
+        'ms',
+      );
+    }
+    this.initState.pass();
   }
 
   *observeGraph(): GeneratorFn<void> {

packages/legend-application-studio/src/stores/EditorTabManagerState.ts
@@ -21,9 +21,7 @@ import { makeObservable, action } from 'mobx';
 import type { EditorState } from './editor-state/EditorState.js';
 import { ElementEditorState } from './editor-state/element-editor-state/ElementEditorState.js';
 import { EntityDiffViewerState } from './editor-state/entity-diff-editor-state/EntityDiffEditorState.js';
-import { FileGenerationViewerState } from './editor-state/FileGenerationViewerState.js';
-import { ModelImporterState } from './editor-state/ModelImporterState.js';
-import { ProjectConfigurationEditorState } from './editor-state/project-configuration-editor-state/ProjectConfigurationEditorState.js';
+import type { FileGenerationViewerState } from './editor-state/FileGenerationViewerState.js';
 import type { EditorStore } from './EditorStore.js';
 
 export class EditorTabManagerState extends TabManagerState {
@@ -93,44 +91,64 @@ export class EditorTabManagerState extends TabManagerState {
    * cause memory issues
    *
    * See https://github.com/finos/legend-studio/issues/1713
-   *
-   * FIXME: we allow this so the UX stays the same but this can cause memory leak
-   * we should change `reprocess` model to do something like having source information
-   * on the form to navigate to it properly so that information is not dependent on the
-   * graph, but on the component itself, with IDs and such.
-   *
-   * TODO: to be removed when we process editor states properly
-   *
-   * @risk memory-leak
    */
-  recoverTabs(tabs: EditorState[], currentTab: EditorState | undefined): void {
-    let newCurrentTab: EditorState | undefined;
-    this.tabs = tabs
-      .map((tab) => {
-        let newTab: EditorState | undefined = undefined;
-        if (tab instanceof ElementEditorState) {
+  recoverTabs = (
+    openedTabEditorPaths: string[],
+    openedGeneratedFileTabStates: FileGenerationViewerState[],
+    currentTabState: EditorState | undefined,
+    currentTabElementPath: string | undefined,
+    shouldRecoverTabs: boolean,
+  ): void => {
+    const getElemenetPathFromFilePath = (filePath: string): string => {
+      const paths = filePath.split('/');
+      let elementPath = '';
+      const fileName = paths.slice(-1)[0]?.split('.')[0];
+      paths
+        .slice(1, paths.length - 1)
+        .forEach((path) => (elementPath = `${elementPath + path}::`));
+      elementPath = `${elementPath + fileName}`;
+      return elementPath;
+    };
+    if (shouldRecoverTabs) {
+      this.tabs = openedTabEditorPaths
+        .map((editorPath) => {
           const correspondingElement =
             this.editorStore.graphManagerState.graph.getNullableElement(
-              tab.element.path,
+              editorPath,
             );
           if (correspondingElement) {
-            newTab = tab.reprocess(correspondingElement, this.editorStore);
+            return this.editorStore.createElementEditorState(
+              correspondingElement,
+            );
           }
-        } else if (
-          // No need to reprocess generated file state as it has no reference to any of the graphs
-          // TODO: we should do a check to see if the generated file is still around or not
-          tab instanceof FileGenerationViewerState ||
-          tab instanceof ModelImporterState ||
-          tab instanceof ProjectConfigurationEditorState
-        ) {
-          newTab = tab;
-        }
-        if (tab === currentTab) {
-          newCurrentTab = newTab;
-        }
-        return newTab;
-      })
-      .filter(isNonNullable);
-    this.setCurrentTab(newCurrentTab);
-  }
+          const fileGenerationViewerEditor = openedGeneratedFileTabStates.find(
+            (editorState) =>
+              editorState.generatedFilePath === editorPath &&
+              this.editorStore.graphManagerState.graph.getNullableElement(
+                getElemenetPathFromFilePath(editorState.file.path),
+              ) !== undefined,
+          );
+          return fileGenerationViewerEditor;
+        })
+        .filter(isNonNullable);
+      this.setCurrentTab(
+        this.findCurrentTab(currentTabState, currentTabElementPath),
+      );
+    }
+  };
+
+  findCurrentTab = (
+    tab: EditorState | undefined,
+    tabElementPath: string | undefined,
+  ): EditorState | undefined => {
+    if (tab) {
+      return tab;
+    } else {
+      return this.tabs.find(
+        (editorState) =>
+          editorState instanceof ElementEditorState &&
+          editorState.elementPath === tabElementPath,
+      );
+    }
+  };
 }

packages/legend-application-studio/src/stores/ExplorerTreeState.ts
@@ -87,6 +87,8 @@ export class ExplorerTreeState {
       setElementToRename: action,
       build: action,
       buildImmutableModelTrees: action,
+      buildTreeInTextMode: action,
+      openExplorerTreeNodes: action,
       reprocess: action,
       onTreeNodeSelect: action,
       openNode: action,
@@ -186,6 +188,22 @@ export class ExplorerTreeState {
     this.buildState.complete();
   }
 
+  buildTreeInTextMode(): void {
+    this.buildState.reset();
+    this.treeData = getPackableElementTreeData(
+      this.editorStore,
+      this.editorStore.graphManagerState.graph.root,
+      '',
+    );
+    this.generationTreeData = getPackableElementTreeData(
+      this.editorStore,
+      this.editorStore.graphManagerState.graph.generationModel.root,
+      ExplorerTreeRootPackageLabel.MODEL_GENERATION,
+    );
+    this.setSelectedNode(undefined);
+    this.buildState.complete();
+  }
+
   buildImmutableModelTrees(): void {
     this.systemTreeData = getPackableElementTreeData(
       this.editorStore,
@@ -197,6 +215,86 @@ export class ExplorerTreeState {
       this.editorStore.graphManagerState.graph.dependencyManager.root,
       ExplorerTreeRootPackageLabel.PROJECT_DEPENDENCY,
     );
+  }
+
+  openExplorerTreeNodes(
+    mainTreeOpenedNodeIds: string[],
+    generationTreeOpenedNodeIds: string[],
+    selectedNodeId: string | undefined,
+  ): void {
+    // Main tree
+    {
+      const openElements = new Set(
+        mainTreeOpenedNodeIds
+          .map((id) =>
+            this.editorStore.graphManagerState.graph.getNullableElement(
+              id,
+              true,
+            ),
+          )
+          .filter(isNonNullable),
+      );
+      openNodes(
+        this.editorStore,
+        this.editorStore.graphManagerState.graph.root,
+        openElements,
+        guaranteeNonNullable(this.treeData),
+      );
+    }
+    // Generated tree
+    {
+      const openElements = new Set(
+        generationTreeOpenedNodeIds
+          .map((id) =>
+            this.editorStore.graphManagerState.graph.generationModel.getOwnNullableElement(
+              id,
+              true,
+            ),
+          )
+          .filter(isNonNullable),
+      );
+      openNodes(
+        this.editorStore,
+        this.editorStore.graphManagerState.graph.generationModel.root,
+        openElements,
+        guaranteeNonNullable(this.generationTreeData),
+      );
+      if (
+        generationTreeOpenedNodeIds.includes(ROOT_PACKAGE_NAME.MODEL_GENERATION)
+      ) {
+        openNodeById(
+          ROOT_PACKAGE_NAME.MODEL_GENERATION,
+          this.generationTreeData,
+        );
+      }
+    }
+    {
+      if (selectedNodeId) {
+        const element =
+          this.editorStore.graphManagerState.graph.getNullableElement(
+            selectedNodeId,
+          );
+        if (element) {
+          const openingNode =
+            openNode(
+              this.editorStore,
+              element,
+              guaranteeNonNullable(this.treeData),
+            ) ??
+            openNode(
+              this.editorStore,
+              element,
+              guaranteeNonNullable(this.generationTreeData),
+            );
+          // ?? openNode(element, this.systemTreeData)
+          // ?? openNode(element, this.legalTreeData)
+          // ?? openNode(element, this.dependencyTreeData);
+          this.setSelectedNode(openingNode);
+        } else {
+          this.setSelectedNode(undefined);
+        }
+      }
+    }
   }
 
   /**

packages/legend-application-studio/src/stores/editor-state/FileGenerationViewerState.ts
@@ -59,6 +59,7 @@ export class FileGenerationViewerState extends EditorState {
     makeObservable(this, {
       file: observable,
       label: computed,
+      generatedFilePath: computed,
     });
 
     this.file = file;
@@ -70,5 +71,9 @@ export class FileGenerationViewerState extends EditorState {
 
   override match(tab: EditorState): boolean {
     return tab instanceof FileGenerationViewerState && tab.file === this.file;
+  }
+
+  get generatedFilePath(): string {
+    return this.file.path;
   }
 }

packages/legend-application-studio/src/stores/editor-state/element-editor-state/ElementEditorState.ts
@@ -55,6 +55,7 @@ export abstract class ElementEditorState extends EditorState {
       isReadOnly: observable,
       label: computed,
       description: computed,
+      elementPath: computed,
       setTextContent: action,
       setEditMode: action,
       setGenerationViewMode: action,
@@ -68,6 +69,10 @@ export abstract class ElementEditorState extends EditorState {
 
   get label(): string {
     return this.element.name;
+  }
+
+  get elementPath(): string {
+    return this.element.path;
   }
 
   override get description(): string | undefined {

commit c29ea2d490cfd068cf97fc70c930060381754dc4

fix regression introduced in #1693

packages/legend-application-studio/src/stores/EditorStore.ts
@@ -347,11 +347,21 @@ export class EditorStore implements CommandRegistrar {
     if (graphEditor === GRAPH_EDITOR_MODE.GRAMMAR_TEXT) {
       // Stop change detection as we don't need the actual change detection in text mode
       this.changeDetectionState.stop();
-      this.changeDetectionState.computeLocalChangesInTextMode(
-        (yield this.graphManagerState.graphManager.pureCodeToEntities(
-          this.grammarTextEditorState.graphGrammarText,
-        )) as Entity[],
-      );
+      try {
+        yield flowResult(
+          this.changeDetectionState.computeLocalChangesInTextMode(
+            (yield this.graphManagerState.graphManager.pureCodeToEntities(
+              this.grammarTextEditorState.graphGrammarText,
+            )) as Entity[],
+          ),
+        );
+      } catch (error) {
+        assertErrorThrown(error);
+        this.applicationStore.log.warn(
+          LogEvent.create(GRAPH_MANAGER_EVENT.PARSING_FAILURE),
+          error,
+        );
+      }
     }
   }
 