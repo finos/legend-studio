9cc93cff96aa9843ece85764439a5480b7993fa1
Gayathri Rallapalle <87973126+gayathrir11@users.noreply.github.com>
2023-01-12 20:32:03 +0530

    add integration test on navigation of dependency tree (#1813)

---
+++ packages/legend-application-studio/src/components/editor/side-bar/__tests__/DependencyTree.test.tsx
@@ -0,0 +1,187 @@
+/**
+ * Copyright (c) 2020-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { test, beforeEach } from '@jest/globals';
+import {
+  type RenderResult,
+  waitFor,
+  fireEvent,
+  getByText,
+  findByText,
+} from '@testing-library/react';
+import { integrationTest } from '@finos/legend-shared';
+import {
+  TEST__provideMockedEditorStore,
+  TEST__setUpEditorWithDefaultSDLCData,
+} from '../../../EditorComponentTestUtils.js';
+import { LEGEND_STUDIO_TEST_ID } from '../../../LegendStudioTestID.js';
+import { TEST_DATA__ProjectDependencyReport } from '../../edit-panel/__tests__/TEST_DATA__ProjectDependencyReport.js';
+import type { EditorStore } from '../../../../stores/EditorStore.js';
+
+let renderResult: RenderResult;
+
+const TEST_DATA__ProjectConfiguration = {
+  projectStructureVersion: { version: 10, extensionVersion: 1 },
+  projectId: 'PROD-1234',
+  groupId: 'org.finos.legend',
+  artifactId: 'dependency-test',
+  projectDependencies: [
+    {
+      projectId: 'PROD-1',
+      versionId: '2.0.0',
+    },
+    {
+      projectId: 'org.finos.legend:prod-2',
+      versionId: '3.0.0',
+    },
+  ],
+  metamodelDependencies: [],
+};
+
+const TEST_DATA__ProjectData = [
+  {
+    id: 'PROD-1',
+    projectId: 'PROD-1',
+    groupId: 'org.finos.legend',
+    artifactId: 'prod-1',
+    versions: ['1.0.0', '2.0.0'],
+    latestVersion: '2.0.0',
+  },
+];
+
+const TEST_DATA__Projects = [
+  {
+    id: 'PROD-1',
+    projectId: 'PROD-1',
+    groupId: 'org.finos.legend',
+    artifactId: 'prod-1',
+    versions: ['1.0.0', '2.0.0'],
+    latestVersion: '2.0.0',
+  },
+  {
+    id: 'PROD-2',
+    projectId: 'PROD-2',
+    groupId: 'org.finos.legend',
+    artifactId: 'prod-2',
+    versions: ['1.0.0', '2.0.0', '3.0.0'],
+    latestVersion: '3.0.0',
+  },
+  {
+    id: 'PROD-3',
+    projectId: 'PROD-3',
+    groupId: 'org.finos.legend',
+    artifactId: 'prod-3',
+    versions: ['1.0.0', '2.0.0', '3.0.0'],
+    latestVersion: '3.0.0',
+  },
+];
+
+const TEST_DATA__DependencyEntities = [
+  {
+    groupId: 'org.finos.legend',
+    artifactId: 'prod-1',
+    versionId: '2.0.0',
+    versionedEntity: false,
+    entities: [
+      {
+        path: 'model::ClassB',
+        content: {
+          _type: 'class',
+          name: 'ClassB',
+          package: 'model',
+          properties: [
+            {
+              multiplicity: {
+                lowerBound: 1,
+                upperBound: 1,
+              },
+              name: 'prop1',
+              type: 'String',
+            },
+          ],
+        },
+        classifierPath: 'meta::pure::metamodel::type::Class',
+      },
+    ],
+  },
+  {
+    groupId: 'org.finos.legend',
+    artifactId: 'prod-2',
+    versionId: '3.0.0',
+    versionedEntity: false,
+    entities: [
+      {
+        path: 'test::ClassC',
+        content: {
+          _type: 'class',
+          name: 'ClassC',
+          package: 'test',
+          properties: [
+            {
+              multiplicity: {
+                lowerBound: 1,
+                upperBound: 1,
+              },
+              name: 'prop',
+              type: 'String',
+            },
+          ],
+        },
+        classifierPath: 'meta::pure::metamodel::type::Class',
+      },
+    ],
+  },
+];
+
+const TEST_DATA__latestProjectStructure = { version: 11, extensionVersion: 1 };
+
+let MOCK__editorStore: EditorStore;
+beforeEach(async () => {
+  MOCK__editorStore = TEST__provideMockedEditorStore();
+  renderResult = await TEST__setUpEditorWithDefaultSDLCData(MOCK__editorStore, {
+    entities: [],
+    projectConfiguration: TEST_DATA__ProjectConfiguration,
+    latestProjectStructureVersion: TEST_DATA__latestProjectStructure,
+    projects: TEST_DATA__Projects,
+    projectData: TEST_DATA__ProjectData,
+    projectDependency: TEST_DATA__DependencyEntities,
+    dependencyReport: TEST_DATA__ProjectDependencyReport,
+  });
+});
+
+test(integrationTest('Test navigation of dependency tree'), async () => {
+  const editPanel = renderResult.getByTestId(
+    LEGEND_STUDIO_TEST_ID.EXPLORER_TREES,
+  );
+
+  // expanding dependency tree
+  await waitFor(() => findByText(editPanel, 'dependencies'));
+  fireEvent.click(getByText(editPanel, 'dependencies'));
+
+  // expanding first dependency
+  await waitFor(() => findByText(editPanel, 'org.finos.legend:prod-1'));
+  fireEvent.click(getByText(editPanel, 'org.finos.legend:prod-1'));
+  await waitFor(() => findByText(editPanel, 'model'));
+  fireEvent.click(getByText(editPanel, 'model'));
+  await waitFor(() => findByText(editPanel, 'ClassB'));
+
+  // expanding second dependency
+  await waitFor(() => findByText(editPanel, 'org.finos.legend:prod-2'));
+  fireEvent.click(getByText(editPanel, 'org.finos.legend:prod-2'));
+  await waitFor(() => findByText(editPanel, 'test'));
+  fireEvent.click(getByText(editPanel, 'test'));
+  await waitFor(() => findByText(editPanel, 'ClassC'));
+});
