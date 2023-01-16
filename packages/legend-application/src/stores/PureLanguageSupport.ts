d89d4cb9968c9238214b39049fcbc5b658730880
An Phi <aphi@skidmore.edu>
2023-01-16 00:35:06 -0500

    pure ide: fix syntax highlighting for type generics

--- packages/legend-application/src/stores/PureLanguageSupport.ts
+++ packages/legend-application/src/stores/PureLanguageSupport.ts
@@ -259,7 +259,15 @@ const generateLanguageMonarch = (
     hexdigits: /[[0-9a-fA-F]+(_+[0-9a-fA-F]+)*/,
     multiplicity: /\[(?:[a-zA-Z0-9]+(?:\.\.(?:[a-zA-Z0-9]+|\*|))?|\*)\]/,
     package: /(?:[\w_]+::)+/,
-    generics: /<.+>/,
+    // NOTE: generics is a little tricky because in order to do it right, we have to
+    // do some sort of bracket matching, but we just can use a simple tokenizer here
+    // so to account for cases like `<Nil,Any|*>)->` `Function<{T[1]->Boolean[1]}>[1]`
+    // we have to make sure the content does not contain any `:` or `.` characters
+    // in order to avoid the accidental greedy match with inputs like
+    // `function doSomething<T>(a: Function<T[1]->Boolean[1]>)`
+    // nor we want to make sure the last character of the content is not `-` to avoid
+    // accidentally matching `->` as the end of the generics
+    generics: /<[^:.]+[^-]>/,
     date: /%-?\d+(?:-\d+(?:-\d+(?:T(?:\d+(?::\d+(?::\d+(?:.\d+)?)?)?)(?:[+-][0-9]{4})?)))/,
     time: /%\d+(?::\d+(?::\d+(?:.\d+)?)?)?/,
 
