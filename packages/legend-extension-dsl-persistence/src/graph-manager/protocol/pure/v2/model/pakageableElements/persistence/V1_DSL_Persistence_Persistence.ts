21af40a375378210bbc624402abe7d124686cdc2
Ankit Amrit Raj <ankit.amritraj@ny.email.gs.com>
2023-07-04 13:24:08 +0530

    Initial commit

---
+++ packages/legend-extension-dsl-persistence/src/graph-manager/protocol/pure/v2/model/pakageableElements/persistence/V1_DSL_Persistence_Persistence.ts
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2020-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { V1_Notifier } from './V1_DSL_Persistence_Notifier.js';
+import type { V1_Persister } from './V1_DSL_Persistence_Persister.js';
+import type { V1_Trigger } from './V1_DSL_Persistence_Trigger.js';
+import {
+  V1_PackageableElement,
+  type V1_PackageableElementVisitor,
+} from '@finos/legend-graph';
+import { type Hashable, hashArray } from '@finos/legend-shared';
+import { PERSISTENCE_HASH_STRUCTURE } from '../../../../../../../graph/DSL_Persistence_HashUtils.js';
+import type { V1_PersistenceTest } from './V1_DSL_Persistence_PersistenceTest.js';
+
+export class V1_Persistence extends V1_PackageableElement implements Hashable {
+  documentation!: string;
+  trigger!: V1_Trigger;
+  service!: string;
+  persister!: V1_Persister;
+  notifier!: V1_Notifier;
+  tests: V1_PersistenceTest[] = [];
+
+  override get hashCode(): string {
+    return hashArray([
+      PERSISTENCE_HASH_STRUCTURE.PERSISTENCE,
+      this.documentation,
+      this.trigger,
+      this.service,
+      this.persister,
+      this.notifier,
+      hashArray(this.tests),
+    ]);
+  }
+
+  accept_PackageableElementVisitor<T>(
+    visitor: V1_PackageableElementVisitor<T>,
+  ): T {
+    return visitor.visit_PackageableElement(this);
+  }
+}
