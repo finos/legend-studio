f151a5b7f1074cedb633a0602daa73e84758015c
An Phi <aphi@skidmore.edu>
2023-07-06 15:43:43 -0400

    m: cleanup mock server

--- fixtures/legend-mock-server/src/index.ts
+++ fixtures/legend-mock-server/src/index.ts
@@ -14,12 +14,19 @@
  * limitations under the License.
  */
 
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
 import { fastify } from 'fastify';
 import { fastifyCors } from '@fastify/cors';
-import DOCUMENTATION_DATA from './DummyDocumentationData.json' assert { type: 'json' };
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const getFileContent = (file: string) =>
+  readFileSync(file, { encoding: 'utf-8' });
 
 const PORT = 9999;
-// const API_BASE_URL = '/api';
+const RESOURCE_BASE_URL = '/resource';
 
 const server = fastify({
   logger: true,
@@ -32,9 +39,18 @@ server.register(fastifyCors, {
   credentials: true,
 });
 
-server.get(`/documentation.json`, async (request, reply) => {
-  await reply.send(DOCUMENTATION_DATA);
-});
+server.get(
+  `${RESOURCE_BASE_URL}/documentation.json`,
+  async (request, reply) => {
+    await reply.send(
+      JSON.parse(
+        getFileContent(
+          resolve(__dirname, '../data/documentationRegistry/dummy.json'),
+        ),
+      ),
+    );
+  },
+);
 
 server.listen(
   {
