f7efd515eed93db4d8f5353e7893001a7dad9a42
Gayathri <rallapalle.gayathrichowdary@ny.email.gs.com>
2023-07-10 18:28:27 +0530

    Fix processing of calendar function with nested date columns

---
+++ .changeset/beige-tools-kiss.md
@@ -0,0 +1,5 @@
+---
+'@finos/legend-query-builder': patch
+---
+
+Fix processing of calendar function with nested date columns.
--- packages/legend-query-builder/src/stores/__tests__/TEST_DATA__QueryBuilder_Calendar.ts
+++ packages/legend-query-builder/src/stores/__tests__/TEST_DATA__QueryBuilder_Calendar.ts
@@ -421,22 +421,186 @@ export const TEST_DATA__simpleDerivationWithCalendarAggregation = {
   parameters: [],
 };
 
+export const TEST_DATA__simpleProjectionWithCalendarAggregationWithNestedDateColumn =
+  {
+    _type: 'lambda',
+    body: [
+      {
+        _type: 'func',
+        function: 'groupBy',
+        parameters: [
+          {
+            _type: 'func',
+            function: 'getAll',
+            parameters: [
+              {
+                _type: 'packageableElementPtr',
+                fullPath: 'test::Employee',
+              },
+            ],
+          },
+          {
+            _type: 'collection',
+            multiplicity: {
+              lowerBound: 0,
+              upperBound: 0,
+            },
+            values: [],
+          },
+          {
+            _type: 'collection',
+            multiplicity: {
+              lowerBound: 1,
+              upperBound: 1,
+            },
+            values: [
+              {
+                _type: 'func',
+                function: 'agg',
+                parameters: [
+                  {
+                    _type: 'lambda',
+                    body: [
+                      {
+                        _type: 'func',
+                        function: 'ytd',
+                        parameters: [
+                          {
+                            _type: 'property',
+                            parameters: [
+                              {
+                                _type: 'property',
+                                parameters: [
+                                  {
+                                    _type: 'var',
+                                    name: 'x',
+                                  },
+                                ],
+                                property: 'firm',
+                              },
+                            ],
+                            property: 'openingDate',
+                          },
+                          {
+                            _type: 'string',
+                            value: 'NY',
+                          },
+                          {
+                            _type: 'strictDate',
+                            value: '2022-11-16',
+                          },
+                          {
+                            _type: 'property',
+                            parameters: [
+                              {
+                                _type: 'var',
+                                name: 'x',
+                              },
+                            ],
+                            property: 'fteFactor',
+                          },
+                        ],
+                      },
+                    ],
+                    parameters: [
+                      {
+                        _type: 'var',
+                        name: 'x',
+                      },
+                    ],
+                  },
+                  {
+                    _type: 'lambda',
+                    body: [
+                      {
+                        _type: 'func',
+                        function: 'sum',
+                        parameters: [
+                          {
+                            _type: 'var',
+                            name: 'x',
+                          },
+                        ],
+                      },
+                    ],
+                    parameters: [
+                      {
+                        _type: 'var',
+                        name: 'x',
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            _type: 'collection',
+            multiplicity: {
+              lowerBound: 1,
+              upperBound: 1,
+            },
+            values: [
+              {
+                _type: 'string',
+                value: 'ytd',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    parameters: [],
+  };
+
 export const TEST_DATA__ModelCoverageAnalysisResult_Calendar = {
   mappedEntities: [
     {
       path: 'test::Employee',
       properties: [
-        { _type: 'MappedProperty', name: 'endDate' },
-        { _type: 'MappedProperty', name: 'firmName' },
-        { _type: 'MappedProperty', name: 'fteFactor' },
-        { _type: 'MappedProperty', name: 'hireDate' },
-        { _type: 'MappedProperty', name: 'hireType' },
-        { _type: 'MappedProperty', name: 'id' },
+        {
+          _type: 'MappedProperty',
+          name: 'endDate',
+        },
+        {
+          _type: 'entity',
+          entityPath: 'test::Firm',
+          name: 'firm',
+        },
+        {
+          _type: 'MappedProperty',
+          name: 'firmName',
+        },
+        {
+          _type: 'MappedProperty',
+          name: 'fteFactor',
+        },
+        {
+          _type: 'MappedProperty',
+          name: 'hireDate',
+        },
+        {
+          _type: 'MappedProperty',
+          name: 'hireType',
+        },
+        {
+          _type: 'MappedProperty',
+          name: 'id',
+        },
       ],
     },
     {
-      path: 'test::temp',
-      properties: [{ _type: 'MappedProperty', name: 'id' }],
+      path: 'test::Firm',
+      properties: [
+        {
+          _type: 'MappedProperty',
+          name: 'id',
+        },
+        {
+          _type: 'MappedProperty',
+          name: 'openingDate',
+        },
+      ],
     },
   ],
 };
