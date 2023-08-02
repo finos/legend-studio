finos/legend-studio

commit addc5fc0712c9e2e088567054adc33e638c8ae42

fix a bug where failing to generate default value for service test parameter of type Byte (#2466)

packages/legend-query-builder/src/stores/QueryBuilderValueSpecificationHelper.ts
@@ -211,13 +211,12 @@ export const generateDefaultValueForPrimitiveType = (
       return '';
     case PRIMITIVE_TYPE.BOOLEAN:
       return false;
+    case PRIMITIVE_TYPE.BYTE:
+      return btoa('');
     case PRIMITIVE_TYPE.NUMBER:
     case PRIMITIVE_TYPE.DECIMAL:
     case PRIMITIVE_TYPE.FLOAT:
     case PRIMITIVE_TYPE.INTEGER:
-    case PRIMITIVE_TYPE.BYTE:
-    case PRIMITIVE_TYPE.BINARY:
-      return 0;
     case PRIMITIVE_TYPE.DATE:
     case PRIMITIVE_TYPE.STRICTDATE:
       return formatDate(new Date(Date.now()), DATE_FORMAT);

packages/legend-application-studio/src/components/editor/editor-group/service-editor/__tests__/ServiceEditorTest.test.tsx
@@ -22,6 +22,7 @@ import {
   getByTitle,
   queryByText,
   getAllByTitle,
+  getAllByPlaceholderText,
 } from '@testing-library/react';
 import { integrationTest } from '@finos/legend-shared/test';
 import {
@@ -233,6 +234,12 @@ test(
     );
     await waitFor(() =>
       getAllByTitle(serviceTestSetupEditor, 'Open in a popup...'),
+    );
+
+    // Test click `generate` button to generate an empty string as a default value
+    fireEvent.click(getByText(serviceTestEditor, 'Generate'));
+    await waitFor(() =>
+      getAllByPlaceholderText(serviceTestSetupEditor, '(empty)'),
     );
   },
 );

packages/legend-query-builder/src/stores/shared/ValueSpecificationEditorHelper.ts
@@ -224,7 +224,17 @@ export const generateVariableExpressionMockValue = (
 ): ValueSpecification | undefined => {
   const varType = parameter.genericType?.value.rawType;
   const multiplicity = parameter.multiplicity;
-  if ((!multiplicity.upperBound || multiplicity.upperBound > 1) && varType) {
+  /**
+   *  Studio doesn't handle byte[*] as a CollectionInstanceValue but as a
+   *  PrimitiveValueSpecification of type PrimitiveType.BYTE.
+   *  Engine sends a Base64String transformed from byte[] by Jackson to Studio, then
+   *  Studio stores this Base64String as the value of V1_CByteArray.
+   */
+  if (
+    (!multiplicity.upperBound || multiplicity.upperBound > 1) &&
+    varType &&
+    varType !== PrimitiveType.BYTE
+  ) {
     return new CollectionInstanceValue(
       multiplicity,
       GenericTypeExplicitReference.create(new GenericType(varType)),

commit c2e16a3ac061052c44f45979ecb524e5bca6721a

Fix regression introduced in #2466

packages/legend-query-builder/src/stores/QueryBuilderValueSpecificationHelper.ts
@@ -217,6 +217,7 @@ export const generateDefaultValueForPrimitiveType = (
     case PRIMITIVE_TYPE.DECIMAL:
     case PRIMITIVE_TYPE.FLOAT:
     case PRIMITIVE_TYPE.INTEGER:
+      return 0;
     case PRIMITIVE_TYPE.DATE:
     case PRIMITIVE_TYPE.STRICTDATE:
       return formatDate(new Date(Date.now()), DATE_FORMAT);