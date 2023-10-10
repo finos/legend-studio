a2de4047bcc9c7bf544b7fa9cc5d05af3adc13fe
Mauricio Uyaguari <lmauricio12@gmail.com>
2023-10-10 15:05:36 -0400

    query db: support more than one compatible connection

--- packages/legend-application-studio/src/components/editor/editor-group/database/IsolatedQueryDatabase.tsx
+++ packages/legend-application-studio/src/components/editor/editor-group/database/IsolatedQueryDatabase.tsx
@@ -14,16 +14,49 @@
  * limitations under the License.
  */
 
+import type { PackageableConnection } from '@finos/legend-graph';
 import { getMappingCompatibleClasses } from '@finos/legend-graph';
 import { QueryBuilderClassSelector } from '@finos/legend-query-builder';
 import type { IsolatedDatabaseBuilderState } from '../../../../stores/editor/editor-state/element-editor-state/database/QueryDatabaseState.js';
 import { observer } from 'mobx-react-lite';
-import { CustomSelectorInput, PURE_DatabaseIcon } from '@finos/legend-art';
+import {
+  CustomSelectorInput,
+  PURE_ConnectionIcon,
+  PURE_DatabaseIcon,
+} from '@finos/legend-art';
+import { returnUndefOnError } from '@finos/legend-shared';
 
 const IsolatedDatabseQueryBuilderSetupPanelContext = observer(
   (props: { queryBuilderState: IsolatedDatabaseBuilderState }) => {
     const { queryBuilderState } = props;
+    const globalGraphManagerState = queryBuilderState.globalGraphManagerState;
+    const getConnectionValue = (
+      val: string,
+    ): PackageableConnection | undefined =>
+      returnUndefOnError(() =>
+        globalGraphManagerState.graph.getConnection(val),
+      );
     const database = queryBuilderState.database;
+    const compConnections = queryBuilderState.compatibleConnections;
+    const compConnectionsOptions = Array.from(compConnections.keys()).map(
+      (e) => ({ value: e, label: getConnectionValue(e)?.name ?? e }),
+    );
+    const changeConnection = (
+      val: { value: string; label: string } | null,
+    ): void => {
+      if (val) {
+        queryBuilderState.changeConnection(val.value);
+      }
+    };
+    const connection = compConnections.get(queryBuilderState.connectionKey);
+    const selectedConnection = connection
+      ? {
+          label: queryBuilderState.connectionKey,
+          value:
+            getConnectionValue(queryBuilderState.connectionKey)?.name ??
+            queryBuilderState.connectionKey,
+        }
+      : undefined;
     const databaseOption = {
       value: database,
       label: database.path,
@@ -36,10 +69,15 @@ const IsolatedDatabseQueryBuilderSetupPanelContext = observer(
       : [];
     return (
       <>
+        <QueryBuilderClassSelector
+          queryBuilderState={queryBuilderState}
+          classes={classes}
+          noMatchMessage="No classes selected from"
+        />
         <div className="query-builder__setup__config-group">
           <div className="query-builder__setup__config-group__header">
             <div className="query-builder__setup__config-group__header__title">
-              database
+              database/connection
             </div>
           </div>
           <div className="query-builder__setup__config-group__content">
@@ -62,13 +100,28 @@ const IsolatedDatabseQueryBuilderSetupPanelContext = observer(
                 }
               />
             </div>
+            <div className="query-builder__setup__config-group__item">
+              <div
+                className="btn--sm query-builder__setup__config-group__item__label"
+                title="connection"
+              >
+                <PURE_ConnectionIcon />
+              </div>
+              <CustomSelectorInput
+                className="panel__content__form__section__dropdown query-builder__setup__config-group__item__selector"
+                noMatchMessage="No compatible mapping found for specified class"
+                disabled={compConnectionsOptions.length < 2}
+                options={compConnectionsOptions}
+                onChange={changeConnection}
+                value={selectedConnection}
+                darkMode={
+                  !queryBuilderState.applicationStore.layoutService
+                    .TEMPORARY__isLightColorThemeEnabled
+                }
+              />
+            </div>
           </div>
         </div>
-        <QueryBuilderClassSelector
-          queryBuilderState={queryBuilderState}
-          classes={classes}
-          noMatchMessage="No classes selected from"
-        />
       </>
     );
   },
