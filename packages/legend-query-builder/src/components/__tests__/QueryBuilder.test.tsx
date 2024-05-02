dad7e7e5b482c36d86ffc59d8fca7a5121a1e16b
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2024-05-02 16:18:36 -0400

    feat: Add test for QueryBuilder advanced buttons

---
+++ packages/legend-query-builder/src/components/__tests__/QueryBuilder.test.tsx
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2024-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { test, expect } from '@jest/globals';
+import { waitFor, fireEvent, act, getByRole } from '@testing-library/react';
+import { integrationTest } from '@finos/legend-shared/test';
+import { create_RawLambda, stub_RawLambda } from '@finos/legend-graph';
+import { QUERY_BUILDER_TEST_ID } from '../../__lib__/QueryBuilderTesting.js';
+import { TEST__setUpQueryBuilder } from '../__test-utils__/QueryBuilderComponentTestUtils.js';
+import TEST_DATA__QueryBuilder_Model_SimpleRelational from '../../stores/__tests__/TEST_DATA__QueryBuilder_Model_SimpleRelational.json';
+import { TEST_DATA__ModelCoverageAnalysisResult_SimpleRelationalWithExists } from '../../stores/__tests__/TEST_DATA__ModelCoverageAnalysisResult.js';
+import { TEST_DATA__simpeFilterWithMilestonedExists } from '../../stores/__tests__/TEST_DATA__QueryBuilder_Generic.js';
+
+test(
+  integrationTest(
+    "Query builder disables correct advanced options if query can't be built",
+  ),
+
+  async () => {
+    const { renderResult, queryBuilderState } = await TEST__setUpQueryBuilder(
+      TEST_DATA__QueryBuilder_Model_SimpleRelational,
+      stub_RawLambda(),
+      'execution::RelationalMapping',
+      'execution::Runtime',
+      TEST_DATA__ModelCoverageAnalysisResult_SimpleRelationalWithExists,
+    );
+
+    await act(async () => {
+      queryBuilderState.initializeWithQuery(
+        create_RawLambda(
+          TEST_DATA__simpeFilterWithMilestonedExists.parameters,
+          TEST_DATA__simpeFilterWithMilestonedExists.body,
+        ),
+      );
+    });
+
+    // Reset filter value
+    const filterPanel = await waitFor(() =>
+      renderResult.getByTestId(
+        QUERY_BUILDER_TEST_ID.QUERY_BUILDER_FILTER_PANEL,
+      ),
+    );
+    fireEvent.click(getByRole(filterPanel, 'button', { name: 'Reset' }));
+
+    // Verify buttons are disabled
+    fireEvent.click(renderResult.getByRole('button', { name: 'Advanced' }));
+    expect(
+      await renderResult.findByRole('button', { name: 'Check Entitlements' }),
+    ).not.toBeNull();
+    expect(
+      await renderResult.findByRole('button', { name: 'Check Entitlements' }),
+    ).toHaveProperty('disabled', true);
+    expect(
+      await renderResult.findByRole('button', { name: 'Edit Query in Pure' }),
+    ).not.toBeNull();
+    expect(
+      await renderResult.findByRole('button', { name: 'Edit Query in Pure' }),
+    ).toHaveProperty('disabled', true);
+    expect(
+      await renderResult.findByRole('button', { name: 'Show Query Protocol' }),
+    ).not.toBeNull();
+    expect(
+      await renderResult.findByRole('button', { name: 'Show Query Protocol' }),
+    ).toHaveProperty('disabled', true);
+  },
+);
