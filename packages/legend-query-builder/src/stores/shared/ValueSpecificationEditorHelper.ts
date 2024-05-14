267db2c59e5e97cf9e3f3f0bfb37d5bab61befcf
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2024-05-14 15:45:56 -0400

    feat: Re-add support for binary and byte types to buildDefaultInstanceValue function

--- packages/legend-query-builder/src/stores/shared/ValueSpecificationEditorHelper.ts
+++ packages/legend-query-builder/src/stores/shared/ValueSpecificationEditorHelper.ts
@@ -146,6 +146,8 @@ export const buildDefaultInstanceValue = (
     case PRIMITIVE_TYPE.NUMBER:
     case PRIMITIVE_TYPE.DECIMAL:
     case PRIMITIVE_TYPE.FLOAT:
+    case PRIMITIVE_TYPE.BINARY:
+    case PRIMITIVE_TYPE.BYTE:
     case PRIMITIVE_TYPE.INTEGER: {
       return buildPrimitiveInstanceValue(
         graph,
