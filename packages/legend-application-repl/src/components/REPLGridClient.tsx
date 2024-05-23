46b70e09fa5983960f908b05356d07c00db1d85f
An Phi <aphi@skidmore.edu>
2024-05-23 10:46:40 -0400

    Add more features to REPL Grid (#3184)
    Co-authored-by: Gayathri <rallapalle.gayathrichowdary@ny.email.gs.com>
    Co-authored-by: Gayathri Rallapalle <87973126+gayathrir11@users.noreply.github.com>

--- packages/legend-application-repl/src/components/REPLGridClient.tsx
+++ packages/legend-application-repl/src/components/REPLGridClient.tsx
@@ -15,12 +15,9 @@
  */
 
 import { observer } from 'mobx-react-lite';
-import {
-  useREPLGridClientStore,
-  withEditorStore,
-} from './REPLGridClientStoreProvider.js';
+import { useREPLGridClientStore } from './REPLGridClientStoreProvider.js';
 import { AgGridComponent } from './AgGrid.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { flowResult } from 'mobx';
 import { getTDSRowData } from '../components/grid/GridUtils.js';
 import { ServerSideDataSource } from '../components/grid/ServerSideDataSource.js';
@@ -31,18 +28,223 @@ import {
   CodeEditor,
 } from '@finos/legend-lego/code-editor';
 import {
+  BasePopover,
   CheckSquareIcon,
+  CogIcon,
+  Dialog,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalFooterButton,
+  ModalHeader,
+  ModalHeaderActions,
+  ModalTitle,
   PanelLoadingIndicator,
   PlayIcon,
+  PlusIcon,
+  SearchIcon,
   SquareIcon,
+  TimesIcon,
   clsx,
 } from '@finos/legend-art';
 import { LEGEND_APPLICATION_REPL_SETTING_KEY } from '../Const.js';
 
 import { QueryEditor } from './REPLQueryEditor.js';
+import { useParams } from '@finos/legend-application/browser';
+import {
+  LEGEND_REPL_GRID_CLIENT_PATTERN_TOKEN,
+  type REPLQueryEditorPathParams,
+} from './LegendREPLGridClientApplication.js';
+import { REPLGridClientStore } from '../stores/REPLGridClientStore.js';
+import { PIVOT_PANEL_TABS } from '../stores/REPLGridState.js';
+import { TDSSort, TDS_SORT_ORDER } from './grid/TDSRequest.js';
 
-export const Editor = withEditorStore(
-  observer(() => {
+const HPivotAndSortEditor = observer(
+  (props: { editorStore: REPLGridClientStore }) => {
+    const { editorStore } = props;
+    const columns = editorStore.replGridState.columns ?? [];
+    const [sortedColumns, setSortedColumns] = useState<TDSSort[]>(
+      editorStore.replGridState.lastQueryTDSRequest?.sort ?? [],
+    );
+    const [availableColumns, setAvailableColumns] = useState<TDSSort[]>(
+      columns
+        .filter((col) => !sortedColumns.find((c) => c.column === col))
+        .map((col) => new TDSSort(col, TDS_SORT_ORDER.ASCENDING)),
+    );
+    const [availableColSearchText, setAvailableColSearchText] = useState('');
+    return (
+      <div className="repl__hpivot__sort__editor">
+        <div className="repl__hpivot__sort__column__editor">
+          <div className="repl__hpivot__sort__column__editor__header">
+            Sorts
+          </div>
+          <div className="repl__hpivot__sort__column__editor__content">
+            <div className="repl__hpivot__sort__column__editor__available__columns">
+              <div className="repl__hpivot__sort__column__editor__description">
+                Available sort columns:
+              </div>
+              <div className="repl__hpivot__sort__column__editor__container">
+                <div className="query-builder-property-search-panel__header">
+                  <div className="query-builder-property-search-panel__input__container">
+                    <input
+                      // ref={searchInputRef}
+                      className={clsx(
+                        'query-builder-property-search-panel__input',
+                        {
+                          'query-builder-property-search-panel__input--searching':
+                            availableColSearchText,
+                        },
+                      )}
+                      spellCheck={false}
+                      // onChange={onSearchPropertyTextChange}
+                      value={availableColSearchText}
+                      placeholder="Search"
+                    />
+                    {!availableColSearchText ? (
+                      <>
+                        <div className="query-builder-property-search-panel__input__search__icon">
+                          <SearchIcon />
+                        </div>
+                      </>
+                    ) : (
+                      <button
+                        className="query-builder-property-search-panel__input__clear-btn"
+                        tabIndex={-1}
+                        // onClick={clearSearch}
+                        title="Clear"
+                      >
+                        <TimesIcon />
+                      </button>
+                    )}
+                  </div>
+                </div>
+                <div className="repl__hpivot__sort__column__editor__available__columns__content">
+                  <div className="repl__hpivot__sort__column__editor__available__columns__root">
+                    <PlusIcon />
+                    <div className="repl__hpivot__sort__column__editor__available__columns__root__label">
+                      All
+                    </div>
+                  </div>
+                  {availableColumns.map((col) => (
+                    <div
+                      className="repl__hpivot__sort__column__editor__available__columns__children"
+                      key={col.column}
+                    >
+                      {col.column}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="repl__hpivot__sort__column__editor__actions">
+              Actions
+            </div>
+            <div className="repl__hpivot__sort__column__editor__selected__columns">
+              <div className="repl__hpivot__sort__column__editor__description">
+                Selected sort columns:
+              </div>
+              <div className="repl__hpivot__sort__column__editor__container"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
+
+const PivotPanelEditor = observer(
+  (props: { editorStore: REPLGridClientStore }) => {
+    const { editorStore } = props;
+    const applicationStore = editorStore.applicationStore;
+    const closeEditor = (): void => {
+      editorStore.replGridState.setIsPivotPanelOpened(false);
+    };
+    const selectedTab = editorStore.replGridState.selectedPivotPanelTab;
+    const tabOptions = [
+      PIVOT_PANEL_TABS.COLUMNS_AND_PIVOTS,
+      PIVOT_PANEL_TABS.HPIVOTS_AND_SORTS,
+      PIVOT_PANEL_TABS.GENERAL_PROPERTIES,
+      PIVOT_PANEL_TABS.COLUMN_PROPERTIES,
+      PIVOT_PANEL_TABS.DEVELOPER_OPTIONS,
+      PIVOT_PANEL_TABS.PIVOT_LAYOUT,
+    ];
+    const setSelectedTab = (tab: PIVOT_PANEL_TABS): void => {
+      editorStore.replGridState.setSelectedPivotPanelTab(tab);
+    };
+    return (
+      <BasePopover
+        open={editorStore.replGridState.isPivotPanelOpened}
+        onClose={closeEditor}
+        // TransitionProps={{
+        //   onEnter: onSearchConfigMenuOpen,
+        // }}
+        // anchorEl={searchConfigTriggerRef.current}
+
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+      >
+        <Modal
+          darkMode={
+            !applicationStore.layoutService.TEMPORARY__isLightColorThemeEnabled
+          }
+          className="editor-modal embedded-runtime-editor"
+        >
+          <ModalHeader>
+            <ModalTitle title="cube properties" />
+            <ModalHeaderActions>
+              <button
+                className="modal__header__action"
+                tabIndex={-1}
+                onClick={closeEditor}
+              >
+                <TimesIcon />
+              </button>
+            </ModalHeaderActions>
+          </ModalHeader>
+          <ModalBody>
+            <div style={{ height: '100%', width: '100%' }}>
+              <div className="panel__header uml-element-editor__tabs__header ">
+                <div className="uml-element-editor__tabs">
+                  {tabOptions.map((tab) => (
+                    <div
+                      key={tab}
+                      onClick={(): void => setSelectedTab(tab)}
+                      className={clsx('uml-element-editor__tab', {
+                        'uml-element-editor__tab--active': tab === selectedTab,
+                      })}
+                    >
+                      {tab}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              {selectedTab === PIVOT_PANEL_TABS.HPIVOTS_AND_SORTS && (
+                <HPivotAndSortEditor editorStore={editorStore} />
+              )}
+            </div>
+          </ModalBody>
+          <ModalFooter className="repl__modal__footer">
+            <div className="search-modal__actions">
+              <ModalFooterButton text="Ok" />
+              <ModalFooterButton text="Close" />
+              <ModalFooterButton text="Apply" />
+            </div>
+          </ModalFooter>
+        </Modal>
+      </BasePopover>
+    );
+  },
+);
+
+export const GenericEditor = observer(
+  (props: { queryId?: string | undefined }) => {
+    const { queryId } = props;
     const editorStore = useREPLGridClientStore();
     const selectDarkTheme = (): void => {
       editorStore.applicationStore.layoutService.setColorTheme(
@@ -66,6 +268,12 @@ export const Editor = withEditorStore(
       );
     };
 
+    const saveQuery = (): void => {
+      flowResult(editorStore.saveQuery()).catch(
+        editorStore.applicationStore.alertUnhandledError,
+      );
+    };
+
     const togglePagination = (): void => {
       editorStore.replGridState.setIsPaginationEnabled(
         !editorStore.replGridState.isPaginationEnabled,
@@ -77,10 +285,10 @@ export const Editor = withEditorStore(
     };
 
     useEffect(() => {
-      flowResult(editorStore.getInitialREPLGridServerResult()).catch(
+      flowResult(editorStore.getInitialREPLGridServerResult(queryId)).catch(
         editorStore.applicationStore.alertUnhandledError,
       );
-    }, [editorStore]);
+    }, [editorStore, queryId]);
 
     return (
       <div className="repl">
@@ -88,6 +296,14 @@ export const Editor = withEditorStore(
           <div className="repl__header__content">
             <div className="repl__header__content__title">REPL Grid</div>
             <div className="repl__header__actions">
+              <div
+                className="repl__header__action__pagination"
+                onClick={(): void => saveQuery()}
+              >
+                <div className="repl__header__action__pagination__label">
+                  Save Query
+                </div>
+              </div>
               <div className="repl__header__action__pagination">
                 <button
                   className={clsx(
@@ -192,6 +408,9 @@ export const Editor = withEditorStore(
           />
           {editorStore.executeAction.hasCompleted && (
             <AgGridComponent
+              onGridReady={(params): void => {
+                editorStore.replGridState.setGridApi(params.api);
+              }}
               className={
                 editorStore.applicationStore.layoutService
                   .TEMPORARY__isLightColorThemeEnabled
@@ -220,7 +439,30 @@ export const Editor = withEditorStore(
             />
           )}
         </div>
+        <div className="repl__footer">
+          <div
+            className="repl__footer__pivot"
+            onClick={(): void =>
+              editorStore.replGridState.setIsPivotPanelOpened(true)
+            }
+          >
+            Pivot
+          </div>
+          <div className="repl__footer__filter">Filter</div>
+          <div className="repl__footer__mode">Modes</div>
+          {editorStore.replGridState.isPivotPanelOpened && (
+            <PivotPanelEditor editorStore={editorStore} />
+          )}
+        </div>
       </div>
     );
-  }),
+  },
 );
+
+export const Editor = observer(() => <GenericEditor />);
+
+export const REPLQueryEditor = observer(() => {
+  const params = useParams<REPLQueryEditorPathParams>();
+  const queryId = params[LEGEND_REPL_GRID_CLIENT_PATTERN_TOKEN.QUERY_ID];
+  return <GenericEditor queryId={queryId} />;
+});
--- packages/legend-application-repl/src/stores/REPLGridState.ts
+++ packages/legend-application-repl/src/stores/REPLGridState.ts
@@ -17,13 +17,32 @@
 import { guaranteeNonNullable } from '@finos/legend-shared';
 import {
   getAggregationTDSColumnCustomizations,
+  getFilterModeltype,
+  getTDSColumnCustomizations,
   getTDSRowData,
   type TDSRowDataType,
 } from '../components/grid/GridUtils.js';
 import { action, computed, makeObservable, observable } from 'mobx';
-import type { ColDef } from '@ag-grid-community/core';
+import type { ColDef, GridApi } from '@ag-grid-community/core';
 import type { V1_Lambda, TDSExecutionResult } from '@finos/legend-graph';
 import { QueryEditorState } from '../components/REPLQueryEditor.js';
+import {
+  TDS_FILTER_GROUP,
+  type TDSRequest,
+} from '../components/grid/TDSRequest.js';
+
+interface FilterModel {
+  [key: string]: object;
+}
+
+export enum PIVOT_PANEL_TABS {
+  COLUMNS_AND_PIVOTS = 'Colums/Pivots',
+  HPIVOTS_AND_SORTS = 'HPivots/Sorts',
+  GENERAL_PROPERTIES = 'General Properties',
+  COLUMN_PROPERTIES = 'Column Properties',
+  DEVELOPER_OPTIONS = 'Developer',
+  PIVOT_LAYOUT = 'Pivot Layout',
+}
 
 export class REPLGridState {
   initialResult?: TDSExecutionResult | undefined;
@@ -34,6 +53,13 @@ export class REPLGridState {
   licenseKey?: string | undefined;
   initialQueryLambda?: V1_Lambda | undefined;
   isPaginationEnabled!: boolean;
+  currentQueryTDSRequest?: TDSRequest | undefined;
+  lastQueryTDSRequest?: TDSRequest | undefined;
+
+  gridApi?: GridApi | undefined;
+
+  isPivotPanelOpened = false;
+  selectedPivotPanelTab = PIVOT_PANEL_TABS.COLUMNS_AND_PIVOTS;
 
   constructor(isPaginationEnabled: boolean) {
     makeObservable(this, {
@@ -45,6 +71,14 @@ export class REPLGridState {
       columns: observable,
       isPaginationEnabled: observable,
       queryEditorState: observable,
+      currentQueryTDSRequest: observable,
+      lastQueryTDSRequest: observable,
+      gridApi: observable,
+      isPivotPanelOpened: observable,
+      selectedPivotPanelTab: observable,
+      setSelectedPivotPanelTab: action,
+      setIsPivotPanelOpened: action,
+      setGridApi: action,
       setInitialResult: action,
       setCurrentSubQuery: action,
       setColumns: action,
@@ -52,12 +86,34 @@ export class REPLGridState {
       setCurrentResult: action,
       setLicenseKey: action,
       setIsPaginationEnabled: action,
+      setCurrentQueryTDSRequest: action,
+      setLastQueryTDSRequest: action,
       rowData: computed,
       columnDefs: computed,
     });
 
     this.queryEditorState = new QueryEditorState('');
     this.isPaginationEnabled = isPaginationEnabled;
+  }
+
+  setSelectedPivotPanelTab(val: PIVOT_PANEL_TABS): void {
+    this.selectedPivotPanelTab = val;
+  }
+
+  setIsPivotPanelOpened(val: boolean): void {
+    this.isPivotPanelOpened = val;
+  }
+
+  setGridApi(val: GridApi | undefined): void {
+    this.gridApi = val;
+  }
+
+  setLastQueryTDSRequest(val: TDSRequest | undefined): void {
+    this.lastQueryTDSRequest = val;
+  }
+
+  setCurrentQueryTDSRequest(val: TDSRequest | undefined): void {
+    this.currentQueryTDSRequest = val;
   }
 
   setInitialQueryLambda(val: V1_Lambda | undefined): void {
@@ -93,14 +149,47 @@ export class REPLGridState {
   }
 
   get columnDefs(): ColDef[] {
+    const filterModel: FilterModel = {};
+    this.currentQueryTDSRequest?.filter.forEach((filter) => {
+      if (filter.conditions.length === 1) {
+        filterModel[filter.column] = {
+          filter: filter.conditions[0]?.value,
+          filterType: getFilterModeltype(filter.columnType),
+          type: filter.conditions[0]?.operation,
+        };
+      } else {
+        filterModel[filter.column] = {
+          filterType: getFilterModeltype(filter.columnType),
+          operator:
+            filter.groupOperation === TDS_FILTER_GROUP.AND ? 'AND' : 'OR',
+          condition1: {
+            filter: filter.conditions[0]?.value,
+            filterType: getFilterModeltype(filter.columnType),
+            type: filter.conditions[0]?.operation,
+          },
+          condition2: {
+            filter: filter.conditions[1]?.value,
+            filterType: getFilterModeltype(filter.columnType),
+            type: filter.conditions[1]?.operation,
+          },
+        };
+      }
+    });
+    if (this.currentQueryTDSRequest) {
+      this.gridApi?.setFilterModel(filterModel);
+    }
     return this.columns
       ? this.columns.map((c) => ({
           field: c,
           headerName: c,
           ...getAggregationTDSColumnCustomizations(
-            this.licenseKey ? true : false,
             guaranteeNonNullable(this.initialResult),
             c,
+          ),
+          ...getTDSColumnCustomizations(
+            guaranteeNonNullable(this.initialResult),
+            c,
+            this.currentQueryTDSRequest,
           ),
         }))
       : [];
