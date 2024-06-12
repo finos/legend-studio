723936af5cdd39563e06beba32b088ff81da68f4
Gayathri <rallapalle.gayathrichowdary@ny.email.gs.com>
2024-06-12 12:48:45 +0530

    Add back groupBy feature to DataCube after refactoring

--- packages/legend-application-repl/src/stores/dataCube/DataCubeMetaModelConst.ts
+++ packages/legend-application-repl/src/stores/dataCube/DataCubeMetaModelConst.ts
@@ -55,6 +55,21 @@ export enum DATA_CUBE_AGGREGATE_FUNCTION {
   LAST = 'last',
 }
 
+export enum DATA_CUBE_FILTER_OPERATION {
+  EQUALS = 'equal',
+  NOT_EQUAL = 'notEqual',
+  GREATER_THAN = 'greaterThan',
+  GREATER_THAN_OR_EQUAL = 'greaterThanOrEqual',
+  LESS_THAN = 'lessThan',
+  LESS_THAN_OR_EQUAL = 'lessThanOrEqual',
+  BLANK = 'isEmpty',
+  NOT_BLANK = 'isNotEmpty',
+  CONTAINS = 'contains',
+  NOT_CONTAINS = 'notContains',
+  STARTS_WITH = 'startsWith',
+  ENDS_WITH = 'endsWith',
+}
+
 export enum DATA_CUBE_COLUMN_SORT_DIRECTION {
   ASCENDING = 'ascending',
   DESCENDING = 'descending',
