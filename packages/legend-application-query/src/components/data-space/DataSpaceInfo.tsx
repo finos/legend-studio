d21d4d1c15f375e69f819b6b3d6e2ca5563fc684
Yannan <73408381+YannanGao-gs@users.noreply.github.com>
2024-06-21 15:46:29 -0400

    Display connection and runtime info in About Dataspace

--- packages/legend-application-query/src/components/data-space/DataSpaceInfo.tsx
+++ packages/legend-application-query/src/components/data-space/DataSpaceInfo.tsx
@@ -24,13 +24,21 @@ import {
 import { StoreProjectData } from '@finos/legend-server-depot';
 import { observer } from 'mobx-react-lite';
 import { type QueryEditorStore } from '../../stores/QueryEditorStore.js';
-import type {
-  DataSpace,
-  DataSpaceExecutionContext,
+import {
+  type DataSpace,
+  type DataSpaceExecutionContext,
+  DataSpaceSupportEmail,
 } from '@finos/legend-extension-dsl-data-space/graph';
-import { EXTERNAL_APPLICATION_NAVIGATION__generateStudioSDLCProjectViewUrl } from '../../__lib__/LegendQueryNavigation.js';
+import {
+  EXTERNAL_APPLICATION_NAVIGATION__generateStudioProjectViewUrl,
+  EXTERNAL_APPLICATION_NAVIGATION__generateStudioSDLCProjectViewUrl,
+} from '../../__lib__/LegendQueryNavigation.js';
 import { flowResult } from 'mobx';
 import { assertErrorThrown } from '@finos/legend-shared';
+import {
+  ConnectionPointer,
+  RelationalDatabaseConnection,
+} from '@finos/legend-graph';
 
 export const QueryEditorDataspaceInfoModal = observer(
   (props: {
@@ -68,6 +76,13 @@ export const QueryEditorDataspaceInfoModal = observer(
       }
     };
 
+    const connection =
+      executionContext.defaultRuntime.value.runtimeValue.connections[0]
+        ?.storeConnections[0]?.connection instanceof ConnectionPointer
+        ? executionContext.defaultRuntime.value.runtimeValue.connections[0]
+            ?.storeConnections[0]?.connection
+        : undefined;
+
     return (
       <Dialog
         open={open}
@@ -92,7 +107,28 @@ export const QueryEditorDataspaceInfoModal = observer(
                   <div className="dataspace-info-modal__field__label">
                     Project
                   </div>
-                  <div className="dataspace-info-modal__field__value">
+                  <div
+                    className="dataspace-info-modal__field__value dataspace-info-modal__field__value--linkable"
+                    onClick={() => {
+                      try {
+                        editorStore.applicationStore.navigationService.navigator.visitAddress(
+                          EXTERNAL_APPLICATION_NAVIGATION__generateStudioProjectViewUrl(
+                            editorStore.applicationStore.config
+                              .studioApplicationUrl,
+                            projectInfo.groupId,
+                            projectInfo.artifactId,
+                            projectInfo.versionId,
+                            undefined,
+                          ),
+                        );
+                      } catch (error) {
+                        assertErrorThrown(error);
+                        editorStore.applicationStore.notificationService.notifyError(
+                          `Can't visit project: '${projectInfo.groupId}:${projectInfo.artifactId}:${projectInfo.versionId}'`,
+                        );
+                      }
+                    }}
+                  >
                     {`${projectInfo.groupId}:${projectInfo.artifactId}:${projectInfo.versionId}`}
                   </div>
                 </div>
@@ -141,6 +177,43 @@ export const QueryEditorDataspaceInfoModal = observer(
                   {executionContext.defaultRuntime.value.name}
                 </div>
               </div>
+              {connection && (
+                <>
+                  <div className="dataspace-info-modal__field">
+                    <div className="dataspace-info-modal__field__label">
+                      Store
+                    </div>
+                    <div
+                      className="dataspace-info-modal__field__value  dataspace-info-modal__field__value--linkable"
+                      onClick={() =>
+                        flowResult(visitElement(connection.store.value.path))
+                      }
+                    >
+                      {connection.store.value.name}
+                    </div>
+                  </div>
+                  <div className="dataspace-info-modal__field">
+                    <div className="dataspace-info-modal__field__label">
+                      Connection
+                    </div>
+                    <div
+                      className="dataspace-info-modal__field__value  dataspace-info-modal__field__value--linkable"
+                      onClick={() =>
+                        flowResult(
+                          visitElement(
+                            connection.packageableConnection.value.path,
+                          ),
+                        )
+                      }
+                    >
+                      {connection.packageableConnection.value
+                        .connectionValue instanceof RelationalDatabaseConnection
+                        ? `${connection.packageableConnection.value.name}:${connection.packageableConnection.value.connectionValue.type}`
+                        : connection.packageableConnection.value.name}
+                    </div>
+                  </div>
+                </>
+              )}
               <div className="dataspace-info-modal__field">
                 <div className="dataspace-info-modal__field__label">
                   Configuration
@@ -152,16 +225,16 @@ export const QueryEditorDataspaceInfoModal = observer(
                   Show Dataspace Configuration
                 </div>
               </div>
-              {dataspace.supportInfo?.documentationUrl && (
+              {dataspace.supportInfo instanceof DataSpaceSupportEmail && (
                 <div className="dataspace-info-modal__field">
                   <div className="dataspace-info-modal__field__label">
                     Support Email
                   </div>
                   <a
                     className="dataspace-info-modal__field__value dataspace-info-modal__field__value--linkable"
-                    href={`mailto:${dataspace.supportInfo.documentationUrl}`}
+                    href={`mailto:${dataspace.supportInfo.address}`}
                   >
-                    {dataspace.supportInfo.documentationUrl}
+                    {dataspace.supportInfo.address}
                   </a>
                 </div>
               )}
