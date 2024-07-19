8a90eb51dbf3e773aa2ed3eb378bd38f2b07901d
Jake Atakhanov <jakeatakhanov@MacBook-Pro.local>
2024-07-18 20:25:54 -0400

    form with title and description implemented

---
+++ packages/legend-extension-dsl-data-space-studio/src/components/FormTextEditor.tsx
@@ -0,0 +1,80 @@
+import { observer } from 'mobx-react-lite';
+import { useEditorStore } from '@finos/legend-application-studio';
+import { DataSpaceEditorState } from '../stores/DataSpaceEditorState.js';
+import { useRef, useEffect } from 'react';
+import { PanelFormTextField, PanelFormSection } from '@finos/legend-art';
+
+export const FormTextEditor = observer(() => {
+  const editorStore = useEditorStore();
+  const currentTabState = editorStore.tabManagerState.currentTab;
+  const typeNameRef = useRef<HTMLInputElement>(null);
+
+  if (!(currentTabState instanceof DataSpaceEditorState)) {
+    return null;
+  }
+
+  const formEditorState =
+    editorStore.tabManagerState.getCurrentEditorState(DataSpaceEditorState);
+
+  const formElement = formEditorState.dataSpace;
+  const isReadOnly = formEditorState.isReadOnly;
+
+  useEffect(() => {
+    if (!isReadOnly) {
+      typeNameRef.current?.focus();
+    }
+  }, [isReadOnly]);
+
+  const handleTitleChange = (value: string | undefined) => {
+    formEditorState.setTitle(value ?? '');
+  };
+
+  const handleDescriptionChange = (value: string | undefined) => {
+    formEditorState.setDescription(value ?? '');
+  };
+
+  // const handleDescriptionChange = (
+  //   e: React.ChangeEvent<HTMLTextAreaElement>,
+  // ) => {
+  //   formEditorState.setDescription(e.target.value);
+  // };
+
+  return (
+    <div className="form-text-editor panel text-element-editor">
+      <div className="panel__header text-element-editor__header">
+        <div
+          className="text-element-editor__header__configs"
+          style={{ position: 'relative', top: '200px', height: '200px' }}
+        >
+          <div className="panel__content">
+            <PanelFormSection>
+              <div>
+                <PanelFormTextField
+                  name="Title"
+                  value={formElement.title ?? ''}
+                  update={handleTitleChange}
+                  placeholder="Enter title"
+                />
+              </div>
+              <div>
+                <PanelFormTextField
+                  name="Description"
+                  value={formElement.description ?? ''}
+                  update={handleDescriptionChange}
+                  placeholder="Enter description"
+                />
+                {/* <textarea
+
+                  spellCheck={false}
+                  disabled={isReadOnly}
+                  value={formElement.description ?? ''}
+                  onChange={handleDescriptionChange}
+                /> */}
+              </div>
+            </PanelFormSection>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
---
+++ packages/legend-extension-dsl-data-space-studio/src/stores/DataSpaceEditorState.ts
@@ -0,0 +1,54 @@
+import { guaranteeType } from '@finos/legend-shared';
+import { action, computed, makeObservable, observable } from 'mobx';
+import {
+  type EditorStore,
+  ElementEditorState,
+} from '@finos/legend-application-studio';
+import type { PackageableElement } from '@finos/legend-graph';
+import { DataSpace } from '@finos/legend-extension-dsl-data-space/graph';
+
+export class DataSpaceEditorState extends ElementEditorState {
+  dataSpace: DataSpace;
+
+  constructor(editorStore: EditorStore, element: PackageableElement) {
+    super(editorStore, element);
+    this.dataSpace = guaranteeType(
+      element,
+      DataSpace,
+      'Element inside DataSpaceEditorState must be a DataSpace',
+    );
+
+    makeObservable(this, {
+      dataSpace: observable,
+      title: computed,
+      setTitle: action,
+      setDescription: action,
+    });
+  }
+
+  get title(): string {
+    return this.dataSpace.title ?? '';
+  }
+
+  override get description(): string {
+    return this.dataSpace.description ?? '';
+  }
+
+  setTitle(title: string): void {
+    this.dataSpace.title = title;
+  }
+
+  setDescription(description: string): void {
+    this.dataSpace.description = description;
+  }
+
+  override reprocess(
+    newElement: PackageableElement,
+    editorStore: EditorStore,
+  ): ElementEditorState {
+    const newState = new DataSpaceEditorState(editorStore, newElement);
+    newState.setTitle(this.title);
+    newState.setDescription(this.description);
+    return newState;
+  }
+}
