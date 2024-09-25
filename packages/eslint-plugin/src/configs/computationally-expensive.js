e1b41d6ec53baa037ab0ffe886d27eb3570ab35a
An Phi <aphi@skidmore.edu>
2024-09-25 10:19:11 -0400

    update eslint rules

--- packages/eslint-plugin/src/configs/computationally-expensive.js
+++ packages/eslint-plugin/src/configs/computationally-expensive.js
@@ -26,13 +26,6 @@ const IMPORT_RULES = {
 };
 
 const TYPESCRIPT_RULES = {
-  // NOTE: following rules are classified as `type-aware` linting rule, which has huge initial performance impact on linting
-  // They require parserServices to be generated so we have to specify 'parserOptions.project' property for @typescript-esint/parser
-  // See https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-type-checked.ts
-  '@typescript-eslint/consistent-type-exports': [
-    ERROR,
-    { fixMixedExportsWithInlineTypeSpecifier: true },
-  ],
   '@typescript-eslint/prefer-nullish-coalescing': [
     ERROR,
     {
@@ -51,24 +44,21 @@ const TYPESCRIPT_RULES = {
 
   '@typescript-eslint/no-implied-eval': ERROR,
   '@typescript-eslint/await-thenable': ERROR,
+
+  '@typescript-eslint/unbound-method': WARN,
   '@typescript-eslint/no-redundant-type-constituents': WARN,
+
   // NOTE: since we turn on TS option --exactOptionalPropertyTypes, this rule mistakenly flags ?: ... | undefined as violation
   // so we temporarily turn it off
   // See https://github.com/typescript-eslint/typescript-eslint/issues/9203
   '@typescript-eslint/no-duplicate-type-constituents': OFF,
-  '@typescript-eslint/unbound-method': WARN,
 
   // The following rules are recommended but we need to disable them since we deem them unecessarily strict
   // See https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-type-checked.ts
-  // '@typescript-eslint/require-await': OFF, // in tests, we use `await act()` in various place and this rule is not appropriate there
-  // '@typescript-eslint/restrict-plus-operands': OFF,
-  // '@typescript-eslint/restrict-template-expressions': OFF,
-  // '@typescript-eslint/no-base-to-string': OFF,
-  // '@typescript-eslint/no-unsafe-call': OFF,
-  // '@typescript-eslint/no-unsafe-argument': OFF,
-  // '@typescript-eslint/no-unsafe-member-access': OFF,
-  // '@typescript-eslint/no-unsafe-enum-comparison': OFF, // we compare enum with strings a lot for valid reasons so this rule should be disabled
-  // '@typescript-eslint/no-unsafe-return': OFF, // we use `any` return type a lot for valid reasons so this rule should be disabled
+  '@typescript-eslint/no-unsafe-argument': OFF,
+  '@typescript-eslint/no-unsafe-member-access': OFF,
+  '@typescript-eslint/no-unsafe-enum-comparison': OFF, // we compare enum with strings a lot for valid reasons so this rule should be disabled
+  '@typescript-eslint/no-unsafe-return': OFF, // we use `any` return type a lot for valid reasons so this rule should be disabled
 };
 
 /**
