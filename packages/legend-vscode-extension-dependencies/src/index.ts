61e8c46c6a5ab3373b264d676dd16d432e217e3d
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-01-14 10:38:53 -0500

    fix: Build vscode extension dependencies sass file and fix import

--- packages/legend-vscode-extension-dependencies/src/index.ts
+++ packages/legend-vscode-extension-dependencies/src/index.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import '../style/index.scss';
+import '../lib/index.css'; // eslint-disable-line @finos/legend/no-cross-workspace-non-export-usage
 // NOTE: tailwind style takes precedence over other styles since it's generated and we should not allow
 // other styles to override it
 import '../lib/tailwind.css'; // eslint-disable-line @finos/legend/no-cross-workspace-non-export-usage
