916fa82a940890aa58a701deb634c9f0c07136b2
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-01-14 17:00:41 -0500

    feat: Move index.scss from style to src folder

--- packages/legend-vscode-extension-dependencies/src/index.ts
+++ packages/legend-vscode-extension-dependencies/src/index.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import '../lib/index.css'; // eslint-disable-line @finos/legend/no-cross-workspace-non-export-usage
+import '../src/index.css'; // eslint-disable-line @finos/legend/no-cross-workspace-source-usage
 // NOTE: tailwind style takes precedence over other styles since it's generated and we should not allow
 // other styles to override it
 import '../lib/tailwind.css'; // eslint-disable-line @finos/legend/no-cross-workspace-non-export-usage
