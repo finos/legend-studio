0a50efa020d87fce5d19b0dd1c94ef9793e7ad57
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-03-07 17:00:19 -0500

    feat: Add skeleton code to legend-application-catalog-bootstrap

---
+++ packages/legend-application-catalog-bootstrap/src/index.tsx
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2020-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { LegendCatalog } from '@finos/legend-application-catalog';
+import {
+  type AbstractPreset,
+  type AbstractPlugin,
+  WebConsole,
+} from '@finos/legend-shared';
+
+export class LegendCatalogWebApplication {
+  static getPresetCollection(): AbstractPreset[] {
+    return [];
+  }
+
+  static getPluginCollection(): AbstractPlugin[] {
+    return [
+      // loggers
+      new WebConsole(),
+    ];
+  }
+
+  static run(baseUrl: string): void {
+    LegendCatalog.create()
+      .setup({ baseAddress: baseUrl })
+      .withPresets(LegendCatalogWebApplication.getPresetCollection())
+      .withPlugins(LegendCatalogWebApplication.getPluginCollection())
+      .withDownloadHelper()
+      .start()
+      .catch((e: unknown) => {
+        throw e;
+      });
+  }
+}
