a4d12976c5b2b5322a2d2e90af84e825849b455c
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-03-11 10:55:46 -0400

    fix: Add missing copyrights and remove unused imports

--- packages/legend-application-catalog/src/components/header/LegendCatalogAppInfo.tsx
+++ packages/legend-application-catalog/src/components/header/LegendCatalogAppInfo.tsx
@@ -1,9 +1,23 @@
-import { observer } from 'mobx-react-lite';
+/**
+ * Copyright (c) 2020-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import type React from 'react';
 import { useLegendCatalogApplicationStore } from '../LegendCatalogFrameworkProvider.js';
 import { isNonNullable } from '@finos/legend-shared';
 import {
-  BlankPanelContent,
   CopyIcon,
   Dialog,
   InfoCircleIcon,
@@ -13,7 +27,6 @@ import {
   ModalHeaderActions,
   ModalTitle,
   TimesIcon,
-  TreeView,
 } from '@finos/legend-art';
 
 export const LegendCatalogAppInfo: React.FC<{
