712a82e5acfe6cb1ec7c2e8658916f6d54761a3b
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-03-17 10:43:36 -0400

    feat: Move elements section into its own file

--- packages/legend-extension-dsl-data-space-studio/src/components/DataSpaceGeneralEditor/DataSpaceGeneralEditor.tsx
+++ packages/legend-extension-dsl-data-space-studio/src/components/DataSpaceGeneralEditor/DataSpaceGeneralEditor.tsx
@@ -16,7 +16,6 @@
 
 import { useEditorStore } from '@finos/legend-application-studio';
 import {
-  Checkbox,
   CustomSelectorInput,
   ListEditor,
   PanelContentLists,
@@ -27,7 +26,6 @@ import {
 import { observer } from 'mobx-react-lite';
 import { DataSpaceEditorState } from '../../stores/DataSpaceEditorState.js';
 import {
-  dataSpace_addElement,
   dataSpace_addExecutable,
   dataSpace_combined_addEmail,
   dataSpace_combined_deleteEmail,
@@ -35,25 +33,21 @@ import {
   dataSpace_combined_setSupportUrl,
   dataSpace_combined_setWebsite,
   dataSpace_email_setSupportInfoEmail,
-  dataSpace_removeElement,
   dataSpace_removeExecutable,
   dataSpace_setDescription,
   dataSpace_setDocumentationUrl,
-  dataSpace_setElementExclude,
   dataSpace_setSupportInfo,
   dataSpace_setTitle,
 } from '../../stores/studio/DSL_DataSpace_GraphModifierHelper.js';
 import {
-  type DataSpaceElement,
-  DataSpaceElementPointer,
   DataSpacePackageableElementExecutable,
   DataSpaceSupportCombinedInfo,
   DataSpaceSupportEmail,
 } from '@finos/legend-extension-dsl-data-space/graph';
-import { PackageableElementExplicitReference } from '@finos/legend-graph';
 import { useState } from 'react';
 import { DataSpaceDefaultExecutionContextSection } from './DataSpaceDefaultExecutionContextSection.js';
 import { DataSpaceDiagramsSection } from './DataSpaceDiagramsSection.js';
+import { DataSpaceElementsSection } from './DataSpaceElementsSection.js';
 
 export const DataSpaceGeneralEditor = observer(() => {
   const editorStore = useEditorStore();
@@ -69,31 +63,6 @@ export const DataSpaceGeneralEditor = observer(() => {
 
   const handleDescriptionChange = (value: string | undefined): void => {
     dataSpace_setDescription(dataSpace, value);
-  };
-
-  // Elements handlers
-  const handleAddElement = (option: {
-    label: string;
-    value: DataSpaceElement;
-  }): void => {
-    if (option && option.value && typeof option.value === 'object') {
-      const element = option.value;
-      const elementPointer = new DataSpaceElementPointer();
-      elementPointer.element =
-        PackageableElementExplicitReference.create(element);
-      dataSpace_addElement(dataSpace, elementPointer);
-    }
-  };
-
-  const handleRemoveElement = (element: DataSpaceElementPointer): void => {
-    dataSpace_removeElement(dataSpace, element);
-  };
-
-  const handleElementExcludeChange = (
-    element: DataSpaceElementPointer,
-    event: React.ChangeEvent<HTMLInputElement>,
-  ): void => {
-    dataSpace_setElementExclude(element, event.target.checked);
   };
 
   // Executables handlers
@@ -150,54 +119,6 @@ export const DataSpaceGeneralEditor = observer(() => {
       dataSpace_combined_deleteEmail(dataSpace.supportInfo, email);
     }
   };
-
-  const ElementComponent = observer(
-    (props: { item: DataSpaceElementPointer }): React.ReactElement => {
-      const { item } = props;
-
-      return (
-        <div className="panel__content__form__section__list__item__content">
-          <div className="panel__content__form__section__list__item__content__label">
-            {item.element?.value?.path ?? 'Unknown Element'}
-          </div>
-          <div className="panel__content__form__section__list__item__content__actions">
-            <div className="panel__content__form__section__list__item__content__actions-exclude">
-              <Checkbox
-                disabled={dataSpaceState.isReadOnly}
-                checked={item.exclude ?? false}
-                onChange={(event) => handleElementExcludeChange(item, event)}
-                size="small"
-                className="panel__content__form__section__list__item__content__actions-exclude__btn"
-              />
-              <span className="panel__content__form__section__list__item__content__actions__label">
-                Exclude
-              </span>
-            </div>
-          </div>
-        </div>
-      );
-    },
-  );
-
-  const NewElementComponent = observer(
-    (props: { onFinishEditing: () => void }) => {
-      const { onFinishEditing } = props;
-
-      return (
-        <div className="panel__content__form__section__list__new-item__input">
-          <CustomSelectorInput
-            options={dataSpaceState.getDataSpaceElementOptions()}
-            onChange={(event: { label: string; value: DataSpaceElement }) => {
-              onFinishEditing();
-              handleAddElement(event);
-            }}
-            placeholder="Select an element to add..."
-            darkMode={true}
-          />
-        </div>
-      );
-    },
-  );
 
   const SupportEmailComponent = observer(
     (props: { item: string }): React.ReactElement => {
@@ -270,22 +191,7 @@ export const DataSpaceGeneralEditor = observer(() => {
           />
         </PanelFormSection>
         <DataSpaceDefaultExecutionContextSection />
-        {/* Elements Section */}
-        <PanelFormSection className="dataSpace-editor__general__elements">
-          <ListEditor
-            title="Elements"
-            prompt="Add elements to include in this Data Space. Use the exclude checkbox to exclude elements."
-            items={dataSpace.elements}
-            keySelector={(element: DataSpaceElementPointer) =>
-              element.element.value.path
-            }
-            ItemComponent={ElementComponent}
-            NewItemComponent={NewElementComponent}
-            handleRemoveItem={handleRemoveElement}
-            isReadOnly={dataSpaceState.isReadOnly}
-            emptyMessage="No elements specified"
-          />
-        </PanelFormSection>
+        <DataSpaceElementsSection />
         {/* Executables Section */}
         {/* <PanelFormListItems title="Executables">
             {formElement.executables?.map((executable, index) => (
