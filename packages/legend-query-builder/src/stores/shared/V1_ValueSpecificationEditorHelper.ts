c89f2c6b232e807d2cda81d9aeccbe165e21656d
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-03-31 17:07:35 +0000

    feat: Add errorChecker to V1_BasicValueSpecificaitonEditor

--- packages/legend-query-builder/src/stores/shared/V1_ValueSpecificationEditorHelper.ts
+++ packages/legend-query-builder/src/stores/shared/V1_ValueSpecificationEditorHelper.ts
@@ -16,6 +16,7 @@
 
 import {
   type V1_ValueSpecification,
+  PRIMITIVE_TYPE,
   V1_AppliedFunction,
   V1_AppliedProperty,
   V1_CBoolean,
@@ -39,6 +40,11 @@ import type {
   LegendApplicationPluginManager,
   LegendApplicationPlugin,
 } from '@finos/legend-application';
+
+export const isPritiveType = (type: string): boolean =>
+  !Object.values(PRIMITIVE_TYPE)
+    .map((type) => type.toString())
+    .includes(type);
 
 export const getV1_ValueSpecificationStringValue = (
   valueSpecification: V1_ValueSpecification,
