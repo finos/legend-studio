3054f5a9d210a21bacc9d1d5300a9ce035f223a3
Bojja, Srinivas [Engineering] <srinivas.bojja@ny.email.gs.com>
2025-06-23 20:12:20 +0000

    Legend Marketplace Home products display and API response fix

--- packages/legend-server-marketplace/src/models/Provider.ts
+++ packages/legend-server-marketplace/src/models/Provider.ts
@@ -31,6 +31,7 @@ export class ProviderResult {
   description!: string;
   price!: number;
   isOwned?: boolean;
+  profileId?: string;
 
   static readonly serialization = new SerializationFactory(
     createModelSchema(ProviderResult, {
@@ -41,6 +42,12 @@ export class ProviderResult {
       description: primitive(),
       price: primitive(),
       isOwned: optional(primitive()),
+      profileId: optional(primitive()),
     }),
   );
+}
+
+export interface Filter {
+  label: string;
+  value: string;
 }
