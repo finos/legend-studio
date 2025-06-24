677f9b23b5334eb53278d5de92f4ed985eed503e
Travis Stebbins <travis.stebbins@ny.email.gs.com>
2025-06-24 12:28:19 -0700

    Remove erroneous text in LakehouseEntitlements (#4227)

--- packages/legend-application-marketplace/src/pages/Lakehouse/entitlements/LakehouseEntitlements.tsx
+++ packages/legend-application-marketplace/src/pages/Lakehouse/entitlements/LakehouseEntitlements.tsx
@@ -114,7 +114,6 @@ export const LakehouseEntitlements = withLakehouseEntitlementsStore(
             />
           )}
         </Container>
-        ={' '}
       </LegendMarketplacePage>
     );
   }),
