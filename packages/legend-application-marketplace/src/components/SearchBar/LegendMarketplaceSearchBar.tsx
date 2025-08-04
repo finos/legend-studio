5814eb4c5e74e72f875cb6c8bc498c65fdafa6ab
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-08-04 15:34:40 -0400

    feat: Adjust search bar styling

--- packages/legend-application-marketplace/src/components/SearchBar/LegendMarketplaceSearchBar.tsx
+++ packages/legend-application-marketplace/src/components/SearchBar/LegendMarketplaceSearchBar.tsx
@@ -58,7 +58,9 @@ export const LegendMarketplaceSearchBar = (props: {
             className: 'legend-marketplace__search-bar__input',
             endAdornment: (
               <InputAdornment position="end">
-                <SearchIcon />
+                <SearchIcon
+                  onClick={() => onSearch?.(undefined, searchQuery)}
+                />
               </InputAdornment>
             ),
           },
