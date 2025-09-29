0cf180bd1009a6bb16222747202a94ea1dc23591
Mauricio Uyaguari <lmauricio12@gmail.com>
2025-09-29 15:29:49 -0400

    back to green

--- packages/legend-data-cube/src/stores/core/__tests__/DataCubeQueryRoundtrip.data-cube-test.ts
+++ packages/legend-data-cube/src/stores/core/__tests__/DataCubeQueryRoundtrip.data-cube-test.ts
@@ -1241,7 +1241,7 @@ const cases: TestCase[] = [
   _case(`Group-level Extend: ERROR - expression with compilation issue`, {
     query: `select(~[val])->extend(~[a:x|$x.val + '_123'])`,
     columns: ['val:Integer'],
-    error: `Can't process extend() expression: failed to retrieve type information for columns. Error: Collection element must have a multiplicity [1] - Context:[Processing return type for lambda, new lambda, Applying extend, new lambda, Applying plus], multiplicity:[0..1]'`,
+    error: `Can't process extend() expression: failed to retrieve type information for columns. Error: Collection element must have a multiplicity [1] - Context:[Processing return type for lambda, new lambda, Applying extend, new lambda, Applying plus], multiplicity:[0..1]`,
   }),
 
   // --------------------------------- SORT ---------------------------------
