6ec36dc7cc0937f59eb59383c8ae8ce2e0eb57ff
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-10-31 10:21:26 -0400

    refactor: Put tests in describe block

--- packages/legend-extension-dsl-data-product/src/components/__tests__/EntitlementsDataContractViewer.test.tsx
+++ packages/legend-extension-dsl-data-product/src/components/__tests__/EntitlementsDataContractViewer.test.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { expect, jest, test } from '@jest/globals';
+import { describe, expect, jest, test } from '@jest/globals';
 import {
   act,
   fireEvent,
@@ -129,145 +129,152 @@ const setupDataContractViewerTest = async (
   };
 };
 
-test('Displays contract details', async () => {
-  await setupDataContractViewerTest(
-    mockDataContract,
-    mockPendingManagerApprovalTasksResponse,
-  );
-
-  // Verify title
-  await screen.findByText('Pending Data Contract Request');
-  await screen.findByText(/Access request for/);
-  screen.getByText(/GROUP1/);
-  screen.getByText(/Access Point Group in/);
-  screen.getByText(/MOCK_SDLC_DATAPRODUCT/);
-  screen.getByText(/Data Product/);
-
-  // Verify metadata
-  await screen.findByText('Ordered By:');
-  screen.getByText('test-requester-user-id');
-  screen.getByText(/Ordered For/);
-  screen.getByText('test-consumer-user-id');
-  screen.getByText('Business Justification:');
-  screen.getByText('Test contract creation request');
-
-  // Verify refresh button
-  screen.getByRole('button', { name: 'Refresh' });
-
-  // Verify timeline
-  screen.getByText('Submitted');
-  screen.getByText('Privilege Manager Approval');
-  screen.getByText('Data Producer Approval');
-  screen.getByText('Complete');
-
-  // Verify Contract ID
-  screen.getByText('Contract ID: test-data-contract-id');
-});
-
-test('Shows correct details for Pending Privilege Manager Approval', async () => {
-  await setupDataContractViewerTest(
-    mockDataContract,
-    mockPendingManagerApprovalTasksResponse,
-  );
-
-  // Verify pending assignee
-  await screen.findByText('Assignee:');
-  screen.getByText('test-privilege-manager-user-id');
-});
-
-test('Shows correct details for Pending Data Producer Approval', async () => {
-  await setupDataContractViewerTest(
-    mockDataContract,
-    mockPendingDataOwnerApprovalTasksResponse,
-  );
-
-  // Verify approved task
-  await screen.findByText('Approved by');
-  screen.getByText('test-privilege-manager-user-id');
-  screen.getByText(/08\/06\/2025/);
-  screen.getByText(/:54:46/);
-
-  // Verify pending assignee
-  await screen.findByText('Assignee:');
-  screen.getByText('test-data-owner-user-id');
-});
-
-test('Shows correct details for Approved contract', async () => {
-  await setupDataContractViewerTest(
-    mockDataContract,
-    mockApprovedTasksResponse,
-  );
-
-  // Verify title
-  await screen.findByText('Data Contract Request');
-  expect(screen.queryByText('Pending Data Contract Request')).toBeNull();
-
-  // Verify approved privilege manager task
-  expect(await screen.findAllByText('Approved by')).toHaveLength(2);
-  screen.getByText('test-privilege-manager-user-id');
-  screen.getByText(/08\/06\/2025/);
-  screen.getByText(/:54:46/);
-
-  // Verify approved data owner task
-  screen.getByText('test-data-owner-user-id');
-  screen.getByText(/08\/07\/2025/);
-  screen.getByText(/:32:18/);
-});
-
-test('Shows correct details for Denied contract', async () => {
-  await setupDataContractViewerTest(mockDataContract, mockDeniedTasksResponse);
-
-  // Verify denied privilege manager task
-  await screen.findByText('Denied by');
-  screen.getByText('test-privilege-manager-user-id');
-  screen.getByText(/08\/06\/2025/);
-  screen.getByText(/:54:46/);
-});
-
-test('Shows list of assignees if there is more than 1', async () => {
-  await setupDataContractViewerTest(
-    mockDataContract,
-    mockPendingManagerApprovalMultipleAssigneesTasksResponse,
-  );
-
-  // Verify pending assignees
-  await screen.findByText('Assignees (2):');
-  screen.getByText('test-privilege-manager-user-id-1');
-  screen.getByText('test-privilege-manager-user-id-2');
-});
-
-test('Shows list of "ordered for"" if there is more than 1 consumer and respects initialSelectedUser', async () => {
-  await setupDataContractViewerTest(
-    mockDataContractMultipleConsumers,
-    mockPendingManagerApprovalMultipleConsumersTasksResponse,
-    'test-consumer-user-id-2',
-  );
-
-  // Verify consumers
-  await screen.findByText('test-consumer-user-id-2');
-  expect(screen.queryByText('test-consumer-user-id-1')).toBeNull();
-  const userButton = screen.getByRole('combobox');
-  await act(async () => {
-    fireEvent.mouseDown(userButton);
-  });
-  await screen.findByText('test-consumer-user-id-1');
-});
-
-test('Refresh button re-initializes data contract viewer', async () => {
-  const { MOCK__contractViewerState: mockedContractViewerState } =
+describe('EntitlementsDataContractViewer', () => {
+  test('Displays contract details', async () => {
     await setupDataContractViewerTest(
       mockDataContract,
       mockPendingManagerApprovalTasksResponse,
     );
 
-  // Verify refresh button
-  const refreshButton = await screen.findByRole('button', { name: 'Refresh' });
+    // Verify title
+    await screen.findByText('Pending Data Contract Request');
+    await screen.findByText(/Access request for/);
+    screen.getByText(/GROUP1/);
+    screen.getByText(/Access Point Group in/);
+    screen.getByText(/MOCK_SDLC_DATAPRODUCT/);
+    screen.getByText(/Data Product/);
 
-  const initSpy = jest.spyOn(mockedContractViewerState, 'init');
+    // Verify metadata
+    await screen.findByText('Ordered By:');
+    screen.getByText('test-requester-user-id');
+    screen.getByText(/Ordered For/);
+    screen.getByText('test-consumer-user-id');
+    screen.getByText('Business Justification:');
+    screen.getByText('Test contract creation request');
 
-  expect(initSpy).toHaveBeenCalledTimes(0);
+    // Verify refresh button
+    screen.getByRole('button', { name: 'Refresh' });
 
-  fireEvent.click(refreshButton);
+    // Verify timeline
+    screen.getByText('Submitted');
+    screen.getByText('Privilege Manager Approval');
+    screen.getByText('Data Producer Approval');
+    screen.getByText('Complete');
 
-  await waitFor(() => expect(initSpy).toHaveBeenCalledTimes(1));
+    // Verify Contract ID
+    screen.getByText('Contract ID: test-data-contract-id');
+  });
+
+  test('Shows correct details for Pending Privilege Manager Approval', async () => {
+    await setupDataContractViewerTest(
+      mockDataContract,
+      mockPendingManagerApprovalTasksResponse,
+    );
+
+    // Verify pending assignee
+    await screen.findByText('Assignee:');
+    screen.getByText('test-privilege-manager-user-id');
+  });
+
+  test('Shows correct details for Pending Data Producer Approval', async () => {
+    await setupDataContractViewerTest(
+      mockDataContract,
+      mockPendingDataOwnerApprovalTasksResponse,
+    );
+
+    // Verify approved task
+    await screen.findByText('Approved by');
+    screen.getByText('test-privilege-manager-user-id');
+    screen.getByText(/08\/06\/2025/);
+    screen.getByText(/:54:46/);
+
+    // Verify pending assignee
+    await screen.findByText('Assignee:');
+    screen.getByText('test-data-owner-user-id');
+  });
+
+  test('Shows correct details for Approved contract', async () => {
+    await setupDataContractViewerTest(
+      mockDataContract,
+      mockApprovedTasksResponse,
+    );
+
+    // Verify title
+    await screen.findByText('Data Contract Request');
+    expect(screen.queryByText('Pending Data Contract Request')).toBeNull();
+
+    // Verify approved privilege manager task
+    expect(await screen.findAllByText('Approved by')).toHaveLength(2);
+    screen.getByText('test-privilege-manager-user-id');
+    screen.getByText(/08\/06\/2025/);
+    screen.getByText(/:54:46/);
+
+    // Verify approved data owner task
+    screen.getByText('test-data-owner-user-id');
+    screen.getByText(/08\/07\/2025/);
+    screen.getByText(/:32:18/);
+  });
+
+  test('Shows correct details for Denied contract', async () => {
+    await setupDataContractViewerTest(
+      mockDataContract,
+      mockDeniedTasksResponse,
+    );
+
+    // Verify denied privilege manager task
+    await screen.findByText('Denied by');
+    screen.getByText('test-privilege-manager-user-id');
+    screen.getByText(/08\/06\/2025/);
+    screen.getByText(/:54:46/);
+  });
+
+  test('Shows list of assignees if there is more than 1', async () => {
+    await setupDataContractViewerTest(
+      mockDataContract,
+      mockPendingManagerApprovalMultipleAssigneesTasksResponse,
+    );
+
+    // Verify pending assignees
+    await screen.findByText('Assignees (2):');
+    screen.getByText('test-privilege-manager-user-id-1');
+    screen.getByText('test-privilege-manager-user-id-2');
+  });
+
+  test('Shows list of "ordered for"" if there is more than 1 consumer and respects initialSelectedUser', async () => {
+    await setupDataContractViewerTest(
+      mockDataContractMultipleConsumers,
+      mockPendingManagerApprovalMultipleConsumersTasksResponse,
+      'test-consumer-user-id-2',
+    );
+
+    // Verify consumers
+    await screen.findByText('test-consumer-user-id-2');
+    expect(screen.queryByText('test-consumer-user-id-1')).toBeNull();
+    const userButton = screen.getByRole('combobox');
+    await act(async () => {
+      fireEvent.mouseDown(userButton);
+    });
+    await screen.findByText('test-consumer-user-id-1');
+  });
+
+  test('Refresh button re-initializes data contract viewer', async () => {
+    const { MOCK__contractViewerState: mockedContractViewerState } =
+      await setupDataContractViewerTest(
+        mockDataContract,
+        mockPendingManagerApprovalTasksResponse,
+      );
+
+    // Verify refresh button
+    const refreshButton = await screen.findByRole('button', {
+      name: 'Refresh',
+    });
+
+    const initSpy = jest.spyOn(mockedContractViewerState, 'init');
+
+    expect(initSpy).toHaveBeenCalledTimes(0);
+
+    fireEvent.click(refreshButton);
+
+    await waitFor(() => expect(initSpy).toHaveBeenCalledTimes(1));
+  });
 });
