8af446ecc2b77b7110d7dd899eacc99d33d1da70
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-11-06 16:07:20 -0500

    feat: Don't show legacy data products in dev

--- packages/legend-application-marketplace/src/stores/LegendMarketplaceEnvState.ts
+++ packages/legend-application-marketplace/src/stores/LegendMarketplaceEnvState.ts
@@ -127,7 +127,7 @@ export class DevelopmentLegendMarketplaceEnvState extends LegendMarketplaceEnvSt
   }
 
   supportsLegacyDataProducts(): boolean {
-    return true;
+    return false;
   }
 
   supportedClassifications() {
