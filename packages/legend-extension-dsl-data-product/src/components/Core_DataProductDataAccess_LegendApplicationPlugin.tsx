f0c8ef0a8e51ccc3a2e5eb87c21a194062f30cd5
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-11-20 18:27:29 -0500

    feat: Enable producer contract for enterprise APG

--- packages/legend-extension-dsl-data-product/src/components/Core_DataProductDataAccess_LegendApplicationPlugin.tsx
+++ packages/legend-extension-dsl-data-product/src/components/Core_DataProductDataAccess_LegendApplicationPlugin.tsx
@@ -263,6 +263,7 @@ export class Core_DataProductDataAccess_LegendApplicationPlugin
       },
       {
         type: 'Producer',
+        enableForEnterpriseAPGs: true,
         createContractRenderer: (
           apgState: DataProductAPGState,
           handleOrganizationalScopeChange: (
