02a012d9716e6ef1f6d84710b36af7a515699b8a
Kanbur, Lisa [Engineering] <lisa.kanbur@ny.email.gs.com>
2025-11-22 03:13:30 +0000

    Manual transitive dependency exclusion

--- packages/legend-application-studio/src/stores/editor/EditorGraphState.ts
+++ packages/legend-application-studio/src/stores/editor/EditorGraphState.ts
@@ -799,15 +799,22 @@ export class EditorGraphState {
     projectDependencies: ProjectDependency[],
   ): Promise<ProjectDependencyCoordinates[]> {
     return Promise.all(
-      projectDependencies.map(async (dep) =>
-        Promise.resolve(
+      projectDependencies.map(async (dep) => {
+        const exclusionCoordinates = (dep.exclusions ?? []).map(
+          (exclusion) => ({
+            groupId: guaranteeNonNullable(exclusion.groupId),
+            artifactId: guaranteeNonNullable(exclusion.artifactId),
+          }),
+        );
+        return Promise.resolve(
           new ProjectDependencyCoordinates(
             guaranteeNonNullable(dep.groupId),
             guaranteeNonNullable(dep.artifactId),
             dep.versionId,
+            exclusionCoordinates.length > 0 ? exclusionCoordinates : undefined,
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 
