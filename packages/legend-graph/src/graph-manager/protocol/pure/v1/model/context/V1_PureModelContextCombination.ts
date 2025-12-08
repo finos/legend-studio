87f955fe5326165baca660f33792cb31af9c0be7
gs-gunjan <Gunjan.Shah@ny.email.gs.com>
2025-12-08 19:05:54 +0530

    adding support for pure model context combination

---
+++ packages/legend-graph/src/graph-manager/protocol/pure/v1/model/context/V1_PureModelContextCombination.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2025-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { V1_PureModelContext } from './V1_PureModelContext.js';
+
+export class V1_PureModelContextCombination extends V1_PureModelContext {
+  contexts: V1_PureModelContext[];
+
+  constructor(contexts: V1_PureModelContext[]) {
+    super();
+    this.contexts = contexts;
+  }
+}
