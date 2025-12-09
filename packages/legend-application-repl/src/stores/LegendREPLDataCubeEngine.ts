fe678a0ffb570b23a785c04f829795f3f5dd0830
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-12-09 10:57:18 -0500

    fix: Fix broken DataCubeEngine implementation

--- packages/legend-application-repl/src/stores/LegendREPLDataCubeEngine.ts
+++ packages/legend-application-repl/src/stores/LegendREPLDataCubeEngine.ts
@@ -25,15 +25,17 @@ import {
   type DataCubeExecutionOptions,
 } from '@finos/legend-data-cube';
 import {
-  TDSExecutionResult,
   type V1_AppliedFunction,
+  type V1_PureModelContext,
+  type V1_ValueSpecification,
+  TDSExecutionResult,
+  V1_buildEngineError,
   V1_buildExecutionResult,
   V1_deserializeExecutionResult,
-  type V1_Lambda,
-  type V1_ValueSpecification,
-  V1_buildEngineError,
+  V1_deserializeValueSpecification,
   V1_EngineError,
   V1_getGenericTypeFullPath,
+  V1_Lambda,
   V1_relationTypeModelSchema,
 } from '@finos/legend-graph';
 import {
@@ -72,6 +74,17 @@ export class LegendREPLDataCubeEngine extends DataCubeEngine {
   }
 
   // ---------------------------------- IMPLEMENTATION ----------------------------------
+
+  override _getLambdaRelationType(
+    lambdaObject: PlainObject<V1_Lambda>,
+    model: PlainObject<V1_PureModelContext>,
+  ): Promise<{ columns: { name: string; type: string }[] }> {
+    const lambda = guaranteeType(
+      V1_deserializeValueSpecification(lambdaObject, []),
+      V1_Lambda,
+    );
+    return this._getQueryRelationType(lambda);
+  }
 
   override async processSource(sourceData: PlainObject) {
     switch (sourceData._type) {
