47b5154ce90cbf7c8f35bfc42942aa521837bac1
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2026-03-12 13:47:44 -0400

    feat: Add getDataAccessRequestsCreatedBy and getDataAccessRequestWithWorkflow functions

--- packages/legend-graph/src/graph-manager/protocol/pure/v1/lakehouse/entitlements/V1_DataAccessRequest.ts
+++ packages/legend-graph/src/graph-manager/protocol/pure/v1/lakehouse/entitlements/V1_DataAccessRequest.ts
@@ -124,7 +124,3 @@ export class V1_DataRequestWithWorkflow {
 export class V1_DataRequestsWithWorkflowResponse {
   dataRequests: V1_DataRequestWithWorkflow[] = [];
 }
-
-export class V1_DataRequestsResponse {
-  dataRequests!: V1_DataRequest[];
-}
