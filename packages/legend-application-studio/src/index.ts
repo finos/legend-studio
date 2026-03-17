5fa05ea3e3dc2a7c0227f6e352cf3578c86cc735
jackp5150 <67347420+jackp5150@users.noreply.github.com>
2026-03-17 09:49:26 -0400

    Add service registration export (#4991)
    * Add export for bulk service registration
    
    * changeset
    
    * additional exports
    
    ---------
    
    Co-authored-by: Jack Perkins <jack.perkins@ny.email.gs.com>

--- packages/legend-application-studio/src/index.ts
+++ packages/legend-application-studio/src/index.ts
@@ -126,3 +126,9 @@ export { SnowflakeM2MUdfFunctionActivatorEditor } from './components/editor/edit
 export { HostedServiceFunctionActivatorEditorState } from './stores/editor/editor-state/element-editor-state/function-activator/HostedServiceFunctionActivatorEditorState.js';
 export { MemSQLFunctionActivatorEditorState } from './stores/editor/editor-state/element-editor-state/function-activator/MemSQLFunctionActivatorEditorState.js';
 export { MemSQLFunctionActivatorEditor } from './components/editor/editor-group/function-activator/MemSQLFunctionActivatorEditor.js';
+export { BulkServiceRegistrationEditor } from './components/editor/editor-group/service-editor/BulkServiceRegistrationEditor.js';
+export {
+  REGISTRATION_RESULT,
+  getServiceRegistrationResult,
+} from './stores/editor/sidebar-state/BulkServiceRegistrationState.js';
+export { getRegistrationStatusIcon } from './components/editor/side-bar/RegisterService.js';
