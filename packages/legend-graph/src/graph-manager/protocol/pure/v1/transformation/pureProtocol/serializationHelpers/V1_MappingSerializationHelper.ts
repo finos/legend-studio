5f893fb0dcec98cc8faab35b27d8bf17943ceaed
Reddy Adala, Vithesh R [Engineering] <Vithesh.ReddyAdala@ny.email.gs.com>
2026-06-29 15:04:17 +0530

    lint fix

--- packages/legend-graph/src/graph-manager/protocol/pure/v1/transformation/pureProtocol/serializationHelpers/V1_MappingSerializationHelper.ts
+++ packages/legend-graph/src/graph-manager/protocol/pure/v1/transformation/pureProtocol/serializationHelpers/V1_MappingSerializationHelper.ts
@@ -697,8 +697,8 @@ function V1_serializeRelationFunctionPropertyMapping(
   if (protocol instanceof V1_RelationFunctionPropertyMapping) {
     return serialize(relationFunctionPropertyMappingModelSchema, protocol);
   } else if (protocol instanceof V1_RelationFunctionEmbeddedPropertyMapping) {
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
     return serialize(
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
       relationFunctionEmbeddedPropertyMappingModelSchema,
       protocol,
     );
@@ -713,8 +713,8 @@ function V1_deserializeRelationFunctionPropertyMapping(
     case V1_PropertyMappingType.RELATION_FUNCTION:
       return deserialize(relationFunctionPropertyMappingModelSchema, json);
     case V1_PropertyMappingType.RELATION_FUNCTION_EMBEDDED:
-      // eslint-disable-next-line @typescript-eslint/no-use-before-define
       return deserialize(
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
         relationFunctionEmbeddedPropertyMappingModelSchema,
         json,
       );
