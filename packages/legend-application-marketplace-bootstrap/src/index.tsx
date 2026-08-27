c6fe7a5d1b31aa71a705b97424bcd2f8d1f0fbd1
adishar <aditya.c.sharma@gs.com>
2026-08-27 18:15:14 +0000

    feat: Order progress tracker upgrade and implement search for orders page

--- packages/legend-application-marketplace-bootstrap/src/index.tsx
+++ packages/legend-application-marketplace-bootstrap/src/index.tsx
@@ -19,104 +19,9 @@ import {
   type AbstractPreset,
   type AbstractPlugin,
   WebConsole,
-  NetworkClient,
-  assertNonEmptyString,
-  guaranteeNonNullable,
 } from '@finos/legend-shared';
 import { DSL_DataSpace_GraphManagerPreset } from '@finos/legend-extension-dsl-data-space/graph';
 import { DSL_Diagram_GraphManagerPreset } from '@finos/legend-extension-dsl-diagram/graph';
-import {
-  TelemetryServicePlugin,
-  type TelemetryData,
-} from '@finos/legend-application';
-
-interface GSAnalyticsTelemetryPluginConfigData {
-  url: string;
-  token: string;
-  propertiesKey?: string | undefined;
-}
-
-export class GSAnalyticsTelemetryPlugin extends TelemetryServicePlugin {
-  private _networkClient?: NetworkClient | undefined;
-  private _token?: string | undefined;
-  private _propertiesKey = '__application';
-
-  constructor() {
-    super('@alloy/legend-telemetry-plugin-gs-analytics', '0.0.0');
-  }
-
-  override configure(_configData: object): TelemetryServicePlugin {
-    const configData = _configData as GSAnalyticsTelemetryPluginConfigData;
-    assertNonEmptyString(
-      configData.url,
-      `GS Analytics: Malformed telemetry configuration data: 'url' field is missing or empty`,
-    );
-    assertNonEmptyString(
-      configData.token,
-      `GS Analytics: Malformed telemetry configuration data: 'token' field is missing or empty`,
-    );
-    this._networkClient = new NetworkClient({
-      baseUrl: configData.url,
-    });
-    this._token = configData.token;
-    if (configData.propertiesKey) {
-      this._propertiesKey = configData.propertiesKey;
-    }
-    return this;
-  }
-
-  private get networkClient(): NetworkClient {
-    return guaranteeNonNullable(
-      this._networkClient,
-      `GS Analytics: Telemetry service has not been configured`,
-    );
-  }
-
-  private get token(): string {
-    return guaranteeNonNullable(
-      this._token,
-      `GS Analytics: Telemetry service has not been configured`,
-    );
-  }
-
-  logEvent(eventName: string, data: TelemetryData): void {
-    // See the guide for the event data payload format
-    // See https://gs-analytics.url.gs.com/
-    // See https://confluence.site.gs.com/display/CPUX/Getting+Started+with+Analytics#GettingStartedwithAnalytics-2.Instrumenting
-    this.networkClient
-      .post(
-        '/appevent',
-        {
-          // NOTE: `context.user.kerberos` is optional, but if provided, the other fields in the `context.user` object will be automatically enriched
-          context: { user: { kerberos: this.userId ?? '(unknown)' } },
-          token: this.token,
-          event: eventName,
-          properties: {
-            // NOTE: every form of data must be added within `properties` else they will be removed by the analytics endpoint
-            // so we have to add it like this, though this is sub-optimal
-            // See https://confluence.work.gs.com/display/URMET/Analytics+Data+Model
-            // See https://confluence.work.gs.com/display/URMET/Establishing+Event+Properties
-            [this._propertiesKey]: {
-              name: this.appName,
-              version: this.appVersion,
-              env: this.appEnv,
-              session: this.appSessionId,
-              timestamp: this.appStartTime,
-            },
-            ...data,
-          },
-        },
-        {},
-        {},
-        {},
-        {},
-        { skipProcessing: true },
-      )
-      .catch(() => {
-        /** do nothing */
-      });
-  }
-}
 
 export class LegendMarketplaceWebApplication {
   static getPresetCollection(): AbstractPreset[] {
@@ -130,7 +35,6 @@ export class LegendMarketplaceWebApplication {
   static getPluginCollection(): AbstractPlugin[] {
     return [
       // loggers
-      new GSAnalyticsTelemetryPlugin(),
       new WebConsole(),
     ];
   }
