8682fdf04b6567e4027748465646dc5b6a86b926
ramyas <yash.ramani@ny.email.gs.com>
2026-09-14 17:50:01 +0000

    Unset mapping and set selected runtime for DataProduct Access Point query

--- packages/legend-query-builder/src/stores/workflows/dataProduct/DataProductQueryBuilderState.ts
+++ packages/legend-query-builder/src/stores/workflows/dataProduct/DataProductQueryBuilderState.ts
@@ -29,6 +29,7 @@ import {
   resolveUsableDataProductClasses,
   LakehouseRuntime,
   type LambdaFunction,
+  attachRuntimeFromQuery,
   attachWithFromQuery,
   type MappingModelCoverageAnalysisResult,
   PackageableElementExplicitReference,
@@ -858,6 +859,21 @@ export class DataProductQueryBuilderState extends QueryBuilderState {
         relationMetadata,
       );
       this.changeSourceElement(accessor);
+      this.executionContextState.setMapping(undefined);
+      if (
+        this.executionState instanceof LakehouseDataProductExecutionState &&
+        this.executionState.selectedRuntime
+      ) {
+        this.executionContextState.setRuntimeValue(
+          new RuntimePointer(
+            PackageableElementExplicitReference.create(
+              this.executionState.selectedRuntime,
+            ),
+          ),
+        );
+      } else {
+        this.executionContextState.setRuntimeValue(undefined);
+      }
     }
   }
 
@@ -894,6 +910,14 @@ export class DataProductQueryBuilderState extends QueryBuilderState {
       const runtime = this.executionState.selectedRuntime;
       if (runtime) {
         return attachWithFromQuery(lambdaFunction, this.dataProduct, runtime);
+      }
+      return lambdaFunction;
+    } else if (
+      this.executionState instanceof LakehouseDataProductExecutionState
+    ) {
+      const runtime = this.executionState.selectedRuntime;
+      if (runtime) {
+        return attachRuntimeFromQuery(lambdaFunction, runtime);
       }
       return lambdaFunction;
     }
